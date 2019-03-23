84a81d6c3effe626684f568ea5eb32c2c45074bc
Eduardo A.F <eacevedof@yahoo.es>
2019-03-23 13:55:10 +0100

    views.py configuro lista de posts

--- platzigram/appposts/views.py
+++ platzigram/appposts/views.py
@@ -3,6 +3,29 @@
 # imports Django
 from django.http import HttpResponse
 
+# utilities
+from datetime import datetime
+
+posts = [
+    {
+        "name": "Mont Blanc",
+        "user": "Fist User",
+        "timestamp": datetime.now().strftime("%b $dth, %Y - %H:%M hrs"),
+        "picture": "https://picsum.photos/200/200/?image=1036",
+    },
+    {
+        "name": "Vía Láctea",
+        "user": "Second User",
+        "timestamp": datetime.now().strftime("%b $dth, %Y - %H:%M hrs"),
+        "picture": "https://picsum.photos/200/200/?image=903",
+    },
+    {
+        "name": "Nuevo Auditorio",
+        "user": "Third User",
+        "timestamp": datetime.now().strftime("%b $dth, %Y - %H:%M hrs"),
+        "picture": "https://picsum.photos/200/200/?image=1076",
+    }
+]
 
 def list_posts(request):
     """List existing posts"""
