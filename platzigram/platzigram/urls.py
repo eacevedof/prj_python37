d26862c7893a135a9d2f2e9d45494af3655fa9d4
Eduardo A.F <eacevedof@yahoo.es>
2019-03-23 12:32:01 +0100

    elimino platzigram

--- platzigram/platzigram/urls.py
+++
@@ -1,9 +0,0 @@
-"""urls.py"""
-from django.urls import path
-from platzigram import views
-
-urlpatterns = [
-    path("hello-world/",views.hello_world),
-    path("sorted/",views.sort_integers),
-    path("hi/<str:name>/<int:age>/",views.say_hi),
-]
