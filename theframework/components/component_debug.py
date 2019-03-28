a06884127998ee85a315b877f1bc9a25727ff9ac
Eduardo A.F <eacevedof@yahoo.es>
2019-03-28 22:44:31 +0100

    component_debug

--- theframework/components/component_debug.py
+++ theframework/components/component_debug.py
@@ -1,5 +1,6 @@
 import io
 import re
+from pprint import pprint
 
 class ComponentDebug():
 
@@ -8,14 +9,31 @@ class ComponentDebug():
     _isPythonInfoOn = False
     _isIncludeOn = False
 
+    _arMessages = []
+    _arSqls = []
+    _arIncluded = []
+
     def __init__(self):
         pass
 
-    @staticmethod
-    def config(isSqlOn=False,isMessageOn=False,isPythonInfoOn=False,isIncludeOn=False):
-        _isSqlOn = isIncludeOn
-        _isMessageOn = isIncludeOn
-        _isPythonInfoOn = isPythonInfoOn
-        _isIncludeOn = isIncludeOn
+    @classmethod
+    def config(cls,isSqlOn=False,isMessageOn=False,isPythonInfoOn=False,isIncludeOn=False):
+        cls._isSqlOn = isIncludeOn
+        cls._isMessageOn = isIncludeOn
+        cls._isPythonInfoOn = isPythonInfoOn
+        cls._isIncludeOn = isIncludeOn
+        cls.debug(dir(cls))
 
-    
+
+    @classmethod
+    def set_sql(cls,sSQL,iCount=0,fTime=""):
+        if cls._isSqlOn:
+            cls._arSqls.append({"sql":sSQL,"count":iCount,"time":fTime})
+
+
+    @classmethod
+    def debug(cls,mxVar):
+        pprint(mxVar)
+
+if __name__ == "__main__":
+    ComponentDebug.config(True,True,True,False)
