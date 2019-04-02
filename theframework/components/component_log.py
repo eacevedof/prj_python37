9acc0280adb2069465e4c622fcb6bb1fd4c5526b
Eduardo A.F <eacevedof@yahoo.es>
2019-04-02 22:24:25 +0200

    constantes

--- theframework/components/component_log.py
+++ theframework/components/component_log.py
@@ -1,13 +1,41 @@
 """
 theframework/components/component_log.py
 """
+import os
+from datetime import datetime
+
 class ComponentLog():
 
-    def __init__(self):
+    DS = os.sep
+    DIR = os.getcwd()
+
+    sNow = datetime.now().strftime("%Y%m%d")
+    sPathFolder = ""
+    sSubfType = ""
+    sFileName = ""
+
+    def __init__(self, sSubfType = "", sPathFolder = ""):
+        self.sPathFolder = sPathFolder
+        self.sSubfType = sSubfType
+        self.sFileName = "app_"+self.sNow+".log"
+
+        if not sPathFolder:
+            self.sPathFolder = self.DIR
+
+        if not sSubfType:
+            self.sSubfType = "debug"
+
+        self.fix_folder()
+    
+
+    def fix_folder(self):
         pass
 
     def write(self):
-        f = open("testwrite.txt", "a")
+
+        sFile = self.sPathFolder+self.DS+self.sFileName
+        print(sFile)
+        f = open(sFile, "a")
         f.write("Now the file has more content!")
         f.close()
 
