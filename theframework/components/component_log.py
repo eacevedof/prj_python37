a3dcf8ceb95634029e93fff4a65521c931894c0b
Eduardo A.F <eacevedof@yahoo.es>
2019-04-02 21:39:41 +0200

    test write

--- theframework/components/component_log.py
+++ theframework/components/component_log.py
@@ -1,3 +1,17 @@
+"""
+theframework/components/component_log.py
+"""
 class ComponentLog():
+
     def __init__(self):
         pass
+
+    def write(self):
+        f = open("testwrite.txt", "a")
+        f.write("Now the file has more content!")
+        f.close()
+
+
+if __name__ == "__main__":
+    o = ComponentLog()
+    o.write()
