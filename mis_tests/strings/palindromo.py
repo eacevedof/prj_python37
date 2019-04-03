9c03595fe29f4379efff53bbc9fc67f6aceb676d
Eduardo A.F <eacevedof@yahoo.es>
2019-04-03 22:21:52 +0200

    palindromo

---
+++ mis_tests/strings/palindromo.py
@@ -0,0 +1,56 @@
+"""
+Detecta palindromos:
+
+http://www.estandarte.com/noticias/idioma-espanol/qu-es-un-palndromo-ejemplos_1723.html
+Ejemplos:
+
+A Bali su flan anal fusilaba
+No subas, abusón
+Oí lo de mamá: me dolió
+Sometamos o matemos
+Yo dono rosas, oro no doy
+Isaac no ronca así
+Lavan esa base naval
+No traces en ese cartón
+¿Será lodo o dólares?
+Logra Casillas allí sacar gol
+"""
+import io
+from pprint import pprint 
+
+class Palindromo():
+
+    arText = []
+
+    def __init__(self,sText=""):
+        if self.arText:
+            self.arText.append(sText)
+
+    def __is_mirror(self,sText):
+        iLen = len(sText)
+        cnr = divmod(iLen,2)
+        pprint(cnr)
+        # pprint(cnr(0))
+        # pprint(cnr(1))        
+        if (iLen % 2)==0 :
+            return False
+
+        iHalf = (iLen / 2)
+
+    def check(self):
+        arResult = []
+        pprint(self.arText)
+        for sText in self.arText:
+            print(sText)
+            self.__is_mirror(sText)
+
+
+
+    def add_text(self,sValue):
+        self.arText.append(sValue)
+
+
+if __name__ == "__main__":
+    o = Palindromo()
+    o.add_text("Logra Casillas allí sacar gol")
+    o.check()
--- theframework/components/component_log.py
+++ theframework/components/component_log.py
@@ -29,7 +29,44 @@ class ComponentLog():
     
 
     def fix_folder(self):
-        pass
+        sLogFolder = self.sPathFolder+self.DS+self.sSubfType+self.DS
+        if not os.path.isdir(sLogFolder):
+            os.mkdir(sLogFolder)
+
+    def __merge(sContent,sTitle)
+        sNow = datetime.now().strftime("%Y%m%d-%H%i%s")
+        sReturn = "-- ["+sNow+"]\n"
+        if sTitle: 
+            sReturn += sTitle + ":\n"
+        if sContent:
+            sReturn += sContent + "\n\n"
+        return sReturn
+
+    def save(mxVar,sTitle=None)
+    
+        if not isinstance(mxVar, str):
+            mxVar = var_export(mxVar,1)
+        
+        sPathFile = self.sPathFolder.self::DS
+                        .self.sSubfType.self::DS
+                        .self.sFileName
+        
+        if(is_file(sPathFile))
+            oCursor = fopen(sPathFile,"a")
+        else:
+            oCursor = fopen(sPathFile,"x")
+        if(oCursor !== False)
+            sToSave = self.merge(mxVar,sTitle)
+            fwrite(oCursor,"") //Grabo el caracter vacio
+            if(!empty(sToSave)) fwrite(oCursor,sToSave)
+            fclose(oCursor) //cierro el archivo.
+        else:
+            return False
+        return True
+    
+    def set_filename(sValue){self.sFileName="sValue.log"}
+    def set_subfolder(sValue){self.sSubfType="sValue"}
+
 
     def write(self):
 
