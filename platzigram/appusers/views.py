9f5187b1590015a10f3904ae92d0280ec5bb8e68
Eduardo A.F <eacevedof@yahoo.es>
2019-04-06 19:20:27 +0200

    Platzigram 22 Video. Signup fin, ojo con la excepción

--- platzigram/appusers/views.py
+++ platzigram/appusers/views.py
@@ -4,6 +4,12 @@ platzigram/appusers/views.py
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.db.utils import IntegrityError
+import pdb 
+
+#Models
+from django.contrib.auth.models import User
+from appusers.models import Profile
 
 def login_view(request):
     """Login view."""
@@ -31,3 +37,34 @@ def logout_view(request):
     """
     logout(request)
     return redirect("login")
+
+def signup_view(request):
+    """Sign up view"""
+    # pdb.set_trace() # pausa la consola para meter comandos de depuración: request.POST
+
+    if request.method == "POST":
+        username = request.POST["signup-username"]
+        passw = request.POST["signup-password"]
+        password_confirmation = request.POST["signup-password-confirmation"]
+    
+        if passw != password_confirmation:
+            return render(request,"users/signup.html",{"error":"Pass confrimation does not match!"})
+
+        # try ya que puede fallar si ya existiera el username
+        try:
+            user = User.objects.create_user(username = username,password = passw)
+        except IntegrityError:
+            return render(request,"users/signup.html", {"error":"Username already exists"})
+
+        #despues de guardar el usuario en la bd
+        user.first_name = request.POST["signup-firstname"]
+        user.last_name = request.POST["signup-lastname"]
+        user.email = request.POST["signup-email"]
+        user.save()
+
+        #creamos inmediatamente el perfil
+        profile = Profile(user=user)
+        profile.save()
+        return redirect("login")
+
+    return render(request,"users/signup.html")
--- platzigram/platzigram/urls.py
+++ platzigram/platzigram/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("users/login/", users_views.login_view, name="login"),
     path("users/logout/", users_views.logout_view, name="logout"),
+    path("users/signup/", users_views.signup_view, name="signup"),
 
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
