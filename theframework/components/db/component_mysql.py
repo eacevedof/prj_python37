99f75cd86db04424088b0bae6c32e9355680e424
Eduardo A.F <eacevedof@yahoo.es>
2019-04-10 21:40:51 +0200

    intento de migracion de db

--- theframework/components/db/component_mysql.py
+++ theframework/components/db/component_mysql.py
@@ -1,5 +1,5 @@
 import mysql.connector
-
+from pprint import pprint
 
 """
  * @author Eduardo Acevedo Farje.
@@ -8,129 +8,29 @@ import mysql.connector
  * @file component_mysql.php v2.0.0
  * @date 02-12-2018 13:20 SPAIN
  * @observations
+ pip install mysql-connector-python
  """
 
 class ComponentMysql:
 
-    arConn
-    isError
-    arErrors    
-    iAffected
+    arConn = []
+    isError = False
+    arErrors = []  
+    iAffected = 0
     
-    def __init__(arConn=[]): 
-    
-        self.isError = False
-        self.arErrors = []
+    def __init__(self,arConn=[]): 
         self.arConn = arConn
     
-
-    def __get_conn_string():
-        arCon = []
-        
-        arCon["mysql:host"] = {"mysql:host":{{self.arConn["server"] | "" : self.arConn["server"] }}}
-        arCon["dbname"]= {"dbname":{{self.arConn["server"] | "" : self.arConn["server"] }}}
-        
-        sString = ""
-        for arCon as sK=>sV)
-            sString += "sK=sV"
-        
-        return sString
-    # get_conn_string
-
-    def __get_rowcol(arResult,iCol=NULL,iRow=NULL)
+    def test(self):
+        self.p([],"ComponentMysql")
     
-        if is_int(iCol) || is_int(iRow))
-        
-            arColnames = arResult[0]
-            arColnames = array_keys(arColnames)
-# bug(arColnames)
-            sColname = (isset(arColnames[iCol])?arColnames[iCol]:"")
-            if sColname)
-                arResult = array_column(arResult,sColname)
-        
-            if isset(arResult[iRow]))
-                arResult = arResult[iRow]
-        
-        return arResult
-    
-    
-    def query(sSQL,iCol=NULL,iRow=NULL)
-    
-        try 
-        
-            sConn = self.get_conn_string()
-            # https:# stackoverflow.com/questions/38671330/error-with-php7-and-sql-server-on-windows
-            oPdo = new \PDO(sConn,self.arConn["user"],self.arConn["password"]
-                    ,[\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"])
-            oPdo->setAttribute(\PDO::ATTR_ERRMODE,\PDO::ERRMODE_EXCEPTION ) 
-            self.log(sSQL,"ComponentMysql.exec")
-            oCursor = oPdo->query(sSQL)
-            if oCursor===False)
-            
-                self.add_error("exec-error: sSQL")
-            
-            else:
-            
-                # var_dump(stmt)
-                arResult = []
-                while(arRow = oCursor->fetch(\PDO::FETCH_ASSOC))
-                    arResult[] = arRow
-                
-                self.iAffected = count(arResult)
-                
-                if arResult)
-                    arResult = self.get_rowcol(arResult,iCol,iRow)
-            
-        
-        catch(PDOException oE)
-        
-            sMessage = "exception:oE->getMessage()"
-            self.add_error(sMessage)
-        
-        return arResult
-    # query
-    
-    def exec(sSQL)
-    
-        try 
-        
-            sConn = self.get_conn_string()
-            # https:# stackoverflow.com/questions/19577056/using-pdo-to-create-table
-            oPdo = new \PDO(sConn,self.arConn["user"],self.arConn["password"]
-                    ,[\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"])
-            oPdo->setAttribute(\PDO::ATTR_ERRMODE,\PDO::ERRMODE_EXCEPTION )
-            self.log(sSQL,"ComponentMysql.exec")
-            mxR = oPdo->exec(sSQL)
-            self.iAffected = mxR
-            if mxR===False)
-            
-                self.add_error("exec-error: sSQL")
-            
-            return mxR
-        
-        catch(PDOException oE)
-        
-            sMessage = "exception:oE->getMessage()"
-            self.add_error(sMessage)
-        
-    # exec    
-    
-    def __log(mxVar,sTitle=NULL)
-    
-        if defined("PATH_LOGS") && class_exists("\TheFramework\Components\ComponentLog"))
-        
-            oLog = new \TheFramework\Components\ComponentLog("debug",PATH_LOGS)
-            oLog->save(mxVar,sTitle)
-        
-    
+    def p(self,mxVal,sTitle=""):
+        print("\n")
+        if sTitle:
+            print(sTitle+"\n:")
+        pprint(mxVal)
   
-    def __add_error(sMessage)self.isError = Trueself.iAffected=-1 self.arErrors[]=sMessage    
-    def is_error()return self.isError
-    def get_errors()return self.arErrors
-    def get_error(i=0)return isset(self.arErrors[i])?self.arErrors[i]:NULL
-    def show_errors()echo "<pre>"+var_export(self.arErrors,1)
-    
-    def add_conn(k,v)self.arConn[k]=v
-    def get_affected()return self.iAffected
-    
 # ComponentMysql
+if __name__ == "__main__":
+    o = ComponentMysql()
+    o.test()
