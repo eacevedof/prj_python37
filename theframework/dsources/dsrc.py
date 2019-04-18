c29c2ac819274593780cff9547f32b6ac6e907ab
Eduardo A.F <eacevedof@yahoo.es>
2019-04-18 13:21:31 +0200

    cambio saltos a utf8 unix

---
+++ theframework/dsources/dsrc.py
@@ -0,0 +1,7 @@
+"""
+/theframework/dsources/dsources.py
+"""
+class Dsrc():
+    @staticmethod
+    def get_config():
+        return "text"
---
+++ theframework/tests/test_dsrc.py
@@ -0,0 +1,14 @@
+import sys	
+# sys.path.append('/ufs/guido/lib/python')	
+sys.path.append("..")
+import unittest
+
+from dsources.dsrc import Dsrc	
+class  TestDsrc(unittest.TestCase):	
+
+     def test_test_context(self):
+        text = Dsrc.get_config()	
+        print(text)	
+        #assert x != y;
+        #self.assertEqual(x, y, "Msg");	       
+        self.fail("TODO: Write test")
