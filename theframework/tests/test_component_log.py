5a271a70d1d6aeb7ec3c9e2ba084ddff139b33de
Eduardo A.F <eacevedof@yahoo.es>
2019-04-18 18:47:59 +0200

    escribe el log pero la fecha no es correcta

--- theframework/tests/test_component_log.py
+++ theframework/tests/test_component_log.py
@@ -9,14 +9,17 @@ from pprint import pprint
 from components.component_log import ComponentLog as Log	
 
 class TestComponentLog(unittest.TestCase):	
-    __o = Log()
 
-    def tes_is_connected(self):
+    def test_is_instance(self):
         o = Log()
-        assert isinstance(o,ComponentLog)
+        assert isinstance(o,Log)
         # self.assertEqual(o.is_connected(),True)       
         #self.fail("TODO: Write test")
 
+    def test_save(self):
+        o = Log()
+        o.save("texto de prueba")
+        o.save("texto de prueba2 con titulo","Linea 2")
 
 #class TestComponentLog
 
