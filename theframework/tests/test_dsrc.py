2425e8c1838cbc82ff6e6b4da958e359fccf10d8
Eduardo A.F <eacevedof@yahoo.es>
2019-04-18 14:51:04 +0200

    ya carga el json en un diccionario

--- theframework/tests/test_dsrc.py
+++ theframework/tests/test_dsrc.py
@@ -4,23 +4,20 @@ sobre los test: https://realpython.com/python-testing/#unit-tests-vs-integration
 sobre el import: https://stackoverflow.com/questions/30669474/beyond-top-level-package-error-in-relative-import
 """
 import sys; sys.path.append("..")
+
 import unittest
+from pprint import pprint
 
 from dsources.dsrc import Dsrc	
 
 class  TestDsrc(unittest.TestCase):	
 
     def test_get_config(self):
-        text = Dsrc.get_config()
-        print("test_get_config",text)	
-        assert isinstance(text,str)
+        mx_var = Dsrc.get_config()
+        pprint(mx_var)
+        assert isinstance(mx_var,str)
         #self.assertEqual(x, y, "Msg");	       
         #self.fail("TODO: Write test")
-        
-    def test_loadjson(self):
-        text = Dsrc.load_config()
-        print("test_loadjson: ",text)
-        assert isinstance(text,str)
 
 
 if __name__ == "__main__":
