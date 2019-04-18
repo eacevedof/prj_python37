aa6dd9f5d1e11c4704303d9bab18689fb561bbbe
Eduardo A.F <eacevedof@yahoo.es>
2019-04-18 15:04:29 +0200

    el test pasa bien, refactor de Dsrc a dsrc

--- theframework/dsources/dsrc.py
+++ theframework/dsources/dsrc.py
@@ -5,7 +5,7 @@ from pprint import pprint
 import json
 import os
 
-class Dsrc():
+class dsrc():
     full_path = os.path.realpath(__file__)
     dir_path = os.path.dirname(full_path)
     path_json = os.path.join(dir_path,"dsources.json")
@@ -13,30 +13,41 @@ class Dsrc():
 
     @staticmethod
     def load_config():
-        
-        with open(Dsrc.path_json,"r") as f:
-            # lista de diccionarios
-            o_list = json.load(f)
-          
-        o_dict = {}
-        for i in range(len(o_list)):
-            id = o_list[i]["id"]
-            o_dict[id] = o_list[i]
-            
-        #pprint(o_dict)
-        # from inspect import getmembers
-        #from pprint import pprint
-        # pprint(getmembers(o_list))
-    
-        # print(type(o_list),dir(o_list))
-        # pprint(distros_dict)
-        
-        # y = json.dumps(distros_dict)
-        # print(dir(y))
-        # pprint(y)
-        Dsrc.data = o_dict
+        if not dsrc.data:
+            with open(dsrc.path_json,"r") as f:
+                # lista de diccionarios
+                o_list = json.load(f)
+
+            o_dict = {}
+            for i in range(len(o_list)):
+                id = o_list[i]["id"]
+                o_dict[id] = o_list[i]
+
+            #pprint(o_dict)
+            # from inspect import getmembers
+            #from pprint import pprint
+            # pprint(getmembers(o_list))
+
+            # print(type(o_list),dir(o_list))
+            # pprint(distros_dict)
+
+            # y = json.dumps(distros_dict)
+            # print(dir(y))
+            # pprint(y)
+            dsrc.data = o_dict
     
     @staticmethod
     def get_config():
-        Dsrc.load_config()
-        return Dsrc.data
+        dsrc.load_config()
+        return dsrc.data
+    
+    @staticmethod
+    def get_context(id):
+        dsrc.load_config()
+        return dsrc.data[id]
+    
+    @staticmethod
+    def get_value(id,val):
+        dsrc.load_config()
+        return dsrc.data[id][val]
+        
--- theframework/tests/test_dsrc.py
+++ theframework/tests/test_dsrc.py
@@ -8,17 +8,26 @@ import sys; sys.path.append("..")
 import unittest
 from pprint import pprint
 
-from dsources.dsrc import Dsrc	
+from dsources.dsrc import dsrc	
 
 class  TestDsrc(unittest.TestCase):	
 
     def test_get_config(self):
-        mx_var = Dsrc.get_config()
+        mx_var = dsrc.get_config()
         pprint(mx_var)
         assert isinstance(mx_var,dict)
         #self.assertEqual(x, y, "Msg");	       
         #self.fail("TODO: Write test")
 
+    def test_get_context(self):
+        mx_var = dsrc.get_context("mysql-1")
+        pprint(mx_var)
+        assert isinstance(mx_var,dict)
+        
+    def test_get_value(self):
+        mx_var = dsrc.get_value("mysql-1","description")
+        pprint(mx_var)
+        assert isinstance(mx_var,str)
 
 if __name__ == "__main__":
     unittest.main()
