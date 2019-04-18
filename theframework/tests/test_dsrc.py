27b6ca79365230a2b06b484a673097c886ff5812
Eduardo A.F <eacevedof@yahoo.es>
2019-04-18 13:31:00 +0200

    test_dsrc ya va perfecto

--- theframework/tests/test_dsrc.py
+++ theframework/tests/test_dsrc.py
@@ -2,11 +2,17 @@ import sys; sys.path.append("..")
 import unittest
 
 from dsources.dsrc import Dsrc	
+
 class  TestDsrc(unittest.TestCase):	
 
-     def test_test_context(self):
-        text = Dsrc.get_config()	
-        print(text)	
-        #assert x != y;
+    def test_get_config(self):
+        text = Dsrc.get_config()
+        print("hola",text)	
+        assert isinstance(text,str)
         #self.assertEqual(x, y, "Msg");	       
-        self.fail("TODO: Write test")
+        #self.fail("TODO: Write test")
+
+
+if __name__ == "__main__":
+    unittest.main()
+    print("Everything passed")        
