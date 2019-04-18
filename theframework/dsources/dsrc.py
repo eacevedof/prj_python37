a4e65f7232894d915012aafe4f1727426cb9c00c
Eduardo A.F <eacevedof@yahoo.es>
2019-04-18 14:01:01 +0200

    ya devuelve el json

--- theframework/dsources/dsrc.py
+++ theframework/dsources/dsrc.py
@@ -1,7 +1,24 @@
 """
 /theframework/dsources/dsources.py
 """
+from pprint import pprint
+import json
+import os
+
 class Dsrc():
+    full_path = os.path.realpath(__file__)
+    dir_path = os.path.dirname(full_path)
+    path_file = os.path.join(dir_path,"dsources.json")
+
+    @staticmethod
+    def load_config():
+        
+        with open(Dsrc.path_file,"r") as f:
+            distros_dict = json.load(f)
+            
+        pprint(distros_dict)
+        return ""
+    
     @staticmethod
     def get_config():
         return "text"
