17ca807c869a215f279c12a483a96041f262a576
Eduardo A.F <eacevedof@yahoo.es>
2019-04-18 14:51:04 +0200

    ya carga el json en un diccionario

--- theframework/dsources/dsrc.py
+++ theframework/dsources/dsrc.py
@@ -9,22 +9,34 @@ class Dsrc():
     full_path = os.path.realpath(__file__)
     dir_path = os.path.dirname(full_path)
     path_json = os.path.join(dir_path,"dsources.json")
+    data = {}
 
     @staticmethod
     def load_config():
         
         with open(Dsrc.path_json,"r") as f:
-            distros_dict = json.load(f)
+            # lista de diccionarios
+            o_list = json.load(f)
+          
+        o_dict = {}
+        for i in range(len(o_list)):
+            id = o_list[i]["id"]
+            o_dict[id] = o_list[i]
             
-        
-        #print(type(distros_dict))
+        #pprint(o_dict)
+        # from inspect import getmembers
+        #from pprint import pprint
+        # pprint(getmembers(o_list))
+    
+        # print(type(o_list),dir(o_list))
         # pprint(distros_dict)
         
         # y = json.dumps(distros_dict)
         # print(dir(y))
         # pprint(y)
-        return ""
+        Dsrc.data = o_dict
     
     @staticmethod
     def get_config():
-        return "text"
+        Dsrc.load_config()
+        return Dsrc.data
