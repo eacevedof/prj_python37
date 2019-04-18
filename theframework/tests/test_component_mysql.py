47d6019c183557a4d5af96e5a0481f9265712fc5
Eduardo A.F <eacevedof@yahoo.es>
2019-04-18 17:58:53 +0200

    ComponentMysql 1.0.0 hace el crud completo

--- theframework/tests/test_component_mysql.py
+++ theframework/tests/test_component_mysql.py
@@ -10,7 +10,28 @@ from dsources.dsrc import dsrc
 from components.db.component_mysql import ComponentMysql	
 
 class TestComponentMysql(unittest.TestCase):	
+    __o = ComponentMysql("mysql-1")
 
+    def get_rows(self):
+        strsql = "SELECT * FROM operation LIMIT 3"
+        lstrows = self.__o.query(strsql)
+        return lstrows
+    
+    def insert(self):
+        strsql = "INSERT INTO v(i,s,f) VALUES (1,'some string',1.2)"
+        ir = self.__o.execute(strsql)
+        return ir
+    
+    def update(self):
+        strsql = "UPDATE v SET i=88,s='updated str', f=9.89 WHERE id>3"
+        ir = self.__o.execute(strsql)
+        return ir
+    
+    def delete(self):
+        strsql = "DELETE FROM v WHERE id>15"
+        ir = self.__o.execute(strsql)
+        return ir
+    
     def tes_is_connected(self):
         o = ComponentMysql("mysql-1")
         # assert isinstance(mxvar,dict)
@@ -24,32 +45,28 @@ class TestComponentMysql(unittest.TestCase):
         # self.fail("connection error")
         
     def tes_get_rows(self):
-        o = ComponentMysql("mysql-1")
-        mxvar = o.get_rows()
+        mxvar = self.get_rows()
         #o.show_errors()
         pprint(mxvar)
         assert isinstance(mxvar,list)
         
     def test_update(self):
-        o = ComponentMysql("mysql-1")
-        mxvar = o.update()
-        o.show_errors()
+        mxvar = self.update()
+        self.__o.show_errors()
         pprint(mxvar)
         assert isinstance(mxvar,int)  
         
     def test_insert(self):
-        o = ComponentMysql("mysql-1")
-        mxvar = o.insert()
-        o.show_errors()
+        mxvar = self.insert()
+        self.__o.show_errors()
         pprint(mxvar)
         assert isinstance(mxvar,int)
         
-    def test_lastquery(self):
-        o = ComponentMysql("mysql-1")
-        o.update()
-        o.insert()
-        o.delete()
-        mxvar = o.get_last_query()
+    def tes_lastquery(self):
+        self.update()
+        self.insert()
+        self.delete()
+        mxvar = self.__o.get_last_query()
         pprint(mxvar)
         assert "read" in mxvar
         assert "write" in mxvar
