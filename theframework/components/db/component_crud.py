317c90901a4fcae5f4759006e98078b55214254d
Eduardo A.F <eacevedof@yahoo.es>
2019-04-19 16:50:53 +0200

    backup numeric

--- theframework/components/db/component_crud.py
+++ theframework/components/db/component_crud.py
@@ -4,24 +4,25 @@ from pprint import pprint
 class ComponentCrud():
     
     def __init__(self,objdb=None):
-        self.lstend = []
-        self.lstresult = []
-        self.lstinsertfv = []
-        self.lstupdatefv = []
-        self.lstpksfv = []
+        self.lstends = []
+        self.lstjoins = []
+        self.lstresults = []
+        self.lstinsertfvs = []
+        self.lstupdatefvs = []
+        self.lstpksfvs = []
         self.lstgetfields = []
-        self.lstorderby = []
-        self.lstnumeric = []
+        self.lstorderbys = []
+        self.lstnumerics = []
         self.lstands = []
         self.ojbdb = objdb
         
         
     def __get_orderby(self):
         strorderby = ""
-        if self.lstorderby:
+        if self.lstorderbys:
             lstsql = []
             strorderby = " ORDER BY "
-            for dic in self.lstorderby:
+            for dic in self.lstorderbys:
                 for f in dic.keys():
                     #field AscDesc
                     lstsql.append("{} {}".format(f,dic[f]))
@@ -30,10 +31,23 @@ class ComponentCrud():
         
         return strorderby
     
+    def __get_imploded(self,lststrings):
+        return " "+"\n".join(lststrings)
     
+    def __get_joins(self):
+        strreturn = self._get_imploded(self.lstjoins)
+        return strreturn
+    
+    def __get_end(self):
+        strreturn = self._get_imploded(self.lstends)
+        return strreturn
+        
+   def __is_numeric(self,strfieldname):
+        return strfieldname in self.lstnumerics
+   
     def add_orderby(self,strfield,strascdesc=""):
         if strfield:
-            self.lstorderby.append({strfield:strascdesc})
+            self.lstorderbys.append({strfield:strascdesc})
     
     def test(self):
         return self.__get_orderby()
