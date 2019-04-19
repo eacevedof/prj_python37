3133343c2e0512f81435056801a72e5050242e05
Eduardo A.F <eacevedof@yahoo.es>
2019-04-19 16:22:14 +0200

    backup constructor

--- theframework/components/db/component_crud.py
+++ theframework/components/db/component_crud.py
@@ -3,8 +3,18 @@ from pprint import pprint
 
 class ComponentCrud():
     
-    def __init__(self):
-        pass
+    def __init__(self,objdb=None):
+        self.lstend = []
+        self.lstresult = []
+        self.lstinsertfv = []
+        self.lstupdatefv = []
+        self.lstpksfv = []
+        self.lstgetfields = []
+        self.lstorderby = []
+        self.lstnumeric = []
+        self.lstands = []
+        self.ojbdb = objdb
+    
     
 #class ComponentCrud
 
