17d6e0bfc55eedf6f4340448226d2cc18d6214c0
Eduardo A.F <eacevedof@yahoo.es>
2019-04-20 23:05:33 +0200

    serializers finalizado

--- apirest/learnlang/theapp/serializers.py
+++ apirest/learnlang/theapp/serializers.py
@@ -3,100 +3,100 @@ from .models import *
  
 class AppArraySerializer(serializers.ModelSerializer):
     class Meta:
-        model = xxx
+        model = AppArray
         fields = '__all__'
 
 class AppExamSerializer(serializers.ModelSerializer):
     class Meta:
-        model = xxx
+        model = AppExam
         fields = '__all__'
         
 class AppExamsSentencesSerializer(serializers.ModelSerializer):
     class Meta:
-        model = xxx
+        model = AppExamsSentences
         fields = '__all__'
         
 class AppExamsUsersSerializer(serializers.ModelSerializer):
     class Meta:
-        model = xxx
+        model = AppExamsUsers
         fields = '__all__'
         
 class AppExamsUsersEvalhSerializer(serializers.ModelSerializer):
     class Meta:
-        model = xxx
+        model = AppExamsUsersEvalh
         fields = '__all__'
         
 class AppExamsUsersEvallSerializer(serializers.ModelSerializer):
     class Meta:
-        model = xxx
+        model = AppExamsUsersEvall
         fields = '__all__'
         
 class AppSentenceSerializer(serializers.ModelSerializer):
     class Meta:
-        model = xxx
+        model = AppSentence
         fields = '__all__'
         
 class AppSentenceImagesSerializer(serializers.ModelSerializer):
     class Meta:
-        model = xxx
+        model = AppSentenceImages
         fields = '__all__'
         
 class AppSentencesUsersSerializer(serializers.ModelSerializer):
     class Meta:
-        model = xxx
+        model = AppSentencesUsers
         fields = '__all__'
         
 class AppSentenceTagsSerializer(serializers.ModelSerializer):
     class Meta:
-        model = xxx
+        model = AppSentenceTags
         fields = '__all__'
         
 class AppSentenceTimesSerializer(serializers.ModelSerializer):
     class Meta:
-        model = xxx
+        model = AppSentenceTimes
         fields = '__all__'
         
 class AppSentenceTrSerializer(serializers.ModelSerializer):
     class Meta:
-        model = xxx
+        model = AppSentenceTr
         fields = '__all__'
         
 class AppTagSerializer(serializers.ModelSerializer):
     class Meta:
-        model = xxx
+        model = AppTag
         fields = '__all__'
         
 class BaseLanguageSerializer(serializers.ModelSerializer):
     class Meta:
-        model = xxx
+        model = BaseLanguage
         fields = '__all__'
         
 class BaseLanguageLangSerializer(serializers.ModelSerializer):
     class Meta:
-        model = xxx
+        model = BaseLanguageLang
         fields = '__all__'
         
 class BaseUserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = xxx
+        model = BaseUser
         fields = '__all__'
         
 class BaseUserArraySerializer(serializers.ModelSerializer):
     class Meta:
-        model = xxx
+        model = BaseUserArray
         fields = '__all__'
         
 class TemplateSerializer(serializers.ModelSerializer):
     class Meta:
-        model = xxx
+        model = Template
         fields = '__all__'
         
 class TemplateArraySerializer(serializers.ModelSerializer):
     class Meta:
-        model = xxx
+        model = TemplateArray
         fields = '__all__'
         
 class VersionDbSerializer(serializers.ModelSerializer):
     class Meta:
-        model = xxx
+        model = VersionDb
         fields = '__all__'
