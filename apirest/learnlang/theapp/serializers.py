7e45b4eaa729d6e8f2deec1c8b0f4e9f52f0962e
Eduardo A.F <eacevedof@yahoo.es>
2019-04-20 23:01:58 +0200

    backup serializers

--- apirest/learnlang/theapp/serializers.py
+++ apirest/learnlang/theapp/serializers.py
@@ -1,1 +1,102 @@
+from rest_framework import serializers
+from .models import *
+ 
+class AppArraySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = xxx
+        fields = '__all__'
 
+class AppExamSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = xxx
+        fields = '__all__'
+        
+class AppExamsSentencesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = xxx
+        fields = '__all__'
+        
+class AppExamsUsersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = xxx
+        fields = '__all__'
+        
+class AppExamsUsersEvalhSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = xxx
+        fields = '__all__'
+        
+class AppExamsUsersEvallSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = xxx
+        fields = '__all__'
+        
+class AppSentenceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = xxx
+        fields = '__all__'
+        
+class AppSentenceImagesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = xxx
+        fields = '__all__'
+        
+class AppSentencesUsersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = xxx
+        fields = '__all__'
+        
+class AppSentenceTagsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = xxx
+        fields = '__all__'
+        
+class AppSentenceTimesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = xxx
+        fields = '__all__'
+        
+class AppSentenceTrSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = xxx
+        fields = '__all__'
+        
+class AppTagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = xxx
+        fields = '__all__'
+        
+class BaseLanguageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = xxx
+        fields = '__all__'
+        
+class BaseLanguageLangSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = xxx
+        fields = '__all__'
+        
+class BaseUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = xxx
+        fields = '__all__'
+        
+class BaseUserArraySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = xxx
+        fields = '__all__'
+        
+class TemplateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = xxx
+        fields = '__all__'
+        
+class TemplateArraySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = xxx
+        fields = '__all__'
+        
+class VersionDbSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = xxx
+        fields = '__all__'
