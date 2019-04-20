fd42b8c9babddc2e928ce5cf6fd02a7118db51db
Eduardo A.F <eacevedof@yahoo.es>
2019-04-20 17:02:26 +0200

    configuro la app en settings.py

--- apirest/learnlang/settings.py
+++ apirest/learnlang/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'learnlang.base_user',
 ]
 
 MIDDLEWARE = [
@@ -74,10 +75,7 @@ WSGI_APPLICATION = 'learnlang.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+
 }
 
 
