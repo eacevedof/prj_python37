a6d093377f6dfedf8d7c990c9842dd903cdc9fb1
Eduardo A.F <eacevedof@yahoo.es>
2019-04-20 22:30:21 +0200

    cambio el nombre de la app

--- apirest/learnlang/base_user/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BaseUserConfig(AppConfig):
-    name = 'base_user'
---
+++ apirest/learnlang/theapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TheappConfig(AppConfig):
+    name = 'theapp'
