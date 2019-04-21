cc89c033094ba04d2a85b7cb7d6f7c802dc10aed
Eduardo A.F <eacevedof@yahoo.es>
2019-04-21 17:32:42 +0200

    parece que ya se guarda bien

--- apirest/utils/utils.py
+++ apirest/utils/utils.py
@@ -1,10 +1,11 @@
 # utils\utils.py
+import random
 from datetime import datetime
 import uuid
 
 def get_now():
     strnow = datetime.now()
-    strtoday = "{}{}{}{}{}{}".format(
+    strtoday = "{}{:02d}{:02d}{:02d}{:02d}{:02d}".format(
         strnow.year, strnow.month, strnow.day, 
         strnow.hour, strnow.minute, strnow.second
     )
@@ -13,3 +14,25 @@ def get_now():
 
 def get_uuid():
     return str(uuid.uuid4())
+
+def get_platform():
+    """
+    de base_array: 
+    1 by user on db
+    2 dts
+    3 backoffice
+    4 mobile
+    """
+    return str(random.randint(1,4))
+
+
+def get_session_user(isint=1):
+    if not isint:
+        return str(random.randint(1,10))
+    return random.randint(1,10)
+ 
+
+def get_cruhist():
+    lstactions = ["created","modified","deleted"]
+    i = random.randint(0,len(lstactions)-1)
+    return lstactions[i]
