142844449bf763eddb98384afd9b7850ef0bd0e4
Eduardo A.F <eacevedof@yahoo.es>
2019-04-21 15:23:13 +0200

    utils\utils.py

--- apirest/utils/utils.py
+++ apirest/utils/utils.py
@@ -1,6 +1,11 @@
+# utils\utils.py
 from datetime import datetime
+import uuid
 
 def get_now():
     strnow = datetime.now()
     strtoday = "{}{}{}{}{}{}".format(strnow.day,strnow.month,strnow.year,strnow.hour,strnow.minute,strnow.second)
     return strtoday
+
+def get_uuid():
+    return str(uuid.uuid4())
