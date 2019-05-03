9575858e0efce5648387bb61510cc943bc43df31
Eduardo A.F <eacevedof@yahoo.es>
2019-05-03 11:37:03 +0200

    probando crear campos custom

--- apirest/theapp/fields.py
+++ apirest/theapp/fields.py
@@ -3,16 +3,38 @@ from django.db import models
 class TheappBooleanField(models.BooleanField):
 
     def from_db_value(self, value, expression, connection, context):
-        pr(value,"TheappBooleanField.from_db_value.value")
         if value is None:
             return value
-        return int(value) # return 0/1
+        return int(0) # return 0/1
 
     def to_python(self, value):
-        pr(value,"TheappBooleanField.to_python.value")
+
         if value in ('Y', '1', True):
             return "1"
         if value in ('N', '0', False):
             return "0"
+
         raise ValueError("valueerror: {}".format(value))
 
+    def get_prep_value(self, value):
+        if value:
+            return '1'
+        else:
+            return '0'
+
+class HandField(models.Field):
+
+    description = _("String (up to %(max_length)s)")
+
+    def __init__(self, *args, **kwargs):
+        kwargs['max_length'] = 104
+        super().__init__(*args, **kwargs)
+
+    def deconstruct(self):
+        name, path, args, kwargs = super().deconstruct()
+        # Only include kwarg if it's not the default
+        # if self.separator != ",":
+        #    kwargs['separator'] = self.separator
+        return name, path, args, kwargs        
+
+    
