38d9025a1805df61e2e5f1a8b4c5d67a3d444cd5
Eduardo A.F <eacevedof@yahoo.es>
2019-05-06 21:13:01 +0200

    manage.py

--- apirest/manage.py
+++ apirest/manage.py
@@ -19,3 +19,22 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+"""
+#!/usr/bin/env python3.7                                                                                                              
+import os
+import sys
+
+if __name__ == '__main__':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'apirest.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+         raise ImportError(
+             "Couldn't import Django. Are you sure it's installed and "
+             "available on your PYTHONPATH environment variable? Did you "
+             "forget to activate a virtual environment?"
+         ) from exc
+     execute_from_command_line(sys.argv)
+                                            
+"""
