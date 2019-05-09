ddc90d02aec544ee53cc7bf5dd40cb27dacc0fc5
Eduardo A.F <eacevedof@yahoo.es>
2019-05-09 22:20:53 +0200

    carpeta media

--- apirest/learnlang/config/dev.py
+++ apirest/learnlang/config/dev.py
@@ -14,9 +14,9 @@ if DEBUG:
         'debug_toolbar',
     ]
 
-    MIDDLEWARE += [
+    MIDDLEWARE += (
         'debug_toolbar.middleware.DebugToolbarMiddleware',
-    ]
+    )
 
     INTERNAL_IPS = ('127.0.0.1', )
 
