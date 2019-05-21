8c3d3186fc707ab9eb678e3250eef53a7f473ba1
Eduardo A.F <eacevedof@yahoo.es>
2019-05-21 22:08:38 +0200

    suma de polinomios

--- apirest/pythonexpert/datamodel/data-model.py
+++ apirest/pythonexpert/datamodel/data-model.py
@@ -10,6 +10,10 @@ class Polynomial():
     def __add__(self,other):
         return Polynomial(*(x+y for x,y in zip(self.coeffs, other.coeffs)))
 
+    def __len__(self):
+        return len(self.coeffs)
+
+        
 p1 = Polynomial(1,2,3)  
 p2 = Polynomial(3,4,3)
 
