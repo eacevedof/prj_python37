50f8b95e01ce56bc691dcec1b3bf82c090a35604
Eduardo A.F <eacevedof@yahoo.es>
2019-06-03 22:23:53 +0200

    antes de empezar con metaclass __init_subclass__

--- pythonexpert/metaclasses/library.py
+++ pythonexpert/metaclasses/library.py
@@ -1,7 +1,7 @@
 # library.py
 class BaseMeta(type):
     def __new__(cls, name, bases, body):
-        if not 'bar' in body:
+        if name!="Base" and not 'bar' in body:
             raise TypeError("Bad user class")
 
         print("BaseMeta.__new__",cls,name,bases,body)
@@ -11,3 +11,6 @@ class Base(metaclass=BaseMeta):
     def foo(self):
         return self.bar()
 
+    def __init_subclass__(self,*a,**kw):
+        print("init_subclass",a,kw)
+        return super().__init_subclass__(*a,**kw)
