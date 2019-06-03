d0270de1b304bf4cbc17ffb4e552a6a6b1272b7c
Eduardo A.F <eacevedof@yahoo.es>
2019-06-03 21:08:18 +0200

    pythonexpert metaclasses Usando el hook `__build_class__` ejecucion

--- pythonexpert/metaclasses/library.py
+++ pythonexpert/metaclasses/library.py
@@ -2,3 +2,12 @@
 class Base:
     def foo(self):
         return self.bar
+
+old_bc = __build_class__
+
+def my_bc(*a,**kw):
+    print("my buildclass->",a,kw)
+    return old_bc(*a,**kw)
+
+import builtins
+builtins.__build_class__ = my_bc
