7e0c8cf6b1d2c0223380193e0754c88792b39181
Eduardo A.F <eacevedof@yahoo.es>
2019-06-03 22:09:08 +0200

    antes de empezar con metaclass rise typerror

--- pythonexpert/metaclasses/library.py
+++ pythonexpert/metaclasses/library.py
@@ -1,16 +1,13 @@
 # library.py
-class Base:
+class BaseMeta(type):
+    def __new__(cls, name, bases, body):
+        if not 'bar' in body:
+            raise TypeError("Bad user class")
+
+        print("BaseMeta.__new__",cls,name,bases,body)
+        return super().__new__(cls,name,bases,body)
+
+class Base(metaclass=BaseMeta):
     def foo(self):
-        return self.bar
+        return self.bar()
 
-old_bc = __build_class__
-
-def my_bc(fun,name,base=None,**kw):
-    if base is Base:
-        print("Check if bar method is defined")
-    if base is not None:
-        return old_bc(fun,name,base,**kw)
-    return old_bc(fun, name, **kw)
-
-import builtins
-builtins.__build_class__ = my_bc
