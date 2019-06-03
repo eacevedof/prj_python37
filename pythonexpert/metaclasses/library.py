7b2a0a1307fc0ef650a4b70a11e42831b28a4f26
Eduardo A.F <eacevedof@yahoo.es>
2019-06-03 21:26:13 +0200

    pythonexpert metaclasses patron con name,base, kw

--- pythonexpert/metaclasses/library.py
+++ pythonexpert/metaclasses/library.py
@@ -5,9 +5,12 @@ class Base:
 
 old_bc = __build_class__
 
-def my_bc(*a,**kw):
-    print("my buildclass->",a,kw)
-    return old_bc(*a,**kw)
+def my_bc(fun,name,base=None,**kw):
+    if base is Base:
+        print("Check if bar method is defined")
+    if base is not None:
+        return old_bc(fun,name,base,**kw)
+    return old_bc(fun, name, **kw)
 
 import builtins
 builtins.__build_class__ = my_bc
