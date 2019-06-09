9e56b9cd41a5006cdbe34b91ac9328003e7a9646
Eduardo A.F <eacevedof@yahoo.es>
2019-06-09 13:38:09 +0200

    hilos

--- mis_tests/misc/threads_hugo_nept.py
+++ mis_tests/misc/threads_hugo_nept.py
@@ -4,19 +4,25 @@
 import threading
 import time
 
-class MiHilo(threading.Trhead):
+class MiHilo(threading.Thread):
     
     def run(self):
-        print("{} inició".format(self.getName()))
+        print("{} run.inicio".format(self.getName()))
         time.sleep(2)
-        print("{} terminado".format(self.getName())
-
+        print("{} run.terminado desp 2 sec".format(self.getName()))
 
 if __name__=="__main__":
     for x in range(4):
-        objhilo = MiHilo(name="Thread-{}".format(x+1))
+        objhilo = MiHilo(name="Thread in for-{}".format(x+1))
         objhilo.start()
-        time.sleep(.5)
+        time.sleep(.1)
 
+"""
+tiempo   |-------|-------|-------|
+         0       1       2       3 
+hilo1    |---------------|
+hilo2     |---------------|
+hilo3      |---------------|
+hilo4       |---------------|
 
-
+"""
