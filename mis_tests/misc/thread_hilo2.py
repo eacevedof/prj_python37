56c9db2ca5362a556123c90c51d7688202420bb9
Eduardo A.F <eacevedof@yahoo.es>
2019-06-09 16:15:32 +0200

    hilos mitocode  import ok

--- mis_tests/misc/thread_hilo2.py
+++ mis_tests/misc/thread_hilo2.py
@@ -7,15 +7,16 @@ logging.basicConfig(level=logging.DEBUG, format='[%(levelname)s] (%(threadName)-
 class Hilo2(threading.Thread):
 
     def __init__(self, nombre_hilo,id_persona,data):
-        super.__init__(self,name=nombre_hilo, target=Hilo2.run)
+        # super.__init__(self,name=nombre_hilo, target=Hilo2.run)
+        threading.Thread.__init__(self, name=nombre_hilo,target=Hilo2.run)
         self.nombre_hilo = nombre_hilo
         self.id_persona = id_persona
         self.data = data
 
     def run(self):
-        self.guardar(self.id_persona)
+        self.guardar(self.id_persona, self.data)
 
-    def guardar(self, id_persona):
+    def guardar(self, id_persona,data):
         logging.debug("guardando para el id "+str(id_persona)+" la data: "+data)
         time.sleep(2)
         return
