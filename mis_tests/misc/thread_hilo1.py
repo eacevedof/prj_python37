e9c6231530e6121989ff70f75e3cbaaf167a35dc
Eduardo A.F <eacevedof@yahoo.es>
2019-06-09 15:53:45 +0200

    hilos mitocode NameError: name 'Hilo1' is not defined

---
+++ mis_tests/misc/thread_hilo1.py
@@ -0,0 +1,22 @@
+import threading
+import time
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='[%(levelname)s] (%(threadName)-s) %(message)s')
+
+class Hilo1(threading.Thread):
+
+    #def __init__(self, group, target, name, args, kwargs, daemon):
+    #    return super().__init__(group, target, name, args, kwargs, daemon)
+    def __init__(self, nombre_hilo,id_persona,data):
+        self.nombre_hilo = nombre_hilo
+        self.id_persona = id_persona
+        self.data = data
+
+    def run(self):
+        self.consultar(self.id_persona)
+
+    def consultar(self, id_persona):
+        logging.debug("consultando para el id "+str(id_persona))
+        time.sleep(2)
+        return
