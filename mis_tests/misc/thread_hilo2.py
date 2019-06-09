1db99b85a333a7f20bf789e4f2825d06fc75f8a4
Eduardo A.F <eacevedof@yahoo.es>
2019-06-09 16:00:34 +0200

    hilos mitocode no me va el import de las clases

--- mis_tests/misc/thread_hilo2.py
+++ mis_tests/misc/thread_hilo2.py
@@ -7,6 +7,7 @@ logging.basicConfig(level=logging.DEBUG, format='[%(levelname)s] (%(threadName)-
 class Hilo2(threading.Thread):
 
     def __init__(self, nombre_hilo,id_persona,data):
+        super.__init__(self,name=nombre_hilo, target=Hilo2.run)
         self.nombre_hilo = nombre_hilo
         self.id_persona = id_persona
         self.data = data
@@ -18,3 +19,4 @@ class Hilo2(threading.Thread):
         logging.debug("guardando para el id "+str(id_persona)+" la data: "+data)
         time.sleep(2)
         return
+
