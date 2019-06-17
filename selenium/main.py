695cd3aba53ebecf7eaeb134f3d0ef910429d565
Eduardo A.F <eacevedof@yahoo.es>
2019-06-17 22:39:00 +0200

    selenium arranca chrome y hace submit

--- selenium/main.py
+++ selenium/main.py
@@ -1,4 +1,15 @@
 # main.py
+import time
 from selenium import webdriver
 
 objdriver = webdriver.Chrome("chromedriver.exe")
+objdriver.get("http://newtours.demoaut.com")
+time.sleep(5)
+user_box = objdriver.find_element_by_name("userName")
+pass_box = objdriver.find_element_by_name("password")
+submit_button = objdriver.find_element_by_name("login")
+user_box.send_keys("test")
+pass_box.send_keys("test")
+submit_button.click()
+time.sleep(5)
+objdriver.quit()
