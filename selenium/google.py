8b32a38ef95f0dac78b3ed37a725a9ec1bedc912
Eduardo A.F <eacevedof@yahoo.es>
2019-06-17 23:08:27 +0200

    selenium Capítulo 4 localizando por id y name fin

---
+++ selenium/google.py
@@ -0,0 +1,11 @@
+# google.py
+# https://youtu.be/YLYNThOaP9w?list=PLjM3-neCG6qx4RFeq2X-TpWS_tJTk1qZP
+import time
+from selenium import webdriver
+
+driver = webdriver.Chrome("chromedriver.exe")
+driver.get("http://www.google.com")
+time.sleep(2)
+# searchbox = driver.find_element_by_id("lst-ib")
+searchbox = driver.find_element_by_name("q")
+searchbox.send_keys("Perú")
