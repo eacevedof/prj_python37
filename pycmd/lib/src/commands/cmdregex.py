88e9707234aa434fb23249b59a34163199d82800
Eduardo A.F <eacevedof@yahoo.es>
2019-08-26 19:12:25 +0200

    ya hace una copia

--- pycmd/lib/src/commands/cmdregex.py
+++ pycmd/lib/src/commands/cmdregex.py
@@ -2,21 +2,74 @@
 import os
 import sys
 from pprint import pprint
+from pathlib import Path
 
 import click
+from contextlib import contextmanager
 
 #clic_group convierte a clients en otro decorador
 @click.group()
-def cmdgroup():
-    """Manages the clients lifecycle"""
+# esto se llamaría desde 
+# pycmd regex
+def regex():
+    """
+    trabaja con ficheros y hace busquedas guardando el resultado en otro fichero
+    """
     pass
 
-@cmdgroup.command()
+
+@contextmanager
+def _custom_open(filename, opt="r"):
+    f = open(filename, opt)
+    try:
+        yield f
+    finally:
+        f.close()
+
+@regex.command()
+@click.pass_context
+def matchfile(context):
+    """Matchfile"""
+    click.echo("find pattern")
+    regex          = click.prompt("Reg. Exp", type=str, default="[\d]+love")
+    pathfilefrom   = click.prompt("Path file from", type=str, default="/path/of/origin/file.ext")
+    pathfileto     = click.prompt("Path file to", type=str, default="/path/of/destiny/file.ext")
+
+    with _custom_open(pathfilefrom) as f:
+        contents = f.read()
+
+    # print(contents)
+    if not os.path.isfile(pathfileto):
+        if not os.access(pathfileto, os.R_OK):
+            print("no access to "+pathfileto)
+            return 
+        else:
+            print("with access")
+            try:
+                f = open(pathfileto)
+                f.close()
+            except FileNotFoundError:
+                print("File "+pathfileto+" was not able to create")
+
+    print("trying to write")
+    with _custom_open(pathfileto,"w") as f:
+        print("writing...")
+        try:
+            f.write(contents)
+        except Exception:
+            print("Error al escribir en "+pathfileto)
+
+    # C:\Users\ioedu\Desktop\tmp.js
+    # C:\Users\ioedu\Desktop\tmp_2.js
+    # C:\Users\ioedu\Desktop\temp.sql
+
+#def matchfile
+
+@regex.command()
 @click.option("-n","--name",type=str,prompt=True,help="The client's name")
 @click.option("-c","--company",type=str,prompt=True,help="The client's company")
 @click.option("-e","--email",type=str,prompt=True,help="The client's email")
 @click.option("-p","--position",type=str,prompt=True,help="The client's position")
-
 @click.pass_context
 def create(context,name,company,email,position):
     """Creates a new client"""
@@ -32,12 +85,12 @@ def create(context,name,company,email,position):
     # pprint(client_service)
     client_service.create_client(oClient)
 
-@cmdgroup.command()
+@regex.command()
 @click.pass_context
 def list(context):
     """List all clients"""
     oClientServ = ClientService(context.obj["clients_table"])
-    lstClients = oClientServ.list_cmdgroup()
+    lstClients = oClientServ.list_regex()
 
     print(" ID | NAME | COMPANY | EMAIL | POSITION")
     click.echo("*" * 100)
@@ -59,13 +112,13 @@ def _update_client_flow(oClient):
     oClient.position    = click.prompt("New position", type=str, default=oClient.position)
     return oClient
 
-@cmdgroup.command()
+@regex.command()
 @click.argument("client_uid",type=str)
 @click.pass_context
 def update(context,client_uid):
     """Updates a client"""
     oClientServ = ClientService(context.obj["clients_table"])
-    lstClient = oClientServ.list_cmdgroup()
+    lstClient = oClientServ.list_regex()
     # print(lstClient)
     # sys.exit()
     dicClient = [dicClient for dicClient in lstClient if dicClient["uid"] == client_uid]
@@ -81,4 +134,4 @@ def update(context,client_uid):
         click.echo("Client not found")
 
 # alloptions son todos los comandos (funciones)
-allfuncs = cmdgroup
+allfuncs = regex
