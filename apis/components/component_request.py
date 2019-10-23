6400d3c78f80d7574b2f5b00501cc248dcd05038
Eduardo A.F <eacevedof@yahoo.es>
2019-10-23 21:27:53 +0200

    refactor self._get_content

--- apis/components/component_request.py
+++ apis/components/component_request.py
@@ -12,40 +12,39 @@ class ComponentRequest:
     self.login = login
     self.key = key
 
+  def _get_content(self,strurl):
+    objresp = requests.get(strurl)
+    dictjson = json.loads(objresp.content)    
+    return dictjson
+
   def get_list_folder(self):
-    print("get_list_folder")
+    print("\nget_list_folder:")
     strurl = "https://api.openload.co/1/file/listfolder?login={login}&key={key}"
     strurl = strurl.format(login=self.login,key=self.key)
 
-    objresp = requests.get(strurl)
-    dictjson = json.loads(objresp.content)
-    pprint(dictjson)
+    dictjson = self._get_content(strurl)
+    print(dictjson)
     return dictjson
   #get_list_folder
 
   def get_folder_content(self,ifolder):
-    print("get_folder_content")
+    print("\nget_folder_content:")
     strurl = "https://api.openload.co/1/file/listfolder?login={}&key={}&folder={}"
     strurl = strurl.format(self.login,self.key,ifolder)
-    objresp = requests.get(strurl)
-    dictjson = json.loads(objresp.content)
-    pprint(dictjson)
+    dictjson = self._get_content(strurl)
     return dictjson
   #get_folder_content
 
   def get_account_info(self):
-    print("get_account_info")
+    print("\nget_account_info:")
     strurl = "https://api.openload.co/1/account/info?login={login}&key={key}"
     strurl = strurl.format(login=self.login,key=self.key)
-    objresp = requests.get(strurl)
-    dictjson = json.loads(objresp.content)
-    pprint(dictjson)
+    dictjson = self._get_content(strurl)
     return dictjson
   # get_account_info
 
   def upload(self,pathlocal,ifolder):
-    print("upload")
-
+    print("\nupload:")
     BLOCKSIZE = 65536
     objsha1 = hashlib.sha1()
     with open(pathlocal, 'rb') as afile:
@@ -59,15 +58,19 @@ class ComponentRequest:
     strurl = "https://api.openload.co/1/file/ul?login={login}&key={key}&sha1={sha1}&folder=8306116"
     strurl = strurl.format(login=self.login, key=self.key, sha1=strsha1)
 
-    objresp = requests.get(url=strurl, headers={})
-    dicjson = objresp.json()
-
-    strurlup = dicjson["result"]["url"]
+    dictjson = self._get_content(strurl)
+    strurlup = dictjson["result"]["url"]
     # print(strurlup)
     objresp = requests.post(url=strurlup, headers={}, files={"file1":open(pathlocal,"rb"),})
     dictjson = json.loads(objresp.text)
     return dictjson
   # upload
+
+  def get_file_info(self,ifile):
+    strurl = "https://api.openload.co/1/file/info?file={file}&login={login}&key={key}"
+    strurl = strurl.format(ifile,self.login,self.key)
+
+  # get_file_info
 
 if __name__=="__main__":
   o = ComponentRequest()
