5fc144471935ad3aba7979885128d5f94c6293a7
Eduardo A.F <eacevedof@yahoo.es>
2019-10-24 22:50:11 +0200

    creo autoid db

--- apis/components/component_sqlite3.py
+++ apis/components/component_sqlite3.py
@@ -2,7 +2,7 @@ import os
 import sqlite3
 from sqlite3 import Error
 
-def create_connection(db_file=None):
+def get_connection(db_file=None):
     """ create a database connection to a SQLite database """
     if db_file is None:
         strthispath = os.path.join(os.path.dirname(__file__), '..')+"\openloadco\db_openloadco.db"
@@ -12,28 +12,30 @@ def create_connection(db_file=None):
     try:
         conn = sqlite3.connect(db_file)
         print(sqlite3.version)
+        return conn
     except Error as e:
         print("error")
         print(e)
-    finally:
-        if conn:
-            conn.close()
+
   
-def create_project(conn, project):
+def insert_loc_folder(conn, tplparams):
     """
     Create a new project into the projects table
     :param conn:
     :param project:
     :return: project id
     """
-    sql = ''' INSERT INTO projects(name,begin_date,end_date)
-              VALUES(?,?,?) '''
+    sql = ''' INSERT INTO loc_folder(fullpath,status)
+              VALUES(?,?) '''
     cur = conn.cursor()
-    cur.execute(sql, project)
+    cur.execute(sql,tplparams)
     return cur.lastrowid
-    
+
 if __name__ == '__main__':
     strthispath = os.path.join(os.path.dirname(__file__), '..')+"\openloadco\db_openloadco.db"
     strthispath = os.path.realpath(strthispath)
     #print(strthispath)
-    create_connection(strthispath)
+    objconx = get_connection(strthispath)
+    print(objconx)
+    strid = insert_loc_folder(objconx,("xx","yy",))
+    print(strid)
