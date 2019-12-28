782d9d2dc283a17a9b991233cd2a6cd4f51246d7
Eduardo A.F <eacevedof@yahoo.es>
2019-12-28 10:17:36 +0100

    skeleton v1 clase madre

--- skeleton/packa/moda0.py
+++ skeleton/packa/moda0.py
@@ -1,6 +1,18 @@
 print("moda0 imported")
+from pprint import pprint
 
 class Moda0:
+
 	def __init__(self,*a,**kw):
 		print("class Moda0",a,kw)
+		self.x = a[1]
+		self.y = a[2]
+		self.k1 = kw["k1"]
+		print(self.x,self.y,self.k1)
+
+
+if __name__ == "__main__":
+	o = Moda0("a","x","y",k1="v1")
+	pprint(o)
+
 
