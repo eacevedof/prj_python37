9745df4e73115931d59b1c5e35b76834f28617b8
Eduardo A.F <eacevedof@yahoo.es>
2019-12-28 13:42:50 +0100

    parece que sin pipenv funciona ^^

--- skeleton/packa/moda2.py
+++ skeleton/packa/moda2.py
@@ -1,12 +1,16 @@
 print("moda2 imported")
 from pprint import pprint
 from packa.moda0 import Moda0
+from packb.modb1 import ModB1
 
 class Moda2(Moda0):
 
 	def __init__(self,*a,**kw):
 		print("class Moda2",a,kw)
 		self.k1 = kw["k1"]
+
+	def showmodb1(self):
+		pprint(ModB1)
 
 if __name__ == "__main__":
 	o = Moda2("x2","y2",k1="m2")
--- skeleton/packb/modb1.py
+++ skeleton/packb/modb1.py
@@ -1,10 +1,23 @@
 print("modb1 imported")
 
 def func_b1():
-  print("func_b1")
+    print("func_b1")
 
 def func_b2():
-  print("func_b2")
+    print("func_b2")
 
 def func_b3():
-  print("func_b3")
+    print("func_b3")
+
+class ModB1:
+
+    def foo(self, x):
+        print "executing foo(%s, %s)" % (self, x)
+
+    @classmethod
+    def class_foo(cls, x):
+        print "executing class_foo(%s, %s)" % (cls, x)
+
+    @staticmethod
+    def static_foo(x):
+        print "executing static_foo(%s)" % x
