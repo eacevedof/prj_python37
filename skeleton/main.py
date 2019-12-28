cf9126c6c47810d231a4ecc6838d26a5c810be58
Eduardo A.F <eacevedof@yahoo.es>
2019-12-28 13:28:39 +0100

    en pipenv activado parece que ya funciona

--- skeleton/main.py
+++ skeleton/main.py
@@ -2,15 +2,15 @@
 print("main.py")
 
 from packa.moda1 import Moda1
-#from packa.moda2 import Moda2
+from packa.moda2 import Moda2
 
 from packb.modb1 import func_b1,func_b3
 from packc.modc1 import *
 
-# m1 = Moda1("x-1","y-2",k1="value-1")
-# m2 = Moda2("-","-",k1="value de moda 2")
-# m1.printself()
-# m2.printself()
+m1 = Moda1("x-1","y-2",k1="value-1")
+m2 = Moda2("-","-",k1="value de moda 2")
+m1.printself()
+m2.printself()
 
 func_b1()
 func_b3()
