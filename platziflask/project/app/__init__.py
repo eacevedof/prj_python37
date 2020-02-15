ba46e79195dd705658e2f868c9bc715e184b8162
ioedumac <eacevedof@yahoo.es>
2020-02-15 17:32:14 +0100

    22 - App Factory

---
+++ platziflask/project/app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from .config import Config
+
+def create_app():
+    app = Flask(__name__)
+    bootstrap = Bootstrap(app)
+    # se pasa a una clase de configuracion (config.py)
+    # app.config["SECRET_KEY"] = "SUPER SECRET KEY"
+    # con esto se cifra la info de la cookie
+    # esto habria que cambiarlo a un hash más seguro, para el ejemplo nos vale    
+    app.config.from_object(Config)
+    return app
