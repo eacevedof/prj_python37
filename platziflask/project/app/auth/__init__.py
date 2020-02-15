d56994ec03bfbca20344c5f2571e8c70fe9ab88e
ioedumac <eacevedof@yahoo.es>
2020-02-15 18:13:57 +0100

    23 - Uso de Blueprints

--- platziflask/project/app/auth/__init__.py
+++ platziflask/project/app/auth/__init__.py
@@ -1,1 +1,7 @@
 # project/app/auth/__init__.py
+from flask import Blueprint
+
+# todas las rutas que empiecen por /auth van a ser redirigidas a este blueprint
+auth = Blueprint("auth",__name__,url_prefix="/auth")
+
+from . import views
