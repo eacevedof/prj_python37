2e09edbcf84ca8355ce39d75a914698b11865576
ioedumac <eacevedof@yahoo.es>
2020-02-24 22:03:59 +0100

    creando controladores

---
+++ platziflask/project/app/controllers/base.py
@@ -0,0 +1,29 @@
+from pprint import pprint
+import sys
+
+from flask import request, make_response, redirect, render_template, session, redirect, url_for, flash
+
+class Session:
+    def get_value(self,strkey=""):
+        return session.get(strkey)
+    
+    def set_value(self,strkey="",mxvalue=None):
+        return session[strkey] = mxvalue
+
+class Base:
+
+    def __init__(self):
+        self.session = Session()
+
+    def get_get(self,strkey=""):
+        return request.args[strkey]
+
+    def get_post(self,strkey=""):
+        return request.form[strkey]
+    
+    def get_session(self, strkey=""):
+        return self.session.get_value(strkey)
+
+    def set_session(self, strkey="",mxvalue=None):
+        self.session.set_value(strkey=strkey,mxvalue=mxvalue)
+
