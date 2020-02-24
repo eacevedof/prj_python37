282b436e217ac3154b6d7fd2427d5a7aa0029ba1
ioedumac <eacevedof@yahoo.es>
2020-02-24 22:29:00 +0100

    funciona el index

--- platziflask/project/app/controllers/home.py
+++ platziflask/project/app/controllers/home.py
@@ -1,8 +1,32 @@
 from .base import Base
 
+from app.services.firestore import get_users, get_todos, put_todo, delete_todo, update_todo
+from app.forms.forms import TodoForm, DeleteTodoForm, UpdateTodoForm
+
 class Home(Base):
     def __init__(self):
         super().__init__()
         
     def index(self):
-        pass
+        user_ip = self.get_session("user_ip")
+        username = self.get_user_id()
+        frmtodo = TodoForm()
+        deleteform = DeleteTodoForm()
+        updateform = UpdateTodoForm()
+
+        context = {
+            "user_ip":user_ip,
+            "todos":get_todos(userid=username),
+            "username":username,
+            "todoform":frmtodo,
+            "deleteform":deleteform,
+            "updateform": updateform
+        }
+
+        if frmtodo.validate_on_submit():
+            put_todo(userid=username,description=todoform.description.data)
+            flash("tu tarea se creó con éxito")
+            return redirect(url_for("todo-list"))
+    
+        # spread operator
+        return render_template("todo-list.html",**context)
