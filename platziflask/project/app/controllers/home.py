4ca3e166f16ba1b27c1272128df7e45df80c7b4c
ioedumac <eacevedof@yahoo.es>
2020-02-26 22:35:04 +0100

    esto está peor

--- platziflask/project/app/controllers/home.py
+++ platziflask/project/app/controllers/home.py
@@ -1,11 +1,11 @@
 from .base import Base
 
-from app.services.firestore import get_users, get_todos, put_todo, delete_todo, update_todo
-from app.forms.forms import TodoForm, DeleteTodoForm, UpdateTodoForm
+# from app.services.firestore import get_users, get_todos, put_todo, delete_todo, update_todo
+# from app.forms.forms import TodoForm, DeleteTodoForm, UpdateTodoForm
 
 class Home(Base):
     def __init__(self):
         super().__init__()
         
     def index(self):
-        return render_template("index.html")
+        return self.render("index.html")
