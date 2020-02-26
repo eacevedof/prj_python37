e3f4237685756c92bc005e7f27e753c3eff856d3
ioedumac <eacevedof@yahoo.es>
2020-02-26 23:00:49 +0100

    esto está peor 2

--- platziflask/project/app/services/firestore.py
+++ platziflask/project/app/services/firestore.py
@@ -3,8 +3,12 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-credential = credentials.ApplicationDefault()
-firebase_admin.initialize_app(credential)
+try:
+    app = firebase_admin.get_app()
+except ValueError as e:
+    credential = credentials.ApplicationDefault()
+    firebase_admin.initialize_app(credential)
+
 
 db = firestore.client()
 
