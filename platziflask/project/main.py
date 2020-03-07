3b8d198e0fca495ce880e01d8132706b72efea49
ioedumac <eacevedof@yahoo.es>
2020-03-07 14:26:35 +0100

    incluyo builtins_ext

--- platziflask/project/main.py
+++ platziflask/project/main.py
@@ -1,46 +1,48 @@
 # project/main.py
 from flask_login import login_required
 import unittest 
-from pprint import pprint
 
-# from folder-app import __init__.py.def create_app
-from app import create_app
+from bootstrap.builtins_ext import *
+
+# from app.init 
+from app import get_flaskapp
 
 from app.controllers.home import Home
 from app.controllers.admin import Admin
 from app.controllers.todos import Todos
 from app.controllers.status import Status
 
-app = create_app()
+flaskapp = get_flaskapp()
 
-@app.route("/")
+@flaskapp.route("/")
 def index():
     return Home().index()
     
-@app.route("/todo-list",methods=["GET","POST"])
+@flaskapp.route("/todo-list",methods=["GET","POST"])
 @login_required
 def todo_list():
     return Admin().index()
     
-@app.route("/todos/delete/<todoid>",methods=["POST"])
+@flaskapp.route("/todos/delete/<todoid>",methods=["POST"])
 def delete(todoid):
     return Todos().delete(todoid)
 
-@app.route("/todos/update/<todoid>/<int:done>",methods=["POST"])
+@flaskapp.route("/todos/update/<todoid>/<int:done>",methods=["POST"])
 def update(todoid, done):
     return Todos().update(todoid,done)
 
-@app.errorhandler(404)
+@flaskapp.errorhandler(404)
 def not_found(error):
     return Status().error_404(error)
 
-@app.errorhandler(500)
+@flaskapp.errorhandler(500)
 def not_found(error):
     return Status().error_500(error)
 
 # se llamara con: flask test
-@app.cli.command()
+@flaskapp.cli.command()
 def test():
+    pr("start test():")
     import werkzeug
     werkzeug.cached_property = werkzeug.utils.cached_property
     # todo lo que este en la carpeta de project/test se ejecutara
