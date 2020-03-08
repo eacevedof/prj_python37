25f32e1d4d7be5440e2b41016d6d1dc212e98e79
ioedumac <eacevedof@yahoo.es>
2020-03-08 17:33:06 +0100

    refactor config y main, paso builtins a __init__

--- platziflask/project/bootstrap/config.py
+++ platziflask/project/bootstrap/config.py
@@ -1,5 +1,6 @@
-# project/app/config.py
-sc("project/app/config.py")
+# project/bootstrap/config.py
+sc("project/bootstrap/config.py")
+
 class Config:
     SECRET_KEY = "SUPER_SECRET"
     MAIL_SERVER = "smtp.gmail.com"
@@ -7,16 +8,5 @@ class Config:
     MAIL_USE_SSL = True
     MAIL_USERNAME = "username@gmail.com"
     MAIL_PASSWORD = "xome paxxworld"
-    # self.flaskapp.config["MAIL_USERNAME"] = "@gmail.com"
-    # self.flaskapp.config["MAIL_PASSWORD"] = ""
-
-class ProductionConfig(Config):
-    DATABASE_URI = "mysql://user@localhost/foo"
-
-class DevelopmentConfig(Config):
-    DEBUG = True
-
-class TestingConfig(Config):
-    TESTING = True
 
     
--- platziflask/project/bootstrap/config_testing.py
+++ platziflask/project/bootstrap/config_testing.py
@@ -1,20 +1,6 @@
-# project/app/config.py
-sc("project/app/config.py")
-class Config:
-    SECRET_KEY = "SUPER_SECRET"
-    MAIL_SERVER = "smtp.gmail.com"
-    MAIL_PORT = 465
-    MAIL_USE_SSL = True
-    MAIL_USERNAME = "username@gmail.com"
-    MAIL_PASSWORD = "xome paxxworld"
-    # self.flaskapp.config["MAIL_USERNAME"] = "@gmail.com"
-    # self.flaskapp.config["MAIL_PASSWORD"] = ""
-
-class ProductionConfig(Config):
-    DATABASE_URI = "mysql://user@localhost/foo"
-
-class DevelopmentConfig(Config):
-    DEBUG = True
+# project/bootstrap/config_testing.py
+sc("project/bootstrap/config_testing.py")
+from config import Config
 
 class TestingConfig(Config):
     TESTING = True
