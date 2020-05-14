9282d7504b2b5979ae54998571dedd117c8535ec
ioedumac <eacevedof@yahoo.es>
2020-05-14 17:15:37 +0200

    integrator ya conecta con mysql y hace insert

--- integrator/mysql/google_sheets.py
+++ integrator/mysql/google_sheets.py
@@ -10,5 +10,30 @@ def two():
     print(wks.get_worksheet(2).get_all_records())
     # print(wks.get_allrecrods())
 
+
+def mysql_check():
+    import mysql.connector
+
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        passwd="1234",
+        database="db_tinymarket"
+    )
+    
+    objcursor = mydb.cursor()
+    sql = """
+            INSERT INTO imp_products (code, description, description_full, price, price2, display) 
+            VALUES (%s, %s,%s, %s,%s, %s)
+          """
+    values = ("a","b","c","d","e","f")
+    objcursor.execute(sql,values)
+
+    sql = "SELECT * FROM imp_products"
+    objcursor.execute(sql)
+    res = objcursor.fetchall()
+    print(res)
+
 if __name__ == '__main__':
-    two()
+    # two()
+    mysql_check();
