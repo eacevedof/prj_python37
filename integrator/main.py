b5c497829642a96e63c688aa99ed979e07ef1222
ioedumac <eacevedof@yahoo.es>
2020-05-14 21:10:00 +0200

    integrator: main.py ejemplo que funciona mysql clase  con read y write

--- integrator/main.py
+++ integrator/main.py
@@ -1,1 +1,23 @@
-from googl.sheets import Sheets
+from googleserv.sheets import Sheets
+from mysqlserv.mysql import Mysql
+
+osheet = Sheets("temp_xls",2)
+#data = osheet.get_data()
+#print(data)
+
+omysql = Mysql()
+sql = "truncate table imp_products"
+rt = omysql.execute(sql=sql,tplval=None,w=1)
+print(rt)
+
+sql = """
+INSERT INTO imp_products (code, description, description_full, price, price2, display) 
+VALUES (%s, %s,%s, %s,%s, %s)
+"""
+values = ("a","b","c","d","e","f")
+omysql.execute(sql=sql, tplval=values, w=1)
+
+sql = "select * from imp_products where id!=%s"
+r = omysql.execute(sql,("0",))
+print(r)
+
---
+++ integrator/mysqlserv/mysql.py
@@ -0,0 +1,41 @@
+import mysql.connector
+import json
+
+class Mysql:
+    pathconfig = ""
+    conx = None
+
+    def __init__(self):
+        self.pathconfig = "./config/mysql.json"
+        print(self.pathconfig)
+
+    def _get_json(self):
+        data = {}
+        with open(self.pathconfig) as f:
+            data = json.load(f)
+        return data
+
+    def _get_cursor(self):
+        dbconfig = self._get_json()
+        # ** transforma un diccionario en kwargs
+        self.conx = mysql.connector.connect(**dbconfig)
+        objcursor = self.conx.cursor(dictionary=True)
+        return objcursor
+
+    def execute(self, sql, tplval,w=0):
+        try:
+            print(sql)
+            print(tplval)
+            objcursor = self._get_cursor()
+            r = objcursor.execute(sql,tplval)
+            if(w==0):
+                r = objcursor.fetchall()
+                print(r)
+            return r
+        except mysql.connector.Error as error:
+            print("Failed to get recor from mysql table: {}".format(error))
+            return -1
+        finally:
+            if(self.conx.is_connected()):
+                objcursor.close()
+                #print("mysql connection is closed")
