0b2b052d4f8a4590bffb4c438d9962dd44c21b0a
ioedumac <eacevedof@yahoo.es>
2020-05-15 16:06:07 +0200

    integrator: ya recupera datos de origen y contexto

--- integrator/etls/gsheet_mysql.py
+++ integrator/etls/gsheet_mysql.py
@@ -5,24 +5,39 @@ from mysqlserv.mysql import Mysql
 from fileserv.json import Json
 
 jsonhelper = Json()
-strpathctx = core.get_path_mapping("fromto.json")
-print(strpathctx)
-sys.exit()
 
-jsonhelper.set_pathfile(strpathctx)
+# configuración general de la ETL
+pathmapping = core.get_path_mapping("fromto.json")
+jsonhelper.set_pathfile(pathmapping)
 jsonhelper.load_data()
-print(jsonhelper.get_data())
-sys.exit()
+transconfig = jsonhelper.get_dictbykey("id","transfer-1")
+#print(transconfig); sys.exit()
 
-dictctx = jsonhelper.get_dictbykey("id","json1")
-infile = core.get_path_in(dictctx["path"]) 
-jsonhelper.set_pathfile(infile)
-products = jsonhelper.get_data()
+#obtengo el origen
+dictsource = transconfig["source"]["context"]
+destdatabase = transconfig["destiny"]["context"]["database"]
+# print(dictsource); print(destdatabase); sys.exit()
+pathcontext = core.get_path_context(dictsource["file"])
+jsonhelper.set_pathfile(pathcontext)
+jsonhelper.load_data()
+# print(jsonhelper.get_data()); sys.exit()
 
+dicsource = jsonhelper.get_dictbykey("id","json1")
+pathsource = core.get_path_in(dicsource["path"])
+jsonhelper.set_pathfile(pathsource)
+jsonhelper.load_data()
+#print(jsonhelper.get_data()); sys.exit()
 
+dictdestiny = transconfig["destiny"]["context"]
+pathdestiny = core.get_path_context(dictdestiny["file"])
+jsonhelper.set_pathfile(pathdestiny)
+jsonhelper.load_data()
+# print(jsonhelper.get_data()); sys.exit()
+dicconxcfg = jsonhelper.get_dictbykey("id","mysql1")
+# print(dicconxcfg); sys.exit()
+dbconfig = core.get_dbconfig(dicconxcfg,destdatabase)
+# print(dbconfig); sys.exit()
 
-print(products)
-sys.exit()
 
 
 def _get_mysql_field(sheetfield):
