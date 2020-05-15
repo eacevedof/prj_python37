51331dd322c8f61721ed9feabeff12e29f71f2e0
ioedumac <eacevedof@yahoo.es>
2020-05-15 13:26:18 +0200

    integrator obtiene datos con el core

--- integrator/etls/gsheet_mysql.py
+++ integrator/etls/gsheet_mysql.py
@@ -5,12 +5,12 @@ from mysqlserv.mysql import Mysql
 from fileserv.json import Json
 
 jsonhelper = Json()
-strpathctx = core.get_path_context("files.json")
+strpathctx = core.get_path_context("%contexts%/files.json")
 jsonhelper.set_pathfile(strpathctx)
 jsonhelper.load_data()
 dictctx = jsonhelper.get_dictbykey("id","json1")
 infile = core.get_path_in(dictctx["path"]) 
-jsonhelper.set_pathfile(infile);
+jsonhelper.set_pathfile(infile)
 products = jsonhelper.get_data()
 
 
