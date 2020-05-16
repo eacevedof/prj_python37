2c4f7c1a22557a571ed60c4232192c8f28b2a6a8
ioedumac <eacevedof@yahoo.es>
2020-05-16 11:55:56 +0200

    integrator: compila con modelos

--- integrator/core/etl.py
+++ integrator/core/etl.py
@@ -1,10 +1,9 @@
 import sys
 from pprint import pprint
 
-from core.core import Core as core
-from core.helpers.mysqlserv.mysql import Mysql
-from core.helpers.mysqlserv.querybuilder import QueryBuilder as qb
-from core.helpers.json import Json
+from core.models.context import Context
+from core.models.mapping import Mapping
+
 
 class Etl:
     def __init__(self, mappingfile, mappingid):
