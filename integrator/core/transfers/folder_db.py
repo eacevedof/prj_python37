ea36df9ad3a722442a327863e42bd27ad7c07856
ioedumac <eacevedof@yahoo.es>
2020-05-29 16:44:07 +0200

    integrator: ya devuelve los archivos de una carpeta a procesar

--- integrator/core/transfers/folder_db.py
+++ integrator/core/transfers/folder_db.py
@@ -16,34 +16,19 @@ class FolderDb:
         self._printattribs()
 
     def _printattribs(self):
-        # print("\nobjsource"); pprint(self.objsource.get_data())
-        # print("\nobjdestiny"); pprint(self.objdestiny.get_data()); sys.exit()
+        #print("\nobjsource"); pprint(self.objsource.get_data())
+        #print("\nobjdestiny"); pprint(self.objdestiny.get_data()); sys.exit()
         pass
 
     def _get_source_data(self):
         return self.objsource.get_context().get_content()
 
-    def _insert_by_rows(self,mysql,tabledest,mapfields,fromfields):
-        for row in self._get_source_data():
-            insert = {"keys":[],"values":[]}
-            for field in row:
-                if field in fromfields:
-                    insert["keys"].append(mapfields[field])
-                    insert["values"].append(row[field])
-            # print(insert)
-            qbsql = qb.get_insert_dict(tabledest, insert["keys"], insert["values"])
-            # print(qbsql);print("\n")
-            mysql.insert(qbsql)        
 
+    def _get_fields_pattern(self):
+        allfields = self.objdestiny.get_data()#["tables"]["fields"]
+        pprint(allfields["tables"])
+        sys.exit()
 
-
-    def _insert_by_table(self, mysql):
-        for tablecfg in self.objdestiny.get_tables():
-            #pprint(tablecfg);sys.exit()
-            tabledest = tablecfg["name"]
-            mapfields = tablecfg["fields"]
-            fromfields = list(mapfields.keys())
-            self._insert_by_rows(mysql, tabledest, mapfields, fromfields)
 
     def _run_queries(self, mysql):
         for sql in self.queries:
@@ -55,11 +40,23 @@ class FolderDb:
         source = self.objsource
         destiny = self.objdestiny
 
-        sourcedata = self._get_source_data()
-        print(sourcedata)
+        #print(source)
+        #print(destiny); sys.exit()
+        arfiles = self._get_source_data()
+        #print(arfiles)
+        self._get_fields_pattern()
+
+
+
+
+
+
+
         sys.exit()
 
         destmysql = Mysql(destiny.get_context().get_dbconfig())
+
+        
         print("...inserting into tables")
         self._insert_by_table(destmysql)
         print("...running extra queries")
