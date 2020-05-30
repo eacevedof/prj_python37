1e15fb568d94505e90bd1f2c2038399751914fcc
ioedumac <eacevedof@yahoo.es>
2020-05-30 13:59:11 +0200

    integrator elchalan.py

--- integrator/etls/elchalan/elchalan.py
+++ integrator/etls/elchalan/elchalan.py
@@ -1,13 +1,11 @@
 import sys
 from core.etl import Etl
 
-
-etl1 = Etl("elchalan.json","products-pictures-to-db")
-etl1.transfer()
-sys.exit()
-
 option = 1
 
+# =============================================
+#              SOURCE a IMP TABLE
+# =============================================
 if option==1:
     # lee config/mapping/<id>
     etl1 = Etl("elchalan.json","json-to-imp-products")
@@ -20,8 +18,17 @@ if option==1:
 if option==2:
     etl1 = Etl("elchalan.json","gsheet-to-imp-products")
 
-#etl1.transfer()
+etl1.transfer()
 
+# =============================================
+#              imagenes
+# =============================================
+etl1 = Etl("elchalan.json","products-pictures-to-db")
+etl1.transfer()
+
+# =============================================
+#              IMP a APP
+# =============================================
 etl1 = Etl("elchalan.json","transfer-imp-to-app")
 etl1.add_query("UPDATE app_product SET code_cache=uuid() WHERE code_cache IS NULL")
 etl1.add_query("""
@@ -32,5 +39,5 @@ AND LENGTH(code_cache)=36
 """)
 etl1.add_query("UPDATE app_product SET description_full = description WHERE description_full IS NULL")
 etl1.add_query("UPDATE app_product SET description = LOWER(description), description_full = LOWER(description_full)")
-#etl1.transfer()
+etl1.transfer()
 
