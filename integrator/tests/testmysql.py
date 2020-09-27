4829a579287552da8a4536303738271fb3f0605f
ioedumac <eacevedof@yahoo.es>
2020-09-27 16:48:34 +0200

    integrator: no me inserta en destino y no me da ningun error. El truncate va bien

---
+++ integrator/tests/testmysql.py
@@ -0,0 +1,10 @@
+import unittest
+from core.helpers.mysqlserv.mysql import Mysql
+
+class MysqlTest(unittest.TestCase):
+    def select(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
