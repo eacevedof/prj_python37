6a87fbcf01b00d523fd3e568e8a25d2223a60442
ioedumac <eacevedof@yahoo.es>
2020-09-27 17:29:35 +0200

    pasando bootrstrap hack a init.py de tests no funciona

--- integrator/tests/bootstrap.py
+++ integrator/tests/bootstrap.py
@@ -1,3 +1,12 @@
 print("bootstrap.py hack core loader")
 # agrega el dicrectorio tests a la ruta de sistema de modo que este pueda ver los otros módulos
 
+# hack para importar 
+#if __name__ == "__main__" and __package__ is None:
+import sys
+from sys import path
+from os.path import dirname as dir
+
+# print(path[0]); sys.exit()
+path.append(dir(path[0]))
+# __package__ = "core"
