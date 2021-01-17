37dd687836409b357479d1f86c4c5cb51b956004
ioedumac <eacevedof@yahoo.es>
2021-01-17 22:41:28 +0100

    no levanta con python3.7

--- pyprint/docker/entrypoint.sh
+++ pyprint/docker/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 echo "im /usr/src/entrypoint.sh"
-python /usr/src/pyprint/server.py
+#python /usr/src/pyprint/server.py
 #tail -f /dev/null
+uvicorn app.main:app --host 0.0.0.0 --port  8080
