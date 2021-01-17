ef4ae98c15b516f203c72c919d6dd7fc85c02547
ioedumac <eacevedof@yahoo.es>
2021-01-17 16:25:44 +0100

    os. has no attribute startfile

--- pyprint/pyprint/main.py
+++ pyprint/pyprint/main.py
@@ -1,41 +1,43 @@
 import os
 import ctypes
-import subprocess
+from subprocess import PIPE, Popen
 from pprint import pprint
 
 # https://askubuntu.com/questions/416995/how-to-list-all-available-printers-from-terminal
 
-
 def exec(cmd):
-    proc = subprocess.Popen([cmd],stdout=subprocess.PIPE, shell=True)
-    (out, err) = proc.communicate()
-    pprint(proc)
-    print(out)
-    print(err)
-    return out
+    process = Popen(
+        args=cmd,
+        stdout=PIPE,
+        shell=True
+    )
+    return process.communicate()[0]
 
 def printit():
+    print("printing....")
     pathfile = "./to-print.txt"
-    printer = ""
+    printer = "Brother_DCP_1610W_series"
     os.startfile(pathfile, printer)
 
 def show_printers():
     cmd = "lpstat -p | awk '{print $2}'"
     r = os.popen(cmd).read()
-    pprint(r)
-
-
-def show_pool():
-    cmd = "lpstat -p | awk '{print $2}' | xargs -n1 lpq -P"
-    r = exec(cmd)
     print(r)
 
 def show_printers2():
+    print("show printers 2:\n")
     cmd = "lpstat -p | awk '{print $2}'"
     r = exec(cmd)
     print(r)
 
+def show_pool():
+    print("show pool:\n")
+    cmd = "lpstat -p | awk '{print $2}' | xargs -n1 lpq -P"
+    r = exec(cmd)
+    print(r)
 
 if __name__ == "__main__":
-    # show_printers()
+    show_printers()
     show_printers2()
+    show_pool()
+    printit()
