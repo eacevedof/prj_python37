35f7bfba3b78f1bcd92f66bd7712602fa95a1a7a
ioedumac <eacevedof@yahoo.es>
2021-01-17 16:06:35 +0100

    con funcion exec(

--- pyprint/pyprint/main.py
+++ pyprint/pyprint/main.py
@@ -3,6 +3,16 @@ import ctypes
 import subprocess
 from pprint import pprint
 
+# https://askubuntu.com/questions/416995/how-to-list-all-available-printers-from-terminal
+
+
+def exec(cmd):
+    proc = subprocess.Popen([cmd],stdout=subprocess.PIPE, shell=True)
+    (out, err) = proc.communicate()
+    pprint(proc)
+    print(out)
+    print(err)
+    return out
 
 def printit():
     pathfile = "./to-print.txt"
@@ -15,13 +25,16 @@ def show_printers():
     pprint(r)
 
 
+def show_pool():
+    cmd = "lpstat -p | awk '{print $2}' | xargs -n1 lpq -P"
+    r = exec(cmd)
+    print(r)
+
 def show_printers2():
     cmd = "lpstat -p | awk '{print $2}'"
-    proc = subprocess.Popen([cmd],stdout=subprocess.PIPE, shell=True)
-    (out, err) = proc.communicate()
-    pprint(proc)
-    print(out)
-    print(err)
+    r = exec(cmd)
+    print(r)
+
 
 if __name__ == "__main__":
     # show_printers()
