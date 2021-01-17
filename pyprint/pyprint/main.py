c49259af60456f92ead07e969300177d4359c482
ioedumac <eacevedof@yahoo.es>
2021-01-17 15:58:19 +0100

    prueba 1 con os.system pero mejor hay que usar subprocess

--- pyprint/pyprint/main.py
+++ pyprint/pyprint/main.py
@@ -1,3 +1,16 @@
 import os
+import ctypes
 
-os.startfile("")
+
+def printit():
+    pathfile = "./to-print.txt"
+    printer = ""
+    os.startfile(pathfile, printer)
+
+def show_printers():
+    cmd = "lpstat -p | awk '{print $2}'"
+    os.system(cmd)
+
+
+if __name__ == "__main__":
+    show_printers()
