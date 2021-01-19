4f0848a78aeb55e2e2f65258e726b8be1896c66b
ioedumac <eacevedof@yahoo.es>
2021-01-19 23:20:24 +0100

    ya levanta

--- pyprint/docker/entrypoint.sh
+++ pyprint/docker/entrypoint.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 echo "im /usr/src/entrypoint.sh"
 #python /usr/src/pyprint/server.py
 #tail -f /dev/null
-uvicorn app.main:app --host 0.0.0.0 --port  8080
+#uvicorn app.main:app --host 0.0.0.0 --port  8080
