88c8b253fe48b4b78dc5c3807e0180d8762c2fa4
ioedumac <eacevedof@yahoo.es>
2021-01-21 23:58:43 +0100

    ya se queda levantado pero no por el 8080

--- pyprint/docker/entrypoint.sh
+++ pyprint/docker/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 echo "im /usr/src/entrypoint.sh"
+pip install fastapi
+pip install uvicorn[standard]
+
+uvicorn main:app --reload
 #python /usr/src/pyprint/server.py
 #tail -f /dev/null
 #uvicorn app.main:app --host 0.0.0.0 --port  8080
