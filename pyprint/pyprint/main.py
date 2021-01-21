e1da06247fbf51d4edd1d91e39dd95a5f2457e16
ioedumac <eacevedof@yahoo.es>
2021-01-21 23:58:43 +0100

    ya se queda levantado pero no por el 8080

--- pyprint/pyprint/main.py
+++ pyprint/pyprint/main.py
@@ -1,50 +1,13 @@
-import os
-import ctypes
-from subprocess import PIPE, Popen
-from pprint import pprint
+from typing import Optional
+from fastapi import FastAPI
 
-#Common UNIX Printing System (CUPS)
-# https://askubuntu.com/questions/416995/how-to-list-all-available-printers-from-terminal
-# libreria en python (python-escpos)
-# https://github.com/python-escpos/python-escpos
-# https://stackoverflow.com/questions/24225647/docker-a-way-to-give-access-to-a-host-usb-or-serial-device
+app = FastAPI()
 
-def exec(cmd):
-    process = Popen(
-        args=cmd,
-        stdout=PIPE,
-        shell=True
-    )
-    return process.communicate()[0]
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
-def printit():
-    print("printing....")
-    pathfile = "./to-print.txt"
-    printer = "Brother_DCP_1610W_series"
-    # os.startfile(pathfile, printer) # error
-    cmd = f"lpr -P {printer} {pathfile}"
-    r = exec(cmd)
-    print(r)
 
-def show_printers():
-    cmd = "lpstat -p | awk '{print $2}'"
-    r = os.popen(cmd).read()
-    print(r)
-
-def show_printers2():
-    print("show printers 2:\n")
-    cmd = "lpstat -p | awk '{print $2}'"
-    r = exec(cmd)
-    print(r)
-
-def show_pool():
-    print("show pool:\n")
-    cmd = "lpstat -p | awk '{print $2}' | xargs -n1 lpq -P"
-    r = exec(cmd)
-    print(r)
-
-if __name__ == "__main__":
-    show_printers()
-    show_printers2()
-    show_pool()
-    printit()
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
