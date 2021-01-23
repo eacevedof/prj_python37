244f7740d053f656ba50fbafa65e9dff8f0d8cb7
ioedumac <eacevedof@yahoo.es>
2021-01-23 19:51:58 +0100

    limpiando entrypoint.sh

--- pyprint/docker/entrypoint.sh
+++ pyprint/docker/entrypoint.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-echo "im /usr/src/entrypoint.sh"
 pip install fastapi
 pip install uvicorn[standard]
-
 uvicorn main:app --reload --host 0.0.0.0 --port 8080
-#python /usr/src/pyprint/server.py
-#tail -f /dev/null
-#uvicorn app.main:app --host 0.0.0.0 --port  8080
