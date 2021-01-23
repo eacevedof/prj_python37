1816b6e9a32e1d6c5b91b8c1d100f2896c2d2601
ioedumac <eacevedof@yahoo.es>
2021-01-23 22:58:03 +0100

    importo routes.routes

--- pyprint/pyprint/src/boot/fastapi.py
+++ pyprint/pyprint/src/boot/fastapi.py
@@ -1,11 +1,14 @@
 from typing import Optional
 from fastapi import FastAPI
+
+from src.routes.routes import routes
 from src.controllers.home_controller import HomeController
 
 app = FastAPI()
 
 @app.get("/")
 def home():
+    return {"routes": routes}
     return (HomeController()).index()
 
 # para que llegue q hay que enviar todo con cabecer accpet application/json
