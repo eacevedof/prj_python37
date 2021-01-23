56b73fda27efdac46de725191d3fdacc887a1bf0
ioedumac <eacevedof@yahoo.es>
2021-01-23 22:29:54 +0100

    prueba get ok

--- pyprint/pyprint/src/controllers/home_controller.py
+++ pyprint/pyprint/src/controllers/home_controller.py
@@ -1,10 +1,6 @@
 from src.services.home_service import HomeService
-import sys
-import os
 
 class HomeController:
-    def __init__(self):
-        pass
 
     def index(self):
         r = (HomeService()).get_index()
