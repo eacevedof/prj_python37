868735ca5b4b1b5b2e1a230020c29c8dcf19f950
ioedumac <eacevedof@yahoo.es>
2021-01-23 21:33:21 +0100

    moviendo fastapi a otro archivo

--- pyprint/pyprint/public/main.py
+++ pyprint/pyprint/public/main.py
@@ -1,5 +1,3 @@
-from typing import Optional
-from fastapi import FastAPI
 import sys
 import os
 
@@ -9,7 +7,6 @@ sys.path.append(pathsrc)
 #print("pathsrc:"+pathsrc)
 # sys.exit()
 #sys.path.append(pathsrc)
-
 #sys.exit()
 """
 /app/public
@@ -19,23 +16,4 @@ sys.path.append(pathsrc)
 /usr/local/lib/python3.8/lib-dynload
 /usr/local/lib/python3.8/site-packages
 """
-
-from src.controllers.home_controller import HomeController
-
-app = FastAPI()
-
-@app.get("/")
-def read_root():
-    ctrl = HomeController()
-    return ctrl.index()
-    paths = []
-    for p in sys.path:
-        p = os.path.realpath(p)
-        paths.append(p)
-
-    return {"hola":"Python Print","paths":paths}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+from src.boot.fastapi import app
