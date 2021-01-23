53c79c570d3c72d7805a0e3d87aa74496f3a4877
ioedumac <eacevedof@yahoo.es>
2021-01-23 21:33:21 +0100

    moviendo fastapi a otro archivo

---
+++ pyprint/pyprint/src/boot/fastapi.py
@@ -0,0 +1,24 @@
+from pprint import pprint
+pprint("fastapi")
+
+from typing import Optional
+from fastapi import FastAPI
+from src.controllers.home_controller import HomeController
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    ctrl = HomeController()
+    return ctrl.index()
+    paths = []
+    for p in sys.path:
+        p = os.path.realpath(p)
+        paths.append(p)
+
+    return {"hola":"Python Print","paths":paths}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
