32d4e7a36fd57f06f8b9926be227bb69e1699adb
ioedumac <eacevedof@yahoo.es>
2021-01-23 20:06:34 +0100

    rutas

--- pyprint/pyprint/public/main.py
+++ pyprint/pyprint/public/main.py
@@ -1,6 +1,22 @@
 from typing import Optional
 from fastapi import FastAPI
-from src.controllers.home_controller import HomeController
+import sys
+
+"""
+for p in sys.path:
+    print(p)
+
+sys.exit()
+.
+/usr/local/bin
+/usr/local/lib/python38.zip
+/usr/local/lib/python3.8
+/usr/local/lib/python3.8/lib-dynload
+/usr/local/lib/python3.8/site-packages
+"""
+
+
+from ..src.controllers.home_controller import HomeController
 
 app = FastAPI()
 
