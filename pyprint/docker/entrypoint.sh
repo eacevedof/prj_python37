39881cd97e37b5b954b0a7abe6276f5544449b41
ioedumac <eacevedof@yahoo.es>
2021-01-24 20:02:49 +0100

    parece que esto puede funcionar con ayuda de debuger.py

--- pyprint/docker/entrypoint.sh
+++ pyprint/docker/entrypoint.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 python -m pip install --upgrade pip
+
+pip install debugpy
 pip install fastapi
 pip install uvicorn[standard]
-pip install debugpy
+#pip install debugpy -t /tmp
 
+#python -m debugpy --listen 0.0.0.0:5678 --wait-for-client -m uvicorn
 #python -m debugpy --listen 0.0.0.0:5678 --wait-for-client -m public/main.py
-python -m debugpy --listen 0.0.0.0:5678 --wait-for-client
+#python /tmp/debugpy --wait-for-client --listen 0.0.0.0:5678 /pyprint/public/main.py
+
+#python -m debugpy --listen 0.0.0.0:5678 --wait-for-client -m src
+
+#python /tmp/debugpy --wait-for-client --listen 0.0.0.0:5678 -m uvicorn
+
+#python /tmp/debugpy --wait-for-client --listen 0.0.0.0:5678 -m uvicorn main:app 
+
 #https://www.uvicorn.org/settings/
 cd public; uvicorn main:app --reload --host 0.0.0.0 --port 8080 --reload-dir "../src"
+#python public/main.py
