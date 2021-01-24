b577d1c5207094f6262cb2758db903a1ac5644bf
ioedumac <eacevedof@yahoo.es>
2021-01-24 22:27:55 +0100

    todo ok

--- pyprint/docker/entrypoint.sh
+++ pyprint/docker/entrypoint.sh
@@ -17,6 +17,11 @@ pip install uvicorn[standard]
 #python /tmp/debugpy --wait-for-client --listen 0.0.0.0:5678 -m uvicorn main:app 
 
 #https://www.uvicorn.org/settings/
-#cd public; uvicorn main:app --reload --host 0.0.0.0 --port 8080 --reload-dir "../src"
+cd public; uvicorn main:app --reload --host 0.0.0.0 --port 8080 --reload-dir "../src"
 
-python public/main.py
+# python public/main.py
+
+# python -m public/main.py
+
+# esta llamada permite reconocer src como módulo pero no me sirve ya que es solo para debug
+# python -m public.main 
--- pyprint/pyprint/public/main.py
+++ pyprint/pyprint/public/main.py
@@ -1,7 +1,11 @@
+#import uvicorn
+
 import sys
 import os
 
 
+# cuando se usa -m public.main
+#from .debug import debugpy_start
 from debug import debugpy_start
 
 debugpy_start()
@@ -9,7 +13,11 @@ debugpy_start()
 pathsrc = os.path.abspath("..")
 sys.path.append(pathsrc)
 from src.routes.all import *
+#from src.boot.fastapi import app
+
 
 
 if __name__=="__main__":
-    uvicorn.run("app.app:app",host='0.0.0.0', port=8080, reload=True, debug=True, workers=3)
+    pass
+    #print("running uvicorn :)")
+    #uvicorn.run(app, host='0.0.0.0', port=8080, reload=True, debug=True, workers=3, log_level="info")
