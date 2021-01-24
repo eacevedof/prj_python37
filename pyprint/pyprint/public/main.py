b948c8b24ba23b1be0e7656eee3d981ebf86553e
ioedumac <eacevedof@yahoo.es>
2021-01-24 20:02:49 +0100

    parece que esto puede funcionar con ayuda de debuger.py

--- pyprint/pyprint/public/main.py
+++ pyprint/pyprint/public/main.py
@@ -1,6 +1,10 @@
 import sys
 import os
 
+from debuger import initialize_flask_server_debugger_if_needed
+
+initialize_flask_server_debugger_if_needed()
+
 pathsrc = os.path.abspath("..")
 sys.path.append(pathsrc)
 from src.routes.all import *
