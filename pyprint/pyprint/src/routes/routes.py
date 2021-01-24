f282f88681191a2b21fe66eccdfc438508abaaee
ioedumac <eacevedof@yahoo.es>
2021-01-24 12:33:12 +0100

    ya se muestra todo con refactor

--- pyprint/pyprint/src/routes/routes.py
+++
@@ -1,4 +0,0 @@
-routes = [
-    {"url":"/","controller":"src.controllers.home_controller","method":"index"},
-    {"url":"/prueba/slug_x","controller":"src.controllers.home_controller","method":"get_test"},
-]
