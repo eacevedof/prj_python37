8afaf9824881a9eb9001e54664581554003df131
ioedumac <eacevedof@yahoo.es>
2021-01-24 17:02:58 +0100

    creo una imagen slim

--- pyprint/docker/entrypoint.sh
+++ pyprint/docker/entrypoint.sh
@@ -2,6 +2,9 @@
 python -m pip install --upgrade pip
 pip install fastapi
 pip install uvicorn[standard]
+pip install debugpy
 
+#python -m debugpy --listen 0.0.0.0:5678 --wait-for-client -m public/main.py
+python -m debugpy --listen 0.0.0.0:5678 --wait-for-client
 #https://www.uvicorn.org/settings/
 cd public; uvicorn main:app --reload --host 0.0.0.0 --port 8080 --reload-dir "../src"
