9b64401189b5ffed25d4ec685148c292b3eb43bd
ioedumac <eacevedof@yahoo.es>
2021-01-24 12:33:12 +0100

    ya se muestra todo con refactor

--- pyprint/pyprint/src/boot/fastapi.py
+++ pyprint/pyprint/src/boot/fastapi.py
@@ -1,22 +1,3 @@
-from typing import Optional
 from fastapi import FastAPI, Request
 
-from src.routes.routes import routes
-from src.controllers.home_controller import HomeController
-
 app = FastAPI()
-
-for route in routes:
-    @app.get(routes["url"])
-
-
-@app.get("/")
-def home():
-    return {"routes": routes}
-    return (HomeController()).index()
-
-# para que llegue q hay que enviar todo con cabecer accpet application/json
-@app.get("/prueba/{slug_x}")
-def test_get(slug_x: str, request: Request):
-    return {"slug_x": slug_x, "perro": request.client.host}
-    return (HomeController()).get_test(slug_x, perro=perro)
---
+++ pyprint/pyprint/src/routes/home.py
@@ -0,0 +1,16 @@
+from src.boot.fastapi import app, Request
+
+from typing import Optional
+from src.controllers.home_controller import HomeController
+
+
+@app.get("/")
+def home():
+    return {"routes": routes}
+    return (HomeController()).index()
+
+# para que llegue q hay que enviar todo con cabecer accpet application/json
+@app.get("/prueba/{slug_x}")
+def test_get(slug_x: str, request: Request):
+    return {"slug_x": slug_x, "perro": request.client.host}
+    return (HomeController()).get_test(slug_x, perro=perro)
