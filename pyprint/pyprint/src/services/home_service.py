2df066854306604ba7e6de30a09d163ec4dc5535
ioedumac <eacevedof@yahoo.es>
2021-01-24 12:56:48 +0100

    todo ok

--- pyprint/pyprint/src/services/home_service.py
+++ pyprint/pyprint/src/services/home_service.py
@@ -4,9 +4,4 @@ import os
 class HomeService:
 
     def get_index(self):
-        paths = []
-        for p in sys.path:
-            p = os.path.realpath(p)
-            paths.append(p)
-
-        return {"HomeService":"Python Print","paths":paths}
+        return {"HomeService":"HomeService->get_index"}
