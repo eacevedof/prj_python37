cbf0e921be40ffc0f2195f8bb5e787f7a3ae25f6
ioedumac <eacevedof@yahoo.es>
2021-01-24 21:26:27 +0100

    no contesta

--- pyprint/pyprint/public/main.py
+++ pyprint/pyprint/public/main.py
@@ -1,10 +1,15 @@
 import sys
 import os
 
+
 from debug import debugpy_start
 
-#debugpy_start()
+debugpy_start()
 
 pathsrc = os.path.abspath("..")
 sys.path.append(pathsrc)
 from src.routes.all import *
+
+
+if __name__=="__main__":
+    uvicorn.run("app.app:app",host='0.0.0.0', port=8080, reload=True, debug=True, workers=3)
