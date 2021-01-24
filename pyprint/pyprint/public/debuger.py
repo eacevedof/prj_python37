b764d1054d649c94c4a884b4bdbd658e7821b2ae
ioedumac <eacevedof@yahoo.es>
2021-01-24 20:02:49 +0100

    parece que esto puede funcionar con ayuda de debuger.py

---
+++ pyprint/pyprint/public/debuger.py
@@ -0,0 +1,15 @@
+# debuger.py
+from os import getenv
+
+def initialize_flask_server_debugger_if_needed():
+    #if getenv("DEBUGGER") == "True":
+    if 1==1:    
+        import multiprocessing
+
+        if multiprocessing.current_process().pid > 1:
+            import debugpy
+
+            debugpy.listen(("0.0.0.0", 5678))
+            print("⏳ VS Code debugger can now be attached, press F5 in VS Code ⏳", flush=True)
+            debugpy.wait_for_client()
+            print("🎉 VS Code debugger attached, enjoy debugging 🎉", flush=True)
