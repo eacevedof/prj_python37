8de314286945d90e2187a26ed158e355ce42bed6
ioedumac <eacevedof@yahoo.es>
2021-01-28 23:13:15 +0100

    linux.sh

--- pyprint/pyinstaller-sh/linux.sh
+++ pyprint/pyinstaller-sh/linux.sh
@@ -1,6 +1,6 @@
 pyinstaller /app/pyprint/public/main.py -F \
---workpath "/app/compiled/build" \
---distpath "/app/compiled/dist" \
+--workpath "/app/compiled/linux/build" \
+--distpath "/app/compiled/linux/dist" \
 --name "pyprint-linux" \
 --icon='icon.icns' \
 --clean
