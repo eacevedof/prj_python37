b3e527185496a70c7d3de28c6fbfce82b5ba8fe9
ioedumac <eacevedof@yahoo.es>
2021-01-28 23:06:15 +0100

    configurando rurtas de compilacion

--- pyprint/pyinstaller-sh/linux.sh
+++ pyprint/pyinstaller-sh/linux.sh
@@ -1,12 +1,6 @@
-# https://github.com/codingforentrepreneurs/30-Days-of-Python/blob/master/tutorial-reference/Day_28/src/wsgi.py
-
-source bin/activate  #activa venv
-pyinstaller src/wsgi.py -F \
---name "cfe-os-mac" \
+pyinstaller /app/pyprint/public/main.py -F \
+--workpath "/app/compiled/build" \
+--distpath "/app/compiled/dist" \
+--name "pyprint-linux" \
 --icon='icon.icns' \
---add-binary='/System/Library/Frameworks/Tk.framework/Tk':'tk' \
---add-binary='/System/Library/Frameworks/Tcl.framework/Tcl':'tcl' \
---add-data "src/data/*:data" \
---add-data "src/data/*.jpg:data" \
---hidden-import waitress \
 --clean
