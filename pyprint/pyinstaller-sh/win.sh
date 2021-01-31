185c19a69204387894b86e31c4ea6e250a509caa
Eduardo A.F <eacevedof@yahoo.es>
2021-01-31 22:25:12 +0100

    hidden imports uvicorn

--- pyprint/pyinstaller-sh/win.sh
+++ pyprint/pyinstaller-sh/win.sh
@@ -12,6 +12,15 @@ pyinstaller ./pyprint/public/main.py -F \
 --workpath "./compiled/win/build" \
 --distpath "./compiled/win/dist" \
 --name "pyprint-win" \
---hidden-import uvicorn \
+--hidden-import uvicorn.logging \
+--hidden-import uvicorn.loops \
+--hidden-import uvicorn.loops.auto \
+--hidden-import uvicorn.protocols \
+--hidden-import uvicorn.protocols.http \
+--hidden-import uvicorn.protocols.http.auto \
+--hidden-import uvicorn.protocols.websockets \
+--hidden-import uvicorn.protocols.websockets.auto \
+--hidden-import uvicorn.lifespan \
+--hidden-import uvicorn.lifespan.on \
 --clean 
 #--icon='icon.icns' \
