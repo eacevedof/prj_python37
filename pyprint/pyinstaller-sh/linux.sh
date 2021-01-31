0e86677a8edac5ba4e9b28798adf149adf228bd9
ioedumac <eacevedof@yahoo.es>
2021-01-31 23:23:47 +0100

    probando en linux

--- pyprint/pyinstaller-sh/linux.sh
+++ pyprint/pyinstaller-sh/linux.sh
@@ -2,5 +2,15 @@ pyinstaller /app/pyprint/public/main.py -F \
 --workpath "/app/compiled/linux/build" \
 --distpath "/app/compiled/linux/dist" \
 --name "pyprint-linux" \
---icon='icon.icns' \
+# --icon='icon.icns' \
+--hidden-import uvicorn.logging \
+--hidden-import uvicorn.loops \
+--hidden-import uvicorn.loops.auto \
+--hidden-import uvicorn.protocols \
+--hidden-import uvicorn.protocols.http \
+--hidden-import uvicorn.protocols.http.auto \
+--hidden-import uvicorn.protocols.websockets \
+--hidden-import uvicorn.protocols.websockets.auto \
+--hidden-import uvicorn.lifespan \
+--hidden-import uvicorn.lifespan.on \
 --clean
