a7b34de6cb5e7b88e66583645f1f93ed7e08c076
ioedumac <eacevedof@yahoo.es>
2021-02-02 21:57:31 +0100

    funciona como servicios, falta probar el compilado

--- pyprint/pyprint/public/main.py
+++ pyprint/pyprint/public/main.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python
+import sys
+import os
 import uvicorn
-from fastapi import FastAPI
 
-app = FastAPI()
-
-@app.get("/")
-def root():
-    return {"hello": "world"}
+pathsrc = os.path.abspath("..")
+sys.path.append(pathsrc)
+from src.routes.all import *
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=5678)
