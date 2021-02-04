2286e078924f66f5482ebd206dae833df245e744
ioedumac <eacevedof@yahoo.es>
2021-02-04 22:11:45 +0100

    ya tengo las trazas en builtin

--- pyprint/pyprint/public/main.py
+++ pyprint/pyprint/public/main.py
@@ -1,10 +1,35 @@
 #!/usr/bin/python
+import builtins
 import sys
 import os
 import uvicorn
+import pathlib
 
-pathsrc = os.path.abspath("..")
-sys.path.append(pathsrc)
+def pd(text, title=""):
+    if title:
+        print(f"\n{title}:")
+    print(f"{text}\n")
+    sys.exit()
+
+def pr(text, title=""):
+    if title:
+        print(f"\n{title}:")
+    print(f"{text}\n")
+
+
+builtins.pd = pd
+builtins.pr = pr
+
+pathpublic = pathlib.Path(__file__).parent.absolute()
+pr(pathpublic,"pathpublic")
+
+pathroot = os.path.abspath(pathpublic.parent)
+pr(pathroot,"pathroot 2")
+
+#pathroot = os.path.abspath("..") # asi funciona sin debug de vscode y apunta a pyprint/pyptint
+#pd(pathroot,"pathroot old")
+
+sys.path.append(pathroot)
 from src.routes.all import *
 
 if __name__ == "__main__":
