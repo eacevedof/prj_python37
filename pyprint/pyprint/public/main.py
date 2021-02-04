c8f7de1b18de4fd1ec2198b512b1d699c6fd5df9
ioedumac <eacevedof@yahoo.es>
2021-02-04 22:17:00 +0100

    ya detecta el so

--- pyprint/pyprint/public/main.py
+++ pyprint/pyprint/public/main.py
@@ -34,4 +34,4 @@ from src.routes.all import *
 
 if __name__ == "__main__":
     #uvicorn.run(app, host="0.0.0.0", port=5678)
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8081)
