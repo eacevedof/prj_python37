3590aee752ef2efa72f4a32e106cc6827fdd9292
ioedumac <eacevedof@yahoo.es>
2021-02-04 21:06:29 +0100

    log_component

--- pyprint/pyprint/src/functions/system.py
+++ pyprint/pyprint/src/functions/system.py
@@ -1,8 +1,13 @@
 from sys import platform
 from src.enums.os import OS
+from src.factories.log_factory import get_log
 
+
+log = get_log()
+log.save(platform,"platform")
 
 def is_linux():
+    print(platform)
     return platform == OS.LINUX1 or platform == OS.LINUX2
 
 def is_windows():
@@ -11,3 +16,14 @@ def is_windows():
 def is_macos():
     return platform == OS.MACOS
 
+def get_os():
+    if is_linux():
+        return OS.LINUX1
+    
+    if is_windows():
+        return OS.WINDOWS
+
+    if is_macos():
+        return OS.MACOS
+
+    return "Not found"
