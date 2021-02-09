565904015a06797d4ef2cfc6bf8bbc171e11734b
Eduardo A.F <eacevedof@yahoo.es>
2021-02-09 22:30:15 +0100

    print_windows_component print link

--- pyprint/pyprint/src/components/print_windows_component.py
+++ pyprint/pyprint/src/components/print_windows_component.py
@@ -2,6 +2,7 @@ from src.factories.log_factory import get_log
 from src.components.cmd_component import CmdComponent
 import win32print
 
+# http://newcenturycomputers.net/projects/pythonicwindowsprinting.html
 # https://es.stackoverflow.com/questions/4185/imprimir-datos-en-una-impresora-desde-python-en-windows
 # http://timgolden.me.uk/python/win32_how_do_i/print.html
 # Use EnumPrintersW to list local printers with their names and descriptions.
