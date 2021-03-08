08160b4dcf3337f55011aaac6e2b80cc14dbb05a
ioedumac <eacevedof@yahoo.es>
2021-03-08 22:11:15 +0100

    sqlalchemy

---
+++ sqlalchemy/docker/.bashrc
@@ -0,0 +1,4 @@
+alias linux='/bin/bash /app/helpers-sh/linux.sh'
+alias dist='cd /app/compiled/linux/dist'
+alias build='cd /app/compiled/linux/build'
+alias pyprint='cd /app/pyprint'
---
+++ sqlalchemy/docker/entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+apt-get install -y binutils libc6
+apt-get install -y binutils libc-bin
+
+python -m pip install --upgrade pip
+
+pip install pyinstaller
+pip install debugpy
+pip install fastapi
+pip install uvicorn[standard]
+
+# arregla max recursion del compilado
+pip uninstall pydantic
+pip install git+git://github.com/samuelcolvin/pydantic@master#egg=pydantic
+
+#pip install debugpy -t /tmp
+
+#python -m debugpy --listen 0.0.0.0:5678 --wait-for-client -m uvicorn
+#python -m debugpy --listen 0.0.0.0:5678 --wait-for-client -m public/main.py
+#python /tmp/debugpy --wait-for-client --listen 0.0.0.0:5678 /pyprint/public/main.py
+
+#python -m debugpy --listen 0.0.0.0:5678 --wait-for-client -m src
+
+#python /tmp/debugpy --wait-for-client --listen 0.0.0.0:5678 -m uvicorn
+
+#python /tmp/debugpy --wait-for-client --listen 0.0.0.0:5678 -m uvicorn main:app 
+
+#https://www.uvicorn.org/settings/
+cd public; uvicorn main:app --reload --host 0.0.0.0 --port 8080 --reload-dir "../src"
+
+# python public/main.py
+
+# python -m public/main.py
+
+# esta llamada permite reconocer src como módulo pero no me sirve ya que es solo para debug
+# python -m public.main 
