dc542c0f9f494d391fa587e92b1cdf074bab0545
ioedumac <eacevedof@yahoo.es>
2021-03-11 22:40:39 +0100

    call

--- lambdas/lambda-a/function/index.py
+++ lambdas/lambda-a/function/index.py
@@ -3,18 +3,22 @@ import boto3
 from botocore.config import Config
 
 
-def invoke_fn_b():
-    #https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/sam-cli-command-reference-sam-local-start-lambda.html
+def invoke_fn_b(local=1):
     config_lambda = Config(retries={'total_max_attempts': 1}, read_timeout=1200)
-    pprint(config_lambda)
-    lambda_client = boto3.client('lambda',
-                             config=config_lambda,
-                             endpoint_url='http://docker.for.mac.localhost:3050')
-    r=lambda_client.invoke(
-        FunctionName='BFunction',
-        InvocationType='RequestResponse',
-        Payload='some_data'.encode('UTF-8')
-    )
+
+    if local:
+        return boto3.client(
+            'lambda',
+            config=config_lambda,
+            #endpoint_url='http://host.docker.internal:3050',
+            endpoint_url='http://localhost:3050',
+            use_ssl=False,
+            verify=False,
+        )
+    else:
+        return  boto3.client(
+            'lambda'
+        )
     pprint(r)
 
 
