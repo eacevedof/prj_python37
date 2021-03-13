fa32ef37acec3d7439f25fd97672b26569e30f3c
ioedumac <eacevedof@yahoo.es>
2021-03-13 18:17:01 +0100

    refactor lambdas

--- lambdas/lambda-b/function/index.py
+++ lambdas/lambda-b/function/index.py
@@ -1,9 +1,12 @@
 from pprint import pprint
+from datetime import datetime
+import json
 
 
 def fn_b(event, context):
-    pprint(event)
-    pprint(context)
-    return (
-        "Hello im function B",
-    )
+    now = datetime.utcnow().strftime("%Y-%m-%d-%H:%M:%S")
+    eventjson = json.dumps(event)
+    return {
+        "event": f"input event:\n {eventjson}",
+        "sucess": f"This is a successfully response from Lambda B {now}",
+    }
