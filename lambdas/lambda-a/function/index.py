8d4f69ad0ffd5ade3a2e2721f9b9f6f19319c708
ioedumac <eacevedof@yahoo.es>
2021-03-13 19:08:48 +0100

    refactor lambdas sin error en sintaxis

--- lambdas/lambda-a/function/index.py
+++ lambdas/lambda-a/function/index.py
@@ -1,45 +1,55 @@
 from pprint import pprint
-import datetime
+from datetime import datetime
 import json
 import boto3
 from botocore.config import Config
 
 
-def invoke_fn_b(local=1):
-  config_lambda = Config(retries={'total_max_attempts': 1}, read_timeout=1200)
-
-    if local:
-        return boto3.client(
+def get_boto_client():
+    config_lambda = Config(retries={'total_max_attempts': 1}, read_timeout=1200)
+    return boto3.client(
             'lambda',
             config=config_lambda,
-            #endpoint_url='http://host.docker.internal:3050',
+            # esto no va
+            # endpoint_url='http://host.docker.internal:3050',
             endpoint_url='http://localhost:3050',
             use_ssl=False,
             verify=False,
         )
-    else:
-        return  boto3.client(
-            'lambda'
-        )
-...
-    payload = json.dumps({
-            "account_id": account_id
-        })
 
-    response = lambda_client.invoke(
-        FunctionName=lambda_function,
-        InvocationType='RequestResponse',
-        #LogType='Tail',
-        Payload=payload
-    )
 
 def fn_a(event, context):
 
-    invoke_fn_b()
     now = datetime.utcnow().strftime("%Y-%m-%d-%H:%M:%S")
     eventjson = json.dumps(event)
 
-    return {
-        "event": f"input event:\n {eventjson}",
-        "success": f"This is a response succes from Lambda A {now}",
-    }
+    try:
+        response = get_boto_client().invoke(
+            FunctionName="MyFunctionB",
+            InvocationType='RequestResponse',
+            #LogType='Tail',
+            Payload=json.dumps({"from": "This is some payload for Lambda B"})
+        )
+    except Exception as ex:
+        ex = str(ex)
+        return {
+            "error": f"Error invoking Lambda B: {ex}"
+        }
+
+    if response['StatusCode'] == 200:
+        payload = json.loads(response['Payload'].read())
+        if "error" in payload:
+            return {
+                "error": f"Error response from Lambda B: {payload['error']}"
+            }
+        return {
+            "event": f"input event:\n {eventjson}",
+            "success": f"This is a response succes from Lambda A {now}",
+            "from-lambda-b": f""
+        }
+    else:
+        return {
+            "error": f"Error in response from Lambda B status: {response['StatusCode']}"
+        }
+
+
