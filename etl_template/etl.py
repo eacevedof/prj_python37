df751d162331432adc91c68c3ae2be9ee5b0a48d
Eduardo A.F <eacevedof@yahoo.es>
2021-10-24 14:17:38 +0200

    etl-template

---
+++ etl_template/etl.py
@@ -0,0 +1,24 @@
+PATH_SOURCE_FILE = ""
+PATH_DESTINY_FILE = ""
+
+def get_file_content(path):
+    return ""
+
+def file_put_contents(path):
+    pass
+
+def extract():
+    return ""
+
+def transform(data):
+    return ""
+
+def load(data):
+    file_put_contents(PATH_DESTINY_FILE)
+
+def main():
+    data = extract()
+    data = transform(data)
+    load(data)
+
+main()
