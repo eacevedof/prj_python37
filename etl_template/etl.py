abeb866d4f6fa49a5d512e891ee4f9e9e480bce5
ioedumac <eacevedof@yahoo.es>
2021-10-24 18:48:31 +0200

    etl

--- etl_template/etl.py
+++ etl_template/etl.py
@@ -1,11 +1,13 @@
 PATH_SOURCE_FILE = "./data/source.txt"
 PATH_DESTINY_FILE = ""
 
-def get_file_content(path):
-    return ""
+def get_file_content(path: str) -> str:
+    with open(path) as f:
+        return f.read()
 
-def file_put_contents(path):
-    pass
+def file_put_contents(path: str, data: str) -> None:
+    with open(path, "a") as f:
+        f.write(data)
 
 def extract():
     return ""
@@ -17,8 +19,11 @@ def load(data):
     file_put_contents(PATH_DESTINY_FILE)
 
 def main():
-    data = extract()
-    data = transform(data)
-    load(data)
+    try:
+        data = extract()
+        data = transform(data)
+        load(data)
+    except Exception:
+        print("Exception")
 
 main()
