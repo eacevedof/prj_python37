820d7b839f4c1501965dc1a91c0eaf29f89509a1
ioedumac <eacevedof@yahoo.es>
2021-10-24 18:57:58 +0200

    etl

--- etl_template/etl.py
+++ etl_template/etl.py
@@ -1,7 +1,11 @@
+from os.path import exists
+
 PATH_SOURCE_FILE = "./data/source.txt"
-PATH_DESTINY_FILE = ""
+PATH_TARGET_FILE = "./data/target.json"
 
 def get_file_content(path: str) -> str:
+    if not exists(path):
+        return ""
     with open(path) as f:
         return f.read()
 
@@ -9,20 +13,21 @@ def file_put_contents(path: str, data: str) -> None:
     with open(path, "a") as f:
         f.write(data)
 
-def extract():
+def extract(path: str) -> str:
+    content = get_file_content(path)
+    return content.strip(" ")
+
+def transform(data) -> str:
     return ""
 
-def transform(data):
-    return ""
-
-def load(data):
-    file_put_contents(PATH_DESTINY_FILE)
+def load(path, data) -> None:
+    file_put_contents(path, data)
 
 def main():
     try:
-        data = extract()
+        data = extract(PATH_SOURCE_FILE)
         data = transform(data)
-        load(data)
+        load(PATH_TARGET_FILE, data)
     except Exception:
         print("Exception")
 
