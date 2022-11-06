90e8b5d73f0d634e1cd44e33474e95cb1a56aadf
ioedumac <eacevedof@yahoo.es>
2022-11-06 22:07:27 +0100

    pdf to csv

--- pdf-to-csv/py-tabula.py
+++ pdf-to-csv/py-tabula.py
@@ -1,9 +1,10 @@
 # pip uninstall tabula; install tabula-py
 
 from files import *
-from tabula import read_pdf
+from tabula import read_pdf, convert_into
 import sys
 from pprint import pprint
+import csv
 # Read a PDF File
 #file = f"./PROYECTO-BASICO-Y-DE-EJECUCION-EL-CASAR-19.10.22.pdf"
 #file_to = f"./proyecto.csv"
@@ -14,18 +15,30 @@ from pprint import pprint
 # area (top,left,bottom,right)
 #df = tabula.read_pdf(file, lattice=True,pages=[639])
 # df = tabula.read_pdf(file, stream=True, pages=[639, 640])
-df = table_pdf = read_pdf(
+df = read_pdf(
     file_merged,
     guess=False,
     pages = 1,
     stream=True,
     encoding="utf-8",
     area = (96,24,558,750),
-    columns = (95, 170, 410, 435, 573)
+    # output_format="json",
+    columns = (95, 95+76, 95+318, 95+346, 95+395,95+494,95+574,95+639)
 )
-print(df); sys.exit()
+
+#f = open(file_to, "w")
+#writer = csv.writer(f)
+#writer.writerows(df[0])
+#f.close()
+#pprint(df[0]); sys.exit()
 
 # convert PDF into CSV
-tabula.convert_into(file, file_to, output_format="csv", pages=[639])
-#tabula.convert_into(file, file_to, output_format="csv",  pages=[600])
+#tabula.convert_into(file, file_to, output_format="json", pages=[639])
+convert_into(
+    file_merged,
+    file_to,
+    output_format="tsv",
+    pages=[1,2],
+    columns = (95, 95+76, 95+318, 95+346, 95+395,95+494,95+574,95+639)
+)
 
