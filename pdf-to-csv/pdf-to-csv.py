750d319275f8b8f1aed73f652927df22933a8c8f
ioedumac <eacevedof@yahoo.es>
2022-11-06 18:05:22 +0100

    tabula

--- pdf-to-csv/pdf-to-csv.py
+++ pdf-to-csv/pdf-to-csv.py
@@ -7,8 +7,10 @@ file_to = f"./proyecto.csv"
 
 #df = tabula.read_pdf(file, pages=[639,650])[0]
 #df = tabula.read_pdf(file, options="--columns 0,1,2,3,4,5,6,7,8,9", pages=[639])[0]
+
 # area (top,left,bottom,right)
-df = tabula.read_pdf(file, lattice=True, area=(0, 0, 700, 1000), pages=[639])[0]
+#df = tabula.read_pdf(file, lattice=True,pages=[639])
+df = tabula.read_pdf(file, stream=True, pages=[639, 640])
 pprint(df); sys.exit()
 
 # convert PDF into CSV
