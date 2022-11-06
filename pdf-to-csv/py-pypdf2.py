f983db36766c23f1a7013cf44392293da0ebb5ac
ioedumac <eacevedof@yahoo.es>
2022-11-06 19:13:37 +0100

    pdf to csv

--- pdf-to-csv/py-pypdf2.py
+++ pdf-to-csv/py-pypdf2.py
@@ -2,7 +2,25 @@
 from files import *
 from pprint import pprint
 from PyPDF2 import PdfReader
+from PyPDF2 import PdfFileReader, PdfFileWriter
+import os
 
-reader = PdfReader(file)
-page = reader.pages[639]
-pprint(page.extractText())
+#reader = PdfReader(file)
+#page = reader.pages[639]
+#pprint(page.extractText())
+
+
+def pdf_splitter(path):
+    fname = os.path.splitext(os.path.basename(path))[0]
+    pdf = PdfFileReader(path)
+    for page in range(pdf.getNumPages()):
+        if page<638:
+            continue
+        pdf_writer = PdfFileWriter()
+        pdf_writer.addPage(pdf.getPage(page))
+        output_filename = f"./splitted/page_{page+1}.pdf"
+        with open(output_filename, "wb") as out:
+            pdf_writer.write(out)
+        print("Created: {}".format(output_filename))
+
+pdf_splitter(file)
