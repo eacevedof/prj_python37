792d404d64cd0436d9aea17ebf514dc015a346cf
ioedumac <eacevedof@yahoo.es>
2022-11-09 18:22:14 +0100

    pdf to csv

--- pdf-to-csv/py-tocsv.py
+++ pdf-to-csv/py-tocsv.py
@@ -4,8 +4,21 @@ from pprint import pprint
 from PyPDF2 import PdfFileReader
 import sys
 
-# CÓDIGO RESUMEN UDS LONGITUD ANCHURA ALTURA CANTIDAD PRECIO IMPORTE
-#
+"""
+90.72 - 127	166.4 - 407.23	411 - 429.40	436.12 - 483.84	487.87 - 535	538.27 - 575.23	590.01 - 636-38	666.62 - 700.22	735.84 - 776.16
+
+"""
+columns = {
+    "codigo": {"x1":90.72, "x2":127},
+    "resumen": {"x1":166.4, "x2":407.23},
+    "uds": {"x1":411, "x2":429.4},
+    "longitud": {"x1":436.12, "x2":483.84},
+    "anchura": {"x1":487.87, "x2":535},
+    "altura": {"x1":538.27, "x2":575.23},
+    "cantidad": {"x1":590.01, "x2":636.38},
+    "precio": {"x1":666.62, "x2":700.22},
+    "importe": {"x1":735.84, "x2":776.16},
+}
 
 page_parts = []
 
@@ -14,7 +27,10 @@ def visitor_body(text, cm, text_matrix, fontDict, fontSize):
     #pprint(fontSize);sys.exit()
     dic = {
         "text": text,
-        "pos": [text_matrix[4], text_matrix[5]],
+        "pos": {
+            "x": text_matrix[4],
+            "y": text_matrix[5],
+        }
     }
     if text_matrix[4] != 0.0 and text_matrix[5] != 0.0:
         page_parts.append(dic)
@@ -39,4 +55,21 @@ def get_text():
     return all_pages
 
 all_pages = get_text()
-pprint(all_pages[1])
+
+def get_merged_line_with_same_y(page, x, y):
+    lines = filter(lambda line: line.get("pos").get("x")==x and line.get("pos").get("y")==y, page.get("content"))
+    #lines = list(lines)
+    lines = map(lambda line: line.get("text", ""), lines)
+    return "".join(lines)
+
+def get_csv():
+    global all_pages
+    for page in all_pages:
+        dic_lines = page.get("content", [])
+        lines = []
+        for dic_line in dic_lines:
+            text_line = get_merged_line_with_same_y(page, dic_line.get("pos").get("x"), dic_line.get("pos").get("y"))
+            lines.append(text_line)
+        pprint(lines)
+
+get_csv()
