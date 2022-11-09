6e88a9df0bf2f1cd03fbbcef0084900a35c2635c
ioedumac <eacevedof@yahoo.es>
2022-11-09 22:22:15 +0100

    pdf to csv

--- pdf-to-csv/py-tocsv.py
+++ pdf-to-csv/py-tocsv.py
@@ -20,92 +20,6 @@ columns = {
     "importe": {"x1": 735.84, "x2": 776.16},
 }
 
-page_sections = {
-    "titulo": {
-        # if x=90.72 && text=="PRESUPUESTO Y MEDICIONES"
-        "codigo": "PRESUPUESTO Y MEDICIONES",
-    },
-    "table_header": {
-        # if x=90.72 && text=="CÓDIGO RESUMEN UDS LONGITUD ANCHURA ALTURA CANTIDAD PRECIO IMPORTE"
-        "codigo": "CÓDIGO RESUMEN UDS LONGITUD ANCHURA ALTURA CANTIDAD PRECIO IMPORTE"
-    },
-    "section_header": {
-        # if same y and in x in codigo is regex[\d{2}] and
-        # next item in x in resumen and no more in other cols
-        """
-        {'pos': {'x': 90.72, 'y': 84.32}, 'text': '\n'}
-        {'pos': {'x': 90.72, 'y': 84.32}, 'text': '01'}
-        {'pos': {'x': 166.4, 'y': 84.32}, 'text': ' VIVIENDA MODULAR'}
-        """
-        "codigo": "01",
-        "resumen": "01 VIVIENDA MODULAR"
-    },
-    "subsection_header": {
-        # if same y and in x in codigo is regex[\d{2}.\d{2}] and next x in resumen and no more in otherscols
-        "codigo": "01.01",
-        "resumen": "ESTRUCTURA METÁLICA",
-    },
-    "chapter": {
-        # if same y and in x in codigo is regex[\d{2}.\d{2}.\d{2}] and next x in resumen and no more in otherscols
-        """
-        {
-            'coord': {'x': 90.72, 'y': 131.52},
-            'text': '01.01.01 kg VIGAS METÁLICAS DE MÓDULOS 1, 2, 3, 4, 5, 6, 7 y 8'
-        }
-        """
-        "codigo": "01.01.01",
-        "resumen": "kg VIGAS METÁLICAS DE MÓDULOS 1, 2, 3, 4, 5, 6, 7 y 8",
-    },
-    "chapter_description": {
-        [
-            # if only in resumen
-            {"codigo": "", "resumen": "a"},
-            {"codigo": "", "resumen": "b"},
-            {"codigo": "", "resumen": "c"},
-            {"codigo": "", "resumen": ""},  # is only description
-            {
-                "codigo": "",
-                "resumen": "TITULO 1"
-            },
-            {
-                "codigo": "",
-                "resumen": "TITULO 2"
-            },
-            {
-                "codigo": "",
-                "resumen": "TITULO 3"
-            },
-        ]
-    },
-    "cantidades": [
-        {
-            # if in resumen and in cantidad
-            "codigo": "",
-            "resumen": "concepto x",
-            "uds": "2",
-            "longitud": "13,64",
-            "anchura": "22.40",
-        },
-    ],
-    "chapter_total": {
-        "codigo": "", "resumen": "", "uds": "", "longitud": "",
-        "anchura": "", "altura": "",
-        # calcular por coor-x y si hay 3 numeros
-        "cantidad": "", "precio": "", "importe": "",
-    },
-    "subsection_total": {
-        # si hay TOTAL y ....
-        # TOTAL 01.03.......................................... 11.455,45
-        "codigo": "", "resumen": "",
-        "uds": "TOTAL nn.pp ...........................................................................................",
-        "longitud": "",
-        "anchura": "",
-        "altura": "",
-        "cantidad": "",
-        "precio": "",
-        "importe": "11.455,45",
-    }
-}
 
 page_coords = []
 
@@ -137,7 +51,7 @@ def get_all_pages_coords():
             "page": i_page,
             "lines": page_coords.copy()
         })
-        if i_page == 2:
+        if i_page == 1:
             return all_pages
 
     return all_pages
@@ -145,7 +59,7 @@ def get_all_pages_coords():
 
 def get_line_by_y(y, lines):
     by_y = filter(lambda line: line.get("coord").get("y") == y and line.get("text") != "\n", lines)
-    r = map(lambda line: {"x": line.get("coord").get("x"), "text": line.get("text")}, by_y)
+    r = map(lambda line: {"x": line.get("coord").get("x"), "text": line.get("text","").strip()}, by_y)
     return {
         "y": y,
         "xs": list(r)
@@ -158,14 +72,19 @@ def get_merged_line_with_same_y():
     transformed = []
     y_processed = []
     for page in page_and_its_coord_lines:
+        i_page = page.get("page")
         lines_coords = page.get("lines")
+        new_lines = []
         for lines_coord in lines_coords:
             y = lines_coord.get("coord").get("y")
             if y in y_processed:
                 continue
             y_processed.append(y)
             by_y = get_line_by_y(y, lines_coords)
-            transformed.append(by_y)
+            new_lines.append(by_y)
+        transformed.append({
+            "page": new_lines
+        })
     return transformed
 
 
