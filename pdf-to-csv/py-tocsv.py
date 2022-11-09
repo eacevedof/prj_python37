c5a3c4dbbea23bcafb9f07b5d2ac097636156ea7
ioedumac <eacevedof@yahoo.es>
2022-11-09 16:27:59 +0100

    py-tabula

--- pdf-to-csv/py-tocsv.py
+++ pdf-to-csv/py-tocsv.py
@@ -4,36 +4,19 @@ from pprint import pprint
 from PyPDF2 import PdfFileReader
 import sys
 
-
 # CÓDIGO RESUMEN UDS LONGITUD ANCHURA ALTURA CANTIDAD PRECIO IMPORTE
 #
 
-def chk(txt, ar_map, ar_text, font, d):
-    dic = {
-        "txt": txt,
-        "ar_map": ar_map,
-        "ar_text": ar_text,
-        #"font": font,
-        #"d": d,
-    }
-    pprint(dic)
-    #sys.exit()
-    return "hola"
+pages_content = []
 
 
-parts = []
 def visitor_body(text, cm, text_matrix, fontDict, fontSize):
     dic = {
         "text": text,
-        "pos": [text_matrix[3],text_matrix[4],text_matrix[5],],
+        "pos": [text_matrix[3], text_matrix[4], text_matrix[5], ],
     }
-    if text_matrix[4]!= 0.0 and text_matrix[5]!=0.0:
-        pprint(dic)
-
-    #pprint(text_matrix)
-    #if text_matrix[1] != 0.0:
-
-    #parts.append(dic)
+    if text_matrix[4] != 0.0 and text_matrix[5] != 0.0:
+        pages_content.append(dic)
 
 
 def get_text():
@@ -42,11 +25,10 @@ def get_text():
     for i_page in range(reader.getNumPages()):
         page = reader.pages[i_page]
         texts.append(page.extract_text(visitor_text=visitor_body))
-        #print(texts[0])
+        pprint(pages_content)
         sys.exit()
 
-
-    #print(texts[0])
+    pprint(pages_content)
 
 
 get_text()
