a4eb8762a2bfc236bb139432123ad15e53b2972d
ioedumac <eacevedof@yahoo.es>
2022-11-11 19:38:18 +0100

    pdf to csv

--- pdf-to-csv/py-tocsv.py
+++ pdf-to-csv/py-tocsv.py
@@ -99,6 +99,7 @@ for i,page in enumerate(pages_by_y):
         if row: as_dicts.append(row)
         row = get_chapter_total(page_y)
         if row: as_dicts.append(row)
-
+        row = get_subsection_total(page_y)
+        if row: as_dicts.append(row)
 pprint(as_dicts)
 
