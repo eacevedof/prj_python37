ecaa89bd815c8180a87c85da82311cd0b3101d56
ioedumac <eacevedof@yahoo.es>
2022-11-11 16:42:28 +0100

    pdf to csv

--- pdf-to-csv/pdf_parts.py
+++ pdf-to-csv/pdf_parts.py
@@ -154,13 +154,18 @@ def get_section_header_row(line_y):
     x1 = xs[1].get("x")
     x1text = xs[1].get("text")
 
-    if _is_in_column("codigo", x0) and re.search("^\d{2}$", x0text) and x1:
+    if _is_in_column("codigo", x0) and _match("^\d{2}$", x0text) and x1 and _are_empty_after("resumen"):
         row = empty_row.copy()
         row["codigo"] = x0text
         row["resumen"] = x1text
         return row
 
     return None
+
+
+def _match(pattern,text):
+    r = re.search(pattern, text)
+    return True if r else False
 
 
 def _is_in_column(name, x):
