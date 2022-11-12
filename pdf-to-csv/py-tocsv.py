4b36d4e3f72870d232e70436a1369ab417032dc3
ioedumac <eacevedof@yahoo.es>
2022-11-12 13:23:17 +0100

    pdf to csv

--- pdf-to-csv/py-tocsv.py
+++ pdf-to-csv/py-tocsv.py
@@ -103,6 +103,8 @@ for i,page in enumerate(pages_by_y):
         if row: as_dicts.append(row)
         row = get_no_desc_quantity_row(page_y)
         if row: as_dicts.append(row)
+        row = get_quantity_with_width_row(page_y)
+        if row: as_dicts.append(row)
 #pprint(as_dicts)
 
 def to_csv():
