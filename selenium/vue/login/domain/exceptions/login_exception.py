600258c2f1464a1e1b5725356107945f08b5b37e
ioedumac <eacevedof@yahoo.es>
2023-01-15 18:23:51 +0100

    selenium login

---
+++ selenium/vue/login/domain/exceptions/login_exception.py
@@ -0,0 +1,9 @@
+class LoginException:
+    def __int__(self):
+        pass
+
+    def fail_if_no_input(self, message: str = "") -> None:
+        raise Exception(message)
+
+    def fail_if_no_button(self, message: str = "") -> None:
+        raise Exception(message)
