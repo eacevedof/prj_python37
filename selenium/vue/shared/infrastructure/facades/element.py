05ec76a2f55b8c8b54b7b7f7db8e302665be5a55
ioedumac <eacevedof@yahoo.es>
2023-01-15 21:42:49 +0100

    mat attribs

--- selenium/vue/shared/infrastructure/facades/element.py
+++ selenium/vue/shared/infrastructure/facades/element.py
@@ -1,3 +1,4 @@
+from time import sleep
 from dataclasses import dataclass
 from vue.shared.infrastructure.facades.dom import Dom
 
@@ -15,7 +16,9 @@ class Element:
 
         input_element = self.__dom.find_by_id(element_id)
         input_element.send_keys(value)
+        sleep(0.5)
 
     @staticmethod
     def set_value_in_input(input_element: object, value: str) -> None:
         input_element.send_keys(value)
+        sleep(0.5)
