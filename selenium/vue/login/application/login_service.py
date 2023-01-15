67aa1de4d4a82839aa27066b798204a95d6b1b8b
ioedumac <eacevedof@yahoo.es>
2023-01-15 18:49:57 +0100

    selenium login

--- selenium/vue/login/application/login_service.py
+++ selenium/vue/login/application/login_service.py
@@ -11,22 +11,14 @@ def login_usr1_or_fail() -> None:
     sleep(2)
 
     input_email = browser.find_element(By.ID, "id-email")
-    if not input_email:
-        LoginException.fail_if_no_input("no id:id-mail")
     input_email.send_keys(LOGIN_DATA.get("usr1").get("email"))
 
     input_password = browser.find_element(By.ID, "id-password")
-    if not input_password:
-        LoginException.fail_if_no_input("no id:id-password")
     input_password.send_keys(LOGIN_DATA.get("usr1").get("secret"))
 
     submit_button = browser.find_element(By.ID, "btnSignIn")
-    if not submit_button:
-        LoginException.fail_if_no_button("no id:btnSignIn")
     submit_button.click()
     sleep(25)
 
-    div_assets = browser.find_element(By.ID, "commons-assetsk")
-    if not div_assets:
-        LoginException.fail_if_not_logged("not logged")
+    div_assets = browser.find_element(By.ID, "commons-assets")
     sleep(120)
