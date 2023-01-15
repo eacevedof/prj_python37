d191a5ca8e60d0e24f65a953b8eb0f003185e463
ioedumac <eacevedof@yahoo.es>
2023-01-15 19:31:08 +0100

    asset repo

--- selenium/vue/assets/application/asset_create_service.py
+++ selenium/vue/assets/application/asset_create_service.py
@@ -1,23 +1,19 @@
 from time import sleep
 from vue.shared.infrastructure.factories.driver_factory import get_chrome, FRONT_URL
+from vue.shared.infrastructure.facades.dom import Dom
 from vue.assets.infrastructure.asset_repository import ASSETS_CREATION
-from selenium.webdriver.common.by import By
+from vue.login.application.login_service import login_usr1_or_fail
+
+
+def __login() -> None:
+    login_usr1_or_fail()
 
 
 def asset_create() -> None:
+    __login()
+
     browser = get_chrome()
     browser.get(FRONT_URL)
     sleep(2)
 
-    input_email = browser.find_element(By.ID, "id-email")
-    input_email.send_keys(LOGIN_DATA.get("usr1").get("email"))
 
-    input_password = browser.find_element(By.ID, "id-password")
-    input_password.send_keys(LOGIN_DATA.get("usr1").get("secret"))
-
-    submit_button = browser.find_element(By.ID, "btnSignIn")
-    submit_button.click()
-    sleep(25)
-
-    div_assets = browser.find_element(By.ID, "commons-assets")
-    sleep(1)
