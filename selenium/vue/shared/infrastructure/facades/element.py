ad8da2bd8e1e6de94aa23d51a16064d165a0d089
ioedumac <eacevedof@yahoo.es>
2023-01-15 22:48:58 +0100

    backup

--- selenium/vue/shared/infrastructure/facades/element.py
+++ selenium/vue/shared/infrastructure/facades/element.py
@@ -16,9 +16,17 @@ class Element:
 
         input_element = self.__dom.find_by_id(element_id)
         input_element.send_keys(value)
-        sleep(0.25)
+        sleep(0.2)
+
+    def set_value_by_name(self, element_name: str, value: str) -> None:
+        if not self.__dom:
+            return None
+
+        input_element = self.__dom.find_by_name(element_name)
+        input_element.send_keys(value)
+        sleep(0.2)
 
     @staticmethod
     def set_value_in_input(input_element: object, value: str) -> None:
         input_element.send_keys(value)
-        sleep(0.25)
+        sleep(0.2)
