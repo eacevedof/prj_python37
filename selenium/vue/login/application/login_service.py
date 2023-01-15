721ae2e5dbbab32e3cfc49f6f38a663e6e291601
ioedumac <eacevedof@yahoo.es>
2023-01-15 18:07:09 +0100

    selenium vuelve a cargar

--- selenium/vue/login/application/login_service.py
+++ selenium/vue/login/application/login_service.py
@@ -1,4 +1,4 @@
-import time
+from time import  sleep
 from vue.shared.infrastructure.factories.driver_factory import get_chrome, FRONT_URL
 from vue.login.infrastructure.login_repository import LOGIN_DATA
 from selenium.webdriver.common.by import By
@@ -7,7 +7,7 @@ from selenium.webdriver.common.by import By
 def login_javi_or_fail() -> None:
     browser = get_chrome()
     browser.get(FRONT_URL)
-    time.sleep(2)
+    sleep(2)
 
     input_email = browser.find_element(By.ID, "id-email")
     input_email.send_keys(LOGIN_DATA.get("javi").get("email"))
--- selenium/vue/shared/infrastructure/factories/driver_factory.py
+++ selenium/vue/shared/infrastructure/factories/driver_factory.py
@@ -2,8 +2,9 @@ import os
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
-PATH_DRIVER = os.environ.get("PATHPRJ","")
-FRONT_URL = "https://localhost:3000"
+PATH_DRIVER = os.environ.get("PATHPRJ", "")
+FRONT_URL = "http://localhost:3000"
+
 
 def get_chrome():
     service = Service(f"{PATH_DRIVER}/chrome-driver-selenium/chromedriver")
