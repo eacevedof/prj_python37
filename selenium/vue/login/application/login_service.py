579930d4cf717b8fa101d3966a50443ca1ef87fb
ioedumac <eacevedof@yahoo.es>
2023-01-15 18:23:51 +0100

    selenium login

--- selenium/vue/login/application/login_service.py
+++ selenium/vue/login/application/login_service.py
@@ -1,6 +1,7 @@
-from time import  sleep
+from time import sleep
 from vue.shared.infrastructure.factories.driver_factory import get_chrome, FRONT_URL
 from vue.login.infrastructure.login_repository import LOGIN_DATA
+from vue.login.domain.exceptions.login_exception import LoginException
 from selenium.webdriver.common.by import By
 
 
@@ -10,12 +11,16 @@ def login_javi_or_fail() -> None:
     sleep(2)
 
     input_email = browser.find_element(By.ID, "id-email")
+    if not input_email:
+        LoginException.fail_if_no_input("no id:id-mail")
     input_email.send_keys(LOGIN_DATA.get("javi").get("email"))
 
     input_password = browser.find_element(By.ID, "id-password")
+    if not input_password:
+        LoginException.fail_if_no_input("no id:id-password")
     input_password.send_keys(LOGIN_DATA.get("javi").get("secret"))
 
     submit_button = browser.find_element(By.ID, "btnSignIn")
     if not submit_button:
-        raise Exception("No submit button")
+        LoginException.fail_if_no_button("no id:btnSignIn")
     submit_button.click()
