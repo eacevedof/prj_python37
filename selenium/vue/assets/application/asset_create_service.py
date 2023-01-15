cb4332cf5af882b263774d12e917c27c1188bce1
ioedumac <eacevedof@yahoo.es>
2023-01-15 19:47:32 +0100

    asset backup

--- selenium/vue/assets/application/asset_create_service.py
+++ selenium/vue/assets/application/asset_create_service.py
@@ -5,12 +5,9 @@ from vue.assets.infrastructure.asset_repository import ASSETS_CREATION
 from vue.login.application.login_service import login_usr1_or_fail
 
 
-def __login() -> None:
+def asset_create() -> None:
     login_usr1_or_fail()
 
-
-def asset_create() -> None:
-    __login()
     create_url = f"{FRONT_URL}/#/assets/add"
     browser = get_chrome()
     browser.get(create_url)
