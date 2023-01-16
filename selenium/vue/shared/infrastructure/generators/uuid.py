8be559d31981dfbd9963ef5fe8bf798ccda8048b
ioedumac <eacevedof@yahoo.es>
2023-01-16 22:32:16 +0100

    selenium

--- selenium/vue/shared/infrastructure/generators/uuid.py
+++ selenium/vue/shared/infrastructure/generators/uuid.py
@@ -2,5 +2,5 @@ import random
 import string
 
 
-def get_uuid(size=32, chars=string.ascii_letters + string.digits):
+def get_uuid(size=32, chars=string.ascii_letters + string.digits) -> str:
     return "".join(random.choice(chars) for _ in range(size))
