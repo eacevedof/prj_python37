e5ebb15c22da619df198da794268f1e02b82de8e
ioedumac <eacevedof@yahoo.es>
2023-01-25 21:57:09 +0100

    selenium back

--- selenium/vue/oco/assets/infrastructure/asset_repository.py
+++ selenium/vue/oco/assets/infrastructure/asset_repository.py
@@ -36,5 +36,10 @@ class AssetRepository:
         if xpath == ElementEnum.BUTTON_XPATH:
             return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[2]/div/div[1]/div/div[2]/div/div[1]/div/div/div[2]/div/div/div/div[2]/button"
         return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[2]/div/div[1]/div/div[2]/div/div[1]/div/div/div[2]/div/div/div/div[3]/ul/li[1]"
+    @staticmethod
+    def get_sel_type(xpath: str = ElementEnum.BUTTON_XPATH) -> str:
+        if xpath == ElementEnum.BUTTON_XPATH:
+            return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[2]/div/div[1]/div/div[2]/div/div[1]/div/div/div[3]/div/div/div/div[2]/button"
+        return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[2]/div/div[1]/div/div[2]/div/div[1]/div/div/div[3]/div/div/div/div[3]/ul/li[1]"
 
 
