92c6e756a89e783df750dfccfaa45a78d3ea645d
ioedumac <eacevedof@yahoo.es>
2023-01-25 22:17:28 +0100

    selenium back

--- selenium/vue/oco/assets/infrastructure/asset_repository.py
+++ selenium/vue/oco/assets/infrastructure/asset_repository.py
@@ -57,4 +57,9 @@ class AssetRepository:
     def get_id_presentation() -> str:
         return "id-Presentación"
 
+    @staticmethod
+    def get_sel_market(xpath: str = ElementEnum.BUTTON_XPATH) -> str:
+        if xpath == ElementEnum.BUTTON_XPATH:
+            return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[2]/div/div[1]/div/div[2]/div/div[1]/div/div/div[7]/div/div/div/div[2]/button"
+        return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[2]/div/div[1]/div/div[2]/div/div[1]/div/div/div[7]/div/div/div/div[3]/ul/li[2]"
 
