27c064fe7122b9599f4f6caa07400a9057b9717e
ioedumac <eacevedof@yahoo.es>
2023-01-25 22:22:52 +0100

    selenium back

--- selenium/vue/oco/assets/infrastructure/asset_repository.py
+++ selenium/vue/oco/assets/infrastructure/asset_repository.py
@@ -70,3 +70,9 @@ class AssetRepository:
             return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[2]/div/div[1]/div/div[2]/div/div[1]/div/div/div[8]/div/div/div/div[2]/button"
         return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[2]/div/div[1]/div/div[2]/div/div[1]/div/div/div[8]/div/div/div/div[3]/ul/li[1]"
 
+    @staticmethod
+    def get_sel_country(xpath: str = ElementEnum.BUTTON_XPATH) -> str:
+        if xpath == ElementEnum.BUTTON_XPATH:
+            return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[2]/div/div[1]/div/div[2]/div/div[1]/div/div/div[9]/div/div/div/div[2]/button"
+        return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[2]/div/div[1]/div/div[2]/div/div[1]/div/div/div[9]/div/div/div/div[3]/ul/li[3]"
+
