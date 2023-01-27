67c1a15cb72d731c912716cefb4cdcd08d709689
ioedumac <eacevedof@yahoo.es>
2023-01-27 23:25:24 +0100

    selenium back

--- selenium/vue/oco/assets/infrastructure/asset_groups_attributes_repository.py
+++ selenium/vue/oco/assets/infrastructure/asset_groups_attributes_repository.py
@@ -16,3 +16,7 @@ class AssetGroupsAttributesRepository:
     @staticmethod
     def get_tab_production() -> str:
         return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[2]/div/div[1]/div/div[1]/button[2]"
+
+    @staticmethod
+    def get_tab_diseno() -> str:
+        return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[2]/div/div[1]/div/div[1]/button[3]"
