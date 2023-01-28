0d255eead4f19ee7c342aee189a110dada864ab0
ioedumac <eacevedof@yahoo.es>
2023-01-28 12:39:25 +0100

    selenium back

--- selenium/vue/oco/assets/infrastructure/controllers/asset_create_material_controller.py
+++ selenium/vue/oco/assets/infrastructure/controllers/asset_create_material_controller.py
@@ -1,11 +1,11 @@
 from vue.oco.assets.application.asset_create_service import asset_create_material
 
 
-def __asset_create_material() -> None:
+def __invoke() -> None:
     try:
         asset_create_material()
     except Exception as e:
         print(e)
 
 
-__asset_create_material()
+__invoke()
