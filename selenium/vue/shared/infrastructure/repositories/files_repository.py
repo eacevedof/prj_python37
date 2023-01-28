4c88a5b0e06555e558ab4c077e1643fc2b7be683
ioedumac <eacevedof@yahoo.es>
2023-01-28 12:44:22 +0100

    selenium back

--- selenium/vue/shared/infrastructure/repositories/files_repository.py
+++ selenium/vue/shared/infrastructure/repositories/files_repository.py
@@ -1,4 +1,4 @@
-from vue.shared.domain.element_enum import ElementEnum
+from vue.shared.infrastructure.facades.env import *
 
 
 class FilesRepository:
@@ -6,7 +6,6 @@ class FilesRepository:
         pass
 
     @staticmethod
-    def get_sel_asset_type(xpath: str = ElementEnum.BUTTON_XPATH) -> str:
-        if xpath == ElementEnum.BUTTON_XPATH:
-            return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[1]/div/div[2]/div[3]/div/div/div/div[2]/button"
-        return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[1]/div/div[2]/div[3]/div/div/div/div[3]/ul/li[1]"
+    def get_rnd_artworks() -> str:
+        home = getenv(ENV_HOME)
+        return f"{home}/Desktop/assets-1/mat-arch-opcionales-2-de-n.jpg"
