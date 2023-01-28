6cae8ad61169194f0283dcdad377ce8503e6b0df
ioedumac <eacevedof@yahoo.es>
2023-01-28 12:34:15 +0100

    selenium back

---
+++ selenium/vue/shared/infrastructure/repositories/files_repository.py
@@ -0,0 +1,12 @@
+from vue.shared.domain.element_enum import ElementEnum
+
+
+class FilesRepository:
+    def __int__(self):
+        pass
+
+    @staticmethod
+    def get_sel_asset_type(xpath: str = ElementEnum.BUTTON_XPATH) -> str:
+        if xpath == ElementEnum.BUTTON_XPATH:
+            return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[1]/div/div[2]/div[3]/div/div/div/div[2]/button"
+        return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[1]/div/div[2]/div[3]/div/div/div/div[3]/ul/li[1]"
