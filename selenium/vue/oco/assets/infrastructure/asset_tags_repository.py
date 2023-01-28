3b5da4e49f3897eac8fc45d6c237a9b662f3cf6d
ioedumac <eacevedof@yahoo.es>
2023-01-28 12:11:51 +0100

    selenium back

---
+++ selenium/vue/oco/assets/infrastructure/asset_tags_repository.py
@@ -0,0 +1,10 @@
+from vue.shared.domain.element_enum import ElementEnum
+
+
+class AssetTagsRepository:
+    def __int__(self):
+        pass
+
+    @staticmethod
+    def get_tag_artworks() -> str:
+        return "Artworks"
