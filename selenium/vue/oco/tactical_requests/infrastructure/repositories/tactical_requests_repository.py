c3840cac3bc231914d9f138b121e13f123d1ac35
ioedumac <eacevedof@yahoo.es>
2023-01-29 20:06:49 +0100

    tactical-request

--- selenium/vue/oco/tactical_requests/infrastructure/repositories/tactical_requests_repository.py
+++ selenium/vue/oco/tactical_requests/infrastructure/repositories/tactical_requests_repository.py
@@ -10,10 +10,16 @@ class TacticalRequestsRepository:
         if xpath == ElementEnum.BUTTON_XPATH:
             return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[1]/div/div[2]/div[1]/div[1]/div/div/div/div[2]/button"
         return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[1]/div/div[2]/div[1]/div[1]/div/div/div/div[3]/ul/li[1]"
-
     @staticmethod
-    def get_id_asset_name() -> str:
-        return "id-asset-name"
+    def get_sel_asset(xpath: str = ElementEnum.BUTTON_XPATH) -> str:
+        if xpath == ElementEnum.BUTTON_XPATH:
+            return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[1]/div/div[2]/div[1]/div[2]/div/div/div/div[2]/button"
+        return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[1]/div/div[2]/div[1]/div[2]/div/div/div/div[3]/ul/li[1]"
+     @staticmethod
+    def get_sel_asset_(xpath: str = ElementEnum.BUTTON_XPATH) -> str:
+        if xpath == ElementEnum.BUTTON_XPATH:
+            return ""
+        return ""
 
     @staticmethod
     def get_id_button_save() -> str:
