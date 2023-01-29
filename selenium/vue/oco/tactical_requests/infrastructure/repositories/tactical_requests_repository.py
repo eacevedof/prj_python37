8ff09cda9f7375bfe910799466811cecfc8e75a2
ioedumac <eacevedof@yahoo.es>
2023-01-29 20:01:55 +0100

    tactical-request

--- selenium/vue/oco/tactical_requests/infrastructure/repositories/tactical_requests_repository.py
+++ selenium/vue/oco/tactical_requests/infrastructure/repositories/tactical_requests_repository.py
@@ -9,8 +9,8 @@ class TacticalRequestsRepository:
     @staticmethod
     def get_sel_request_type(xpath: str = ElementEnum.BUTTON_XPATH) -> str:
         if xpath == ElementEnum.BUTTON_XPATH:
-            return ""
-        return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[1]/div/div[2]/div[3]/div/div/div/div[3]/ul/li[1]"
+            return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[1]/div/div[2]/div[1]/div[1]/div/div/div/div[2]/button"
+        return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[1]/div/div[2]/div[1]/div[1]/div/div/div/div[3]/ul/li[1]"
 
     @staticmethod
     def get_id_asset_name() -> str:
