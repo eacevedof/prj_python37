ad032a51747f8d824411d980d19fbe72fd9fe569
ioedumac <eacevedof@yahoo.es>
2023-01-29 20:17:07 +0100

    tactical-request

--- selenium/vue/oco/tactical_requests/infrastructure/repositories/tactical_requests_attributes_repository.py
+++ selenium/vue/oco/tactical_requests/infrastructure/repositories/tactical_requests_attributes_repository.py
@@ -6,95 +6,19 @@ class TacticalRequestsAttributesRepository:
         pass
 
     @staticmethod
-    def get_sel_asset_type(xpath: str = ElementEnum.BUTTON_XPATH) -> str:
+    def get_sel_request_reason(xpath: str = ElementEnum.BUTTON_XPATH) -> str:
         if xpath == ElementEnum.BUTTON_XPATH:
-            return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[1]/div/div[2]/div[3]/div/div/div/div[2]/button"
-        return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[1]/div/div[2]/div[3]/div/div/div/div[3]/ul/li[1]"
+            return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[2]/div/div[1]/div/div/div/div[1]/div[1]/div/div/div/div[2]/button"
+        return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[2]/div/div[1]/div/div/div/div[1]/div[1]/div/div/div/div[3]/ul/li[1]"
 
     @staticmethod
-    def get_id_material_code() -> str:
-        return "id-Código Material - Versión"
-
-    @staticmethod
-    def get_sel_category(xpath: str = ElementEnum.BUTTON_XPATH) -> str:
+    def get_sel_request_priority(xpath: str = ElementEnum.BUTTON_XPATH) -> str:
         if xpath == ElementEnum.BUTTON_XPATH:
-            return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[2]/div/div[1]/div/div[2]/div/div[1]/div/div/div[2]/div/div/div/div[2]/button"
-        return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[2]/div/div[1]/div/div[2]/div/div[1]/div/div/div[2]/div/div/div/div[3]/ul/li[1]"
+            return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[2]/div/div[1]/div/div/div/div[1]/div[3]/div/div/div/div[2]/button"
+        return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[2]/div/div[1]/div/div/div/div[1]/div[3]/div/div/div/div[3]/ul/li[2]"
 
     @staticmethod
-    def get_sel_type(xpath: str = ElementEnum.BUTTON_XPATH) -> str:
+    def get_sel_tmp(xpath: str = ElementEnum.BUTTON_XPATH) -> str:
         if xpath == ElementEnum.BUTTON_XPATH:
-            return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[2]/div/div[1]/div/div[2]/div/div[1]/div/div/div[3]/div/div/div/div[2]/button"
-        return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[2]/div/div[1]/div/div[2]/div/div[1]/div/div/div[3]/div/div/div/div[3]/ul/li[1]"
-
-    @staticmethod
-    def get_sel_lab_form(xpath: str = ElementEnum.BUTTON_XPATH) -> str:
-        if xpath == ElementEnum.BUTTON_XPATH:
-            return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[2]/div/div[1]/div/div[2]/div/div[1]/div/div/div[4]/div/div/div/div[2]/button"
-        return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[2]/div/div[1]/div/div[2]/div/div[1]/div/div/div[4]/div/div/div/div[3]/ul/li[1]"
-
-    @staticmethod
-    def get_id_dosis() -> str:
-        return "id-Dosis"
-
-    @staticmethod
-    def get_id_presentation() -> str:
-        return "id-Presentación"
-
-    @staticmethod
-    def get_sel_market(xpath: str = ElementEnum.BUTTON_XPATH) -> str:
-        if xpath == ElementEnum.BUTTON_XPATH:
-            return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[2]/div/div[1]/div/div[2]/div/div[1]/div/div/div[7]/div/div/div/div[2]/button"
-        return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[2]/div/div[1]/div/div[2]/div/div[1]/div/div/div[7]/div/div/div/div[3]/ul/li[2]"
-
-    @staticmethod
-    def get_sel_client(xpath: str = ElementEnum.BUTTON_XPATH) -> str:
-        if xpath == ElementEnum.BUTTON_XPATH:
-            return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[2]/div/div[1]/div/div[2]/div/div[1]/div/div/div[8]/div/div/div/div[2]/button"
-        return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[2]/div/div[1]/div/div[2]/div/div[1]/div/div/div[8]/div/div/div/div[3]/ul/li[1]"
-
-    @staticmethod
-    def get_sel_country(xpath: str = ElementEnum.BUTTON_XPATH) -> str:
-        if xpath == ElementEnum.BUTTON_XPATH:
-            return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[2]/div/div[1]/div/div[2]/div/div[1]/div/div/div[9]/div/div/div/div[2]/button"
-        return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[2]/div/div[1]/div/div[2]/div/div[1]/div/div/div[9]/div/div/div/div[3]/ul/li[3]"
-
-    @staticmethod
-    def get_sel_fabricant(xpath: str = ElementEnum.BUTTON_XPATH) -> str:
-        if xpath == ElementEnum.BUTTON_XPATH:
-            return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[2]/div/div[1]/div/div[2]/div/div[1]/div/div/div[10]/div/div/div/div[2]/button"
-        return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[2]/div/div[1]/div/div[2]/div/div[1]/div/div/div[10]/div/div/div/div[3]/ul/li[1]"
-
-    @staticmethod
-    def get_id_principio_activo() -> str:
-        return "id-Principio Activo"
-
-    @staticmethod
-    def get_id_nomenclatura_extra() -> str:
-        return "id-Nomenclatura Extra"
-
-    @staticmethod
-    def get_id_numero_de_tintas() -> str:
-        return "id-Número de tintas"
-
-    @staticmethod
-    def get_id_acabados_especiales() -> str:
-        return "id-Acabados especiales"
-
-    @staticmethod
-    def get_id_laetus() -> str:
-        return "id-LAETUS"
-
-    @staticmethod
-    def get_sel_marcas_visuales(xpath: str = ElementEnum.BUTTON_XPATH) -> str:
-        if xpath == ElementEnum.BUTTON_XPATH:
-            return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[2]/div/div[1]/div/div[2]/div/div[3]/div/div/div[2]/div/div/div/div[2]/button"
-        return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[2]/div/div[1]/div/div[2]/div/div[3]/div/div/div[2]/div/div/div/div[3]/ul/li[1]"
-
-    @staticmethod
-    def get_id_referencia_al_libro() -> str:
-        return "id-Referencia al libro de estilo"
-
-    @staticmethod
-    def get_id_comentarios_opcionales() -> str:
-        return "id-Comentarios Opcionales de Material"
+            return ""
+        return ""
