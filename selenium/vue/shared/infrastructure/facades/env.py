82604ca6c8efcf20fc47006e141930c8e5fb3c41
ioedumac <eacevedof@yahoo.es>
2023-01-30 21:00:36 +0100

    .env

--- selenium/vue/shared/infrastructure/facades/env.py
+++ selenium/vue/shared/infrastructure/facades/env.py
@@ -2,7 +2,9 @@ import os
 
 ENV_HOME = "HOME"
 ENV_PATHPRJ = "PATHPRJ"
+ENV_DEV_URL = "DEV_URL"
+ENV_PRE_URL = "PRE_URL"
 
 
-def getenv(var: str) -> str:
-    return os.environ.get(var, "")
+def getenv(var: str, default: str = "") -> str:
+    return os.environ.get(var, default)
