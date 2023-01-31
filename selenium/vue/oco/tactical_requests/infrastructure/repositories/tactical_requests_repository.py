48fb64f763630706f89cd5f32bd6e0eaddc08b52
Eduardo A. F <eacevedo@lacia.com>
2023-01-31 11:07:53 +0100

    new-dev solicitud

--- selenium/vue/oco/tactical_requests/infrastructure/repositories/tactical_requests_repository.py
+++ selenium/vue/oco/tactical_requests/infrastructure/repositories/tactical_requests_repository.py
@@ -9,7 +9,10 @@ class TacticalRequestsRepository:
     def get_sel_request_type(xpath: str = ElementEnum.BUTTON_XPATH) -> str:
         if xpath == ElementEnum.BUTTON_XPATH:
             return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[1]/div/div[2]/div[1]/div[1]/div/div/div/div[2]/button"
-        return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[1]/div/div[2]/div[1]/div[1]/div/div/div/div[3]/ul/li[1]"
+        # new development
+        return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[1]/div/div[2]/div[1]/div[1]/div/div/div/div[3]/ul/li[2]"
+        # return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[1]/div/div[2]/div[1]/div[1]/div/div/div/div[3]/ul/li[1]"
+
     @staticmethod
     def get_sel_asset(xpath: str = ElementEnum.BUTTON_XPATH) -> str:
         if xpath == ElementEnum.BUTTON_XPATH:
