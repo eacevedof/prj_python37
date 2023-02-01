5d8ee245a60b13cbf5731b38e0c62131c7195298
ioedumac <eacevedof@yahoo.es>
2023-02-01 21:20:41 +0100

    keyline

--- selenium/vue/oco/tactical_requests/infrastructure/repositories/tactical_requests_repository.py
+++ selenium/vue/oco/tactical_requests/infrastructure/repositories/tactical_requests_repository.py
@@ -6,24 +6,24 @@ class TacticalRequestsRepository:
         pass
 
     @staticmethod
-    def get_sel_request_type(xpath: str = ElementEnum.BUTTON_XPATH) -> str:
+    def get_sel_request_type_new_dev(xpath: str = ElementEnum.BUTTON_XPATH) -> str:
         if xpath == ElementEnum.BUTTON_XPATH:
             return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[1]/div/div[2]/div[1]/div[1]/div/div/div/div[2]/button"
         # new development
         return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[1]/div/div[2]/div[1]/div[1]/div/div/div/div[3]/ul/li[2]"
-        # return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[1]/div/div[2]/div[1]/div[1]/div/div/div/div[3]/ul/li[1]"
+
+    @staticmethod
+    def get_sel_request_type_material_modify(xpath: str = ElementEnum.BUTTON_XPATH) -> str:
+        if xpath == ElementEnum.BUTTON_XPATH:
+            return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[1]/div/div[2]/div[1]/div[1]/div/div/div/div[2]/button"
+        # material modification
+        return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[1]/div/div[2]/div[1]/div[1]/div/div/div/div[3]/ul/li[1]"
 
     @staticmethod
     def get_sel_asset(xpath: str = ElementEnum.BUTTON_XPATH) -> str:
         if xpath == ElementEnum.BUTTON_XPATH:
             return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[1]/div/div[2]/div[1]/div[2]/div/div/div/div[2]/button"
         return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[1]/div/div[2]/div[1]/div[2]/div/div/div/div[3]/ul/li[1]"
-
-    @staticmethod
-    def get_sel_asset_(xpath: str = ElementEnum.BUTTON_XPATH) -> str:
-        if xpath == ElementEnum.BUTTON_XPATH:
-            return ""
-        return ""
 
     @staticmethod
     def get_xpath_button_save() -> str:
