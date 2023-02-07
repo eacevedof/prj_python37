ae3240c6e8c9d16c191f1913709a3449920d75e3
Eduardo A. F <eacevedo@lacia.com>
2023-02-07 17:01:52 +0100

    keyline

--- selenium/vue/oco/assets/infrastructure/repositories/asset_attributes_repository.py
+++ selenium/vue/oco/assets/infrastructure/repositories/asset_attributes_repository.py
@@ -6,10 +6,15 @@ class AssetAttributesRepository:
         pass
 
     @staticmethod
-    def get_sel_asset_type(xpath: str = ElementEnum.BUTTON_XPATH) -> str:
+    def get_sel_asset_type_material(xpath: str = ElementEnum.BUTTON_XPATH) -> str:
         if xpath == ElementEnum.BUTTON_XPATH:
             return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[1]/div/div[2]/div[3]/div/div/div/div[2]/button"
         return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[1]/div/div[2]/div[3]/div/div/div/div[3]/ul/li[1]"
+    @staticmethod
+    def get_sel_asset_type_keyline(xpath: str = ElementEnum.BUTTON_XPATH) -> str:
+        if xpath == ElementEnum.BUTTON_XPATH:
+            return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[1]/div/div[2]/div[3]/div/div/div/div[2]/button"
+        return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[1]/div/div[2]/div[3]/div/div/div/div[3]/ul/li[3]"
 
     @staticmethod
     def get_xpath_material_code() -> str:
