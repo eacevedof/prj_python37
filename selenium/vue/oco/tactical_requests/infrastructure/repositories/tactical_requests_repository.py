385c01f3fd40db7c9502cf34ead34d7597471ba1
Eduardo A. F <eacevedo@lacia.com>
2023-02-10 08:23:03 +0100

    hay que corregir la creacion de solicitud

--- selenium/vue/oco/tactical_requests/infrastructure/repositories/tactical_requests_repository.py
+++ selenium/vue/oco/tactical_requests/infrastructure/repositories/tactical_requests_repository.py
@@ -8,16 +8,16 @@ class TacticalRequestsRepository:
     @staticmethod
     def get_sel_request_type_new_dev(xpath: str = ElementEnum.BUTTON_XPATH) -> str:
         if xpath == ElementEnum.BUTTON_XPATH:
-            return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[1]/div/div[2]/div[1]/div[1]/div/div/div/div[2]/button"
+            return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[2]/div[1]/div/div/div[1]/div[1]/div/div/div/div[2]/button"
         # new development
-        return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[1]/div/div[2]/div[1]/div[1]/div/div/div/div[3]/ul/li[2]"
+        return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[2]/div[1]/div/div/div[1]/div[1]/div/div/div/div[3]/ul/li[2]"
 
     @staticmethod
     def get_sel_request_type_material_modify(xpath: str = ElementEnum.BUTTON_XPATH) -> str:
         if xpath == ElementEnum.BUTTON_XPATH:
-            return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[1]/div/div[2]/div[1]/div[1]/div/div/div/div[2]/button"
+            return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[2]/div[1]/div/div/div[1]/div[1]/div/div/div/div[2]/button"
         # material modification
-        return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[1]/div/div[2]/div[1]/div[1]/div/div/div/div[3]/ul/li[1]"
+        return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[2]/div[1]/div/div/div[1]/div[1]/div/div/div/div[3]/ul/li[1]"
 
     @staticmethod
     def get_sel_asset(xpath: str = ElementEnum.BUTTON_XPATH) -> str:
