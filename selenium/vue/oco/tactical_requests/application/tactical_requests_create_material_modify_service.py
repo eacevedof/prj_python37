2ff2e298009f92877e63ae44640f93f4dde4a080
ioedumac <eacevedof@yahoo.es>
2023-02-11 11:52:33 +0100

    asset sel

--- selenium/vue/oco/tactical_requests/application/tactical_requests_create_material_modify_service.py
+++ selenium/vue/oco/tactical_requests/application/tactical_requests_create_material_modify_service.py
@@ -51,13 +51,13 @@ def __config_request_type(dom: Dom) -> None:
     btn_xpath = TacticalRequestsRepository.get_sel_request_type_material_modify()
     li_xpath = TacticalRequestsRepository.get_sel_request_type_material_modify(ElementEnum.LI_XPATH)
     dd.select_by_xpath(btn_xpath, li_xpath)
-    sleep(40)
+    sleep(25)
 
     # activo
     btn_xpath = TacticalRequestsRepository.get_sel_asset()
     li_xpath = TacticalRequestsRepository.get_sel_asset(ElementEnum.LI_XPATH)
     dd.select_by_xpath(btn_xpath, li_xpath)
-    sleep(10)
+    sleep(25)
 
 
 def __requests_details(dom: Dom) -> None:
