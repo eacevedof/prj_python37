5df52e559212ee978202cdc61702011eebdfe674
ioedumac <eacevedof@yahoo.es>
2023-02-11 11:34:37 +0100

    asset sel

--- selenium/vue/oco/tactical_requests/infrastructure/repositories/tactical_requests_repository.py
+++ selenium/vue/oco/tactical_requests/infrastructure/repositories/tactical_requests_repository.py
@@ -22,7 +22,7 @@ class TacticalRequestsRepository:
     @staticmethod
     def get_sel_asset(xpath: str = ElementEnum.BUTTON_XPATH) -> str:
         if xpath == ElementEnum.BUTTON_XPATH:
-            return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[2]/div[1]/div/div/div[1]/div[2]/div/div/div/div[2]/button[2]"
+            return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[2]/div[1]/div/div/div[1]/div[2]/div/div/div/div[2]/button"
         return "/html/body/div[1]/main/div/div[1]/div[3]/section/div[2]/div/div[2]/div[1]/div/div/div[1]/div[2]/div/div/div/div[3]/ul/li[1]"
 
     @staticmethod
