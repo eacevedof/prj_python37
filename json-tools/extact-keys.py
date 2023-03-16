720a9a60700219ee87656ae59756bf57a1d273db
Eduardo A. F <eacevedo@lacia.com>
2023-03-16 15:41:01 +0100

    json

---
+++ json-tools/extact-keys.py
@@ -0,0 +1,22 @@
+import json
+
+path_json = "/Users/eduardoacevedo/projects/temper/json/a.json"
+
+def file_get_contents(filename):
+    with open(filename, 'r') as f:
+        # Cargar el contenido del archivo en una variable de Python
+        datos = json.load(f)
+    return datos
+
+data = file_get_contents(path_json)
+
+# Extraer las claves
+keys = set()
+for value in data.values():
+    keys |= set(value.keys())
+    ks = value.keys()
+    for k in ks:
+        keys |= set(value[k].keys())
+
+for k in keys:
+    print(k)
