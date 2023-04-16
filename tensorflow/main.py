c14213fa9dfca1a4f274a24c84fb2790aa8a2705
ioedumac <eacevedof@yahoo.es>
2023-04-16 14:58:27 +0200

    ejercicio

--- tensorflow/main.py
+++ tensorflow/main.py
@@ -5,3 +5,5 @@ import matplotlib.pyplot as plt
 
 # entradas con numeros de pulgadas
 entradas = np.array([1, 6, 30, 7, 70, 43, 503, 201, 1005, 99], dtype=float)
+resultados = np.array([0.0254, 0.1524, 0.762, 0.1778, 1.778, 1.0922, 12.776, 5.1054, 25.527, 2.514], dtype=float)
+
