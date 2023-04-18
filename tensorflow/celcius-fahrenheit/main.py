f9304827d0ba8f565530e3a142598abba2f9fb5c
ioedumac <eacevedof@yahoo.es>
2023-04-18 23:04:23 +0200

    tensor farentheit

--- tensorflow/celcius-fahrenheit/main.py
+++ tensorflow/celcius-fahrenheit/main.py
@@ -6,3 +6,9 @@ fahrenheit = np.array([-40, 14, 32, 46, 59, 72, 100], dtype=float)
 
 capa = tf.keras.layers.Dense(units=1, input_shape=[1])
 modelo = tf.keras.Sequential([capa])
+
+modelo.compile(
+    # concretamente, Adam le permite a la red ajuste los pesos y sesgos de manera eficiente
+    # de modo que aprenda y no desaprenda
+    optimizer=tf.keras.optimizers.Adam()
+)
