555f9dd87e7a5e72cba82a241930664e54b1610e
ioedumac <eacevedof@yahoo.es>
2023-04-18 23:37:18 +0200

    tensor farentheit

--- tensorflow/celcius-fahrenheit/main.py
+++ tensorflow/celcius-fahrenheit/main.py
@@ -4,8 +4,13 @@ import numpy as np
 celsius = np.array([-40, -10, 0, 8, 15, 22, 38], dtype=float)
 fahrenheit = np.array([-40, 14, 32, 46, 59, 72, 100], dtype=float)
 
-capa = tf.keras.layers.Dense(units=1, input_shape=[1])
-modelo = tf.keras.Sequential([capa])
+#capa = tf.keras.layers.Dense(units=1, input_shape=[1])
+#modelo = tf.keras.Sequential([capa])
+
+oculta1 = tf.keras.layers.Dense(units=3, input_shape=[1])
+oculta2 = tf.keras.layers.Dense(units=3)
+salida = tf.keras.layers.Dense(units=1)
+modelo = tf.keras.Sequential([oculta1, oculta2, salida])
 
 tasa_de_aprendizaje = 0.1
 
@@ -26,8 +31,8 @@ print("modelo entrenado")
 #reultado de la funcion de perdida
 import matplotlib.pyplot as plt
 
-plt.xlabel("# epochs")
-plt.ylabel("perdida")
+plt.xlabel("# ciclos (epochs)")
+plt.ylabel("magnitud de perdida")
 plt.plot(historial.history["loss"])
 plt.show()
 
@@ -37,4 +42,6 @@ print("100 C en fahrenheit")
 print(result)
 
 print("peso, sesgo")
-print(capa.get_weights())
+print(oculta1.get_weights())
+print(oculta2.get_weights())
+print(salida.get_weights())
