c9929193dfdfc4660e1c9f86799777bc627d20e3
ioedumac <eacevedof@yahoo.es>
2023-04-18 23:02:37 +0200

    tensor farentheit

---
+++ tensorflow/celcius-fahrenheit/main.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+import numpy as np
+
+celsius = np.array([-40, -10, 0, 8, 15, 22, 38], dtype=float)
+fahrenheit = np.array([-40, 14, 32, 46, 59, 72, 100], dtype=float)
+
+capa = tf.keras.layers.Dense(units=1, input_shape=[1])
+modelo = tf.keras.Sequential([capa])
--- tensorflow/celcius-farenheit/main.py
+++
@@ -1,5 +0,0 @@
-import tensorflow as tf
-import numpy as np
-
-celsius = np.array([-40, -10, 0, 8, 15, 22, 38])
-fahrenheit = np.array([])
