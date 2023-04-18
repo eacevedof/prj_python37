d44a921dd09b3c91006a937b62512cb91ede7b12
ioedumac <eacevedof@yahoo.es>
2023-04-18 23:16:50 +0200

    tensor farentheit

--- tensorflow/celcius-fahrenheit/main.py
+++ tensorflow/celcius-fahrenheit/main.py
@@ -7,8 +7,22 @@ fahrenheit = np.array([-40, 14, 32, 46, 59, 72, 100], dtype=float)
 capa = tf.keras.layers.Dense(units=1, input_shape=[1])
 modelo = tf.keras.Sequential([capa])
 
+tasa_de_aprendizaje = 0.1
+
 modelo.compile(
     # concretamente, Adam le permite a la red ajuste los pesos y sesgos de manera eficiente
     # de modo que aprenda y no desaprenda
-    optimizer=tf.keras.optimizers.Adam()
+    optimizer=tf.keras.optimizers.Adam(tasa_de_aprendizaje),
+    # error de media cuadratica. Una pequeña cantidad de grandes errores es peor que una cantidad grande de pequeños errores
+    loss="mean_squared_error"
 )
+
+ciclos_de_aprendizaje = 1000
+# entrenamiento
+historial = modelo.fit(celsius, fahrenheit, epochs=ciclos_de_aprendizaje, verbose=False)
+
+print("modelo entrenado")
+
+#reultado de la funcion de perdida
+import matplotlib.pyplot as plt
+
