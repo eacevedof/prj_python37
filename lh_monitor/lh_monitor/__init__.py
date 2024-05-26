b7c88c627028c333dc06566011ee8328b0679803
ioedumac <eacevedof@yahoo.es>
2024-05-26 23:00:02 +0200

    arranca pero con error en ruta en pycharm

--- lh_monitor/lh_monitor/__init__.py
+++ lh_monitor/lh_monitor/__init__.py
@@ -4,3 +4,4 @@ import sys
 lh_monitor_dir = os.path.dirname(os.path.realpath(__file__))
 # sys.path.append(lh_monitor_dir)
 sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath("."))
