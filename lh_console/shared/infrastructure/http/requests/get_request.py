965ca7078e3d66e3758bca09de8f69dcb8ff407c
ioedumac <eacevedof@yahoo.es>
2024-05-26 21:31:31 +0200

    dashboard

--- lh_console/shared/infrastructure/http/requests/get_request.py
+++ lh_console/shared/infrastructure/http/requests/get_request.py
@@ -1,6 +1,14 @@
 import requests
+import json
 
 
-def http_get(url: str) -> str:
+def http_get(url: str) -> dict:
     response = requests.get(url)
-    return response.text
+    return __get_text_as_dict(response.text)
+
+
+def __get_text_as_dict(text: str) -> dict:
+    try:
+        return json.loads(text)
+    except Exception as e:
+        return {"error": str(e)}
