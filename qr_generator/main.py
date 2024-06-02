d3fed6ef9c54045ed894f3805445cf6d8a8ca161
ioedumac <eacevedof@yahoo.es>
2024-06-02 17:11:07 +0200

    qr-generator

--- qr_generator/main.py
+++ qr_generator/main.py
@@ -18,10 +18,10 @@ qr_code.make_image(fill="black", back_color="white").save("./qr_images/qr_url.pn
 qr_image = qr_code.make_image(fill="black", back_color="white")
 image_logo = Image.open("./logos/chalan.jpg")
 
-logo_size = min(qr_image.size)
+logo_size = min(qr_image.size) // 5
 logo_pos = ((qr_image.size[0] - logo_size) // 2, (qr_image.size[1] - logo_size) // 2)
 
 image_logo = image_logo.resize((logo_size, logo_size))
 qr_image.paste(image_logo, logo_pos)
 
-qr_image.save("./qr_images/qr_with_logo.png")
+qr_image.save("./qr_images/qr_url_logo.png")
