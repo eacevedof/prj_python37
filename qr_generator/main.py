c3676fee8d85b1ef0e12481ed6008c6452c7ba1d
ioedumac <eacevedof@yahoo.es>
2024-06-02 20:50:54 +0200

    backup

--- qr_generator/main.py
+++ qr_generator/main.py
@@ -1,38 +1,56 @@
-
 import qrcode
-from PIL import Image
+from PIL import Image, ImageDraw
 
 css = {
     "background": (208, 31, 47),
     "color": (255, 255, 255)
 }
 
-# Generate QR code
-qr_code = qrcode.QRCode(
-    version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_H,
-    box_size=10,
-    border=2,
-)
-url = "http://elchalanaruba.com/la-carta-con-precios"
-qr_code.add_data(url)
-qr_code.make(fit=True)
 
-# qr colo con url
-#qr_code.make_image(back_color=css.get("background"), fill_color=css.get("color")).save("./qr_images/qr_url.png")
+def _get_qr_code_with_url() -> qrcode.QRCode:
+    # Generate QR code
+    qr_code = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_H,
+        box_size=10,
+        border=2,
+    )
+    url = "http://elchalanaruba.com/la-carta-con-precios"
+    qr_code.add_data(url)
+    qr_code.make(fit=True)
+    return qr_code
 
-# image
-qr_image = qr_code.make_image(
-    back_color=css.get("background"),
-    fill_color=css.get("color")
-)
 
-image_logo = Image.open("./logos/chalan.jpg")
+def _get_qr_image_from_qr_code(qr_code: qrcode.QRCode) -> Image:
+    # Generate QR image
+    qr_image = qr_code.make_image(
+        back_color=css.get("background"),
+        fill_color=css.get("color")
+    )
 
-logo_size = min(qr_image.size) // 5
-logo_pos = ((qr_image.size[0] - logo_size) // 2, (qr_image.size[1] - logo_size) // 2)
+    # Generate mask
+    mask = Image.new("L", qr_image.size, 0)
+    draw = ImageDraw.Draw(mask)
+    draw.rounded_rectangle([0, 0, *qr_image.size], radius=20, fill=255)
 
-image_logo = image_logo.resize((logo_size, logo_size))
-qr_image.paste(image_logo, logo_pos)
+    # Apply mask
+    qr_image.putalpha(mask)
+    return qr_image
 
-qr_image.save("./qr_images/qr_url_logo.png")
+
+def _append_logo_to_qr_image(qr_image: Image) -> Image:
+    image_logo = Image.open("./logos/chalan-head.png")
+    #image_logo = Image.open("./logos/chalan-full.png")
+    logo_size = min(qr_image.size) // 5
+    logo_pos = ((qr_image.size[0] - logo_size) // 2, (qr_image.size[1] - logo_size) // 2)
+
+    image_logo = image_logo.resize((logo_size, logo_size))
+    qr_image.paste(image_logo, logo_pos)
+    return qr_image
+
+
+if __name__ == "__main__":
+    qr_code = _get_qr_code_with_url()
+    qr_image = _get_qr_image_from_qr_code(qr_code)
+    _append_logo_to_qr_image(qr_image)
+    qr_image.save("./qr_images/qr_url.png")
