63229bc7f0f902a5d44a3c82da9ecd3e6d0463d5
ioedumac <eacevedof@yahoo.es>
2024-06-02 16:57:13 +0200

    qr-generator

--- qr_generator/main.py
+++ qr_generator/main.py
@@ -2,14 +2,14 @@ import qrcode
 from PIL import Image
 
 # Generate QR code
-
 qr = qrcode.QRCode(
     version=1,
     error_correction=qrcode.constants.ERROR_CORRECT_H,
     box_size=10,
     border=4,
 )
-qr.add_data("Some data")
+url = "http://elchalanaruba.com/la-carta-con-precios"
+qr.add_data(url)
 qr.make(fit=True)
 
 img_qr = qr.make_image(fill="black", back_color="white")
