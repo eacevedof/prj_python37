a94174634e4384b41d308c2900fd51b2322ec129
Eduardo A.F. <eacevedof@yahoo.es>
2024-09-14 19:35:20 +0200

    wa-chatgpt

--- wa-chtgpt/wa-chatgpt/main.py
+++ wa-chtgpt/wa-chatgpt/main.py
@@ -1,10 +1,10 @@
 """
 
 """
-import sys; sys.path.append("..")
+#import sys; sys.path.append("..")
+import openai
 from config.config import CHATGPT_API_KEY
 
-import openai
 openai.api_key = CHATGPT_API_KEY
 model_engine = "text-davinci-003"
 prompt = "la suma de 5 mas 5"
@@ -16,3 +16,8 @@ completion = openai.Completion.create(
     stop=None,
     temperature=0.7
 )
+
+respuesta = ""
+for choice in completion.choices:
+    respuesta = respuesta + choice.text.strip()
+    print(f"response: {respuesta}")
