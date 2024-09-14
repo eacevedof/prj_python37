48cc488124fa66e2dc0adc3c1109a2a29ec226ee
Eduardo A.F. <eacevedof@yahoo.es>
2024-09-14 19:33:46 +0200

    wa-chatgpt

--- wa-chtgpt/wa-chatgpt/main.py
+++ wa-chtgpt/wa-chatgpt/main.py
@@ -2,9 +2,17 @@
 
 """
 import sys; sys.path.append("..")
-
-print("Hello, World!")
+from config.config import CHATGPT_API_KEY
 
 import openai
-openai.api_key = "sk-1234567890abcdef1234567890abcdef"
-modal_engine = "text-davinci-003"
+openai.api_key = CHATGPT_API_KEY
+model_engine = "text-davinci-003"
+prompt = "la suma de 5 mas 5"
+completion = openai.Completion.create(
+    engine=model_engine,
+    prompt=prompt,
+    max_tokens=1024,
+    n=1,
+    stop=None,
+    temperature=0.7
+)
