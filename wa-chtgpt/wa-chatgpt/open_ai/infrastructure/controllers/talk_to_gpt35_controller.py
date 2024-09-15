eff84df1fd24359e99acb543749e750e6a794957
Eduardo A.F. <eacevedof@yahoo.es>
2024-09-15 19:29:39 +0200

    wa-chatgpt gpt35

--- wa-chtgpt/wa-chatgpt/open_ai/infrastructure/controllers/talk_to_gpt35_controller.py
+++ wa-chtgpt/wa-chatgpt/open_ai/infrastructure/controllers/talk_to_gpt35_controller.py
@@ -1,6 +1,6 @@
 from flask import jsonify, Response, request
 from open_ai.application.talk_to_gpt35.talk_to_gpt35_dto import TalkToGpt35DTO
-from open_ai.application.talk_to_gpt35.talk_to_gpt35_service import invoke as talk_to_gpt35_service
+from open_ai.application.talk_to_gpt35.talk_to_gpt35_service import talk_to_gpt35_service
 
 def invoke(http_request: request) -> Response:
     talk_to_gpt35_dto = TalkToGpt35DTO(
