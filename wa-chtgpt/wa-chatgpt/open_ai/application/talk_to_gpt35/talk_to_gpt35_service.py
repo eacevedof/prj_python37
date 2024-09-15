b0e86b526cfe97eab48d096604537524abdd637b
Eduardo A.F. <eacevedof@yahoo.es>
2024-09-15 19:29:39 +0200

    wa-chatgpt gpt35

--- wa-chtgpt/wa-chatgpt/open_ai/application/talk_to_gpt35/talk_to_gpt35_service.py
+++ wa-chtgpt/wa-chatgpt/open_ai/application/talk_to_gpt35/talk_to_gpt35_service.py
@@ -5,7 +5,7 @@ from application.talk_to_gpt35.talked_to_gpt35_dto import TalkedToGpt35DTO
 from infrastructure.repositories.openai_repository import get_gpt35_turbo
 
 
-def invoke(talk_to_gpt35_dto: TalkToGpt35DTO) -> TalkedToGpt35DTO:
+def talk_to_gpt35_service(talk_to_gpt35_dto: TalkToGpt35DTO) -> TalkedToGpt35DTO:
     prompt = talk_to_gpt35_dto.question
     chat_response = get_gpt35_turbo(prompt)
     return TalkedToGpt35DTO(chat_response=chat_response)
