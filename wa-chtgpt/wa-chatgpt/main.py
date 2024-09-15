fee7ab50fa89654e9566ef78907ef5676ce4d067
Eduardo A.F. <eacevedof@yahoo.es>
2024-09-15 13:45:39 +0200

    wa-chatgpt

--- wa-chtgpt/wa-chatgpt/main.py
+++ wa-chtgpt/wa-chatgpt/main.py
@@ -12,27 +12,21 @@ from config.config import OPENAI_API_KEY
 
 model_engine = "gpt-3.5-turbo"
 model_engine = "gpt-4o"
-
 prompt = "la suma de 5 mas 5"
-completion = openai.Completion.create(
-    engine=model_engine,
-    prompt=prompt,
-    max_tokens=1024,
-    n=1,
-    stop=None,
-    temperature=0.7
-)
 
 clientOpenAI = OpenAI(
     api_key=OPENAI_API_KEY
 )
 completion = clientOpenAI.chat.completions.create(
     model = model_engine,
-
+    max_tokens=1024,
+    n=1,
+    stop=None,
+    temperature=0.7,
     messages = [
         {
             "role": "user",
-            "content": "write a haiku about ai"
+            "content": prompt
         }
     ]
 )
