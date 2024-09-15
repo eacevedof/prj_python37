8ffd42853e49fa56280ed8205c2ca24cbce66360
Eduardo A.F. <eacevedof@yahoo.es>
2024-09-15 18:36:23 +0200

    wa-chatgpt: health-check

---
+++ wa-chtgpt/wa-chatgpt/open_ai/infrastructure/repositories/abstract_openai_repository.py
@@ -0,0 +1,25 @@
+from openai import OpenAI
+from config.config import OPENAI_API_KEY
+
+def get_gpt35_turbo(prompt: str) -> str:
+    model_engine = "gpt-3.5-turbo"
+    client_open_ai = __get_client_openai()
+    chat_completion = client_open_ai.chat.completions.create(
+        model=model_engine,
+        max_tokens=250,
+        n=1, # numero de respuestas
+        stop=None,
+        temperature=0.7, # nivel de creatividad moderado [0,1]
+        messages=[
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
+    )
+    return chat_completion.choices[0].message["content"]
+
+def __get_client_openai() -> OpenAI:
+    return OpenAI(
+        api_key = OPENAI_API_KEY
+    )
