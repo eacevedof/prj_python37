dfb0e36fce7ef6fb7384832079703a63b5667dd0
Eduardo A.F. <eacevedof@yahoo.es>
2024-09-15 16:20:07 +0200

    wa-chatgpt: asigref error

--- wa-chtgpt/wa-chatgpt/main.py
+++ wa-chtgpt/wa-chatgpt/main.py
@@ -10,17 +10,6 @@ def index():
     }
     return jsonify(response)
 
-@app.route("/get-data", methods=["GET"])
-def get_data():
-    param1 = request.args.get("param1")
-    param2 = request.args.get("param2")
-    response = {
-        "param1": param1,
-        "param2": param2,
-        "message": "GET request received successfully!"
-    }
-    return jsonify(response)
-
 if __name__ == "__main__":
     import uvicorn
     asgi_app = WsgiToAsgi(app)
