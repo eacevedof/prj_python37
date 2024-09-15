5ea7ce07bcd8ebeb1490a9b84cfccb0ee0fff99d
Eduardo A.F. <eacevedof@yahoo.es>
2024-09-15 19:55:32 +0200

    wa-chatgpt talked

--- wa-chtgpt/wa-chatgpt/open_ai/application/talk_to_gpt35/talked_to_gpt35_dto.py
+++ wa-chtgpt/wa-chatgpt/open_ai/application/talk_to_gpt35/talked_to_gpt35_dto.py
@@ -3,3 +3,8 @@ from dataclasses import dataclass
 @dataclass(frozen=True)
 class TalkedToGpt35DTO:
     chat_response: str
+
+    def __dict__(self):
+        return {
+            "chat_response": self.chat_response
+        }
