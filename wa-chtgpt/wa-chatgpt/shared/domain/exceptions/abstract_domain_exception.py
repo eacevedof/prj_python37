4895cd9348f690291bfcd289aec27b4af9ab967b
Eduardo A.F. <eacevedof@yahoo.es>
2024-09-15 22:15:00 +0200

    wa-chatgpt components

--- wa-chtgpt/wa-chatgpt/shared/domain/exceptions/abstract_domain_exception.py
+++ wa-chtgpt/wa-chatgpt/shared/domain/exceptions/abstract_domain_exception.py
@@ -1,15 +1,4 @@
-from shared.domain.enums.http_response_code_enum import HttpResponseCodeEnum
-
 from abc import ABC
 
 class AbstractDomainException(Exception, ABC):
     pass
-
-class GetTicketByTicketNumberException(AbstractDomainException):
-
-    @staticmethod
-    def ticket_not_found():
-        raise GetTicketByTicketNumberException(
-            "tickets-tr.ticket-not-found",
-            HttpResponseCodeEnum.NOT_FOUND.value
-        )
