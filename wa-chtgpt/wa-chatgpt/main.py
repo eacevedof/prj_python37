8c41f746941f5a07dfaf55d72e25a5b739f02a78
Eduardo A.F. <eacevedof@yahoo.es>
2024-09-15 15:04:49 +0200

    wa-chatgpt uvicorn

--- wa-chtgpt/wa-chatgpt/main.py
+++ wa-chtgpt/wa-chatgpt/main.py
@@ -2,20 +2,20 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/get-data', methods=['GET'])
+@app.route("/get-data", methods=["GET"])
 def get_data():
     # Retrieve query parameters
-    param1 = request.args.get('param1')
-    param2 = request.args.get('param2')
+    param1 = request.args.get("param1")
+    param2 = request.args.get("param2")
 
     # Process the parameters and create a response
     response = {
-        'param1': param1,
-        'param2': param2,
-        'message': 'GET request received successfully!'
+        "param1": param1,
+        "param2": param2,
+        "message": "GET request received successfully!"
     }
 
     return jsonify(response)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
