46a0f6ce71d1dfe2d1ffd60e29088c772ea1f88c
Eduardo A.F. <eacevedof@yahoo.es>
2024-09-15 18:21:01 +0200

    wa-chatgpt: health-check

--- wa-chtgpt/wa-chatgpt/open_ai/application/talk_to_gpt35/talk_to_gpt35_dto.py
+++ wa-chtgpt/wa-chatgpt/open_ai/application/talk_to_gpt35/talk_to_gpt35_dto.py
@@ -3,8 +3,4 @@ from typing import List
 
 @dataclass(frozen=True)
 class TalkToGpt35DTO:
-    prompt: str
-    max_tokens: int
-    n: int
-    stop: List[str]
-    temperature: float
+    question: str
