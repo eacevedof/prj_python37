ec8255b238468e4ecf33c9eed6d8dc5f1fbfe355
Eduardo A.F. <eacevedof@yahoo.es>
2024-09-15 13:46:05 +0200

    wa-chatgpt

--- wa-chtgpt/wa-chatgpt/main.py
+++ wa-chtgpt/wa-chatgpt/main.py
@@ -3,27 +3,21 @@
 """
 import os
 from openai import OpenAI
-
-
-
-#import sys; sys.path.append("..")
-
 from config.config import OPENAI_API_KEY
 
 model_engine = "gpt-3.5-turbo"
-model_engine = "gpt-4o"
 prompt = "la suma de 5 mas 5"
 
 clientOpenAI = OpenAI(
-    api_key=OPENAI_API_KEY
+    api_key = OPENAI_API_KEY
 )
 completion = clientOpenAI.chat.completions.create(
-    model = model_engine,
+    model=model_engine,
     max_tokens=1024,
     n=1,
     stop=None,
     temperature=0.7,
-    messages = [
+    messages=[
         {
             "role": "user",
             "content": prompt
@@ -31,8 +25,7 @@ completion = clientOpenAI.chat.completions.create(
     ]
 )
 
-
 respuesta = ""
 for choice in completion.choices:
-    respuesta = respuesta + choice.text.strip()
+    respuesta = respuesta + choice.message['content'].strip()
     print(f"response: {respuesta}")
