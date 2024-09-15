a93a453fdf5876f3e665b11d398ee9e48190eb67
Eduardo A.F. <eacevedof@yahoo.es>
2024-09-15 18:03:10 +0200

    wa-chatgpt: health-check

--- wa-chtgpt/wa-chatgpt/main.py
+++ wa-chtgpt/wa-chatgpt/main.py
@@ -1,22 +1,1 @@
-from flask import Flask, request, jsonify, render_template_string
-from asgiref.wsgi import WsgiToAsgi
-
-app_flask = Flask(__name__)
-
-@app_flask.route("/", methods=["GET"])
-async def get_documentation_html():
-    from api_doc.infrastructure.controllers.get_documentation_controller import invoke
-    return invoke(request)
-
-
-@app_flask.route("/api/v1/health-check", methods=["GET"])
-async def get_health_check():
-    from health_check.infrastructure.controllers.get_health_check_controller import invoke
-    return invoke(request)
-
-
-if __name__ == "__main__":
-    import uvicorn
-    async_flask = WsgiToAsgi(app_flask)
-    # uvicorn.run(app_flask, host="0.0.0.0", port=3000, log_level="info")
-    uvicorn.run(async_flask, host="0.0.0.0", port=3000, log_level="info")
+from flask import Flask, request, jsonify, render_template_stringfrom asgiref.wsgi import WsgiToAsgiapp_flask = Flask(__name__)@app_flask.route("/", methods=["GET"])async def get_documentation_html():    from api_doc.infrastructure.controllers.get_documentation_controller import invoke    return invoke(request)@app_flask.route("/api/v1/health-check", methods=["GET"])async def get_health_check():    from health_check.infrastructure.controllers.get_health_check_controller import invoke    return invoke(request)if __name__ == "__main__":    import uvicorn    async_flask = WsgiToAsgi(app_flask)    # uvicorn.run(app_flask, host="0.0.0.0", port=3000, log_level="info")    uvicorn.run(async_flask, host="0.0.0.0", port=3000, log_level="info")
