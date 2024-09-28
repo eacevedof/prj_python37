089abd8bb3cf57adc629e65b9a85076e153d36bf
Eduardo A.F. <eacevedof@yahoo.es>
2024-09-28 23:00:21 +0200

    pyspark fix routes

--- etl-pyspark/be-api/api_doc/infrastructure/views/documentation_tpl.py
+++ etl-pyspark/be-api/api_doc/infrastructure/views/documentation_tpl.py
@@ -7,29 +7,11 @@ def render() -> str:
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>API documentation</title>
+	<title>API Pyspark</title>
 </head>
 <body>
-<h1>API Documentation</h1>
+<h1>API Pyspark</h1>
 
-<h2>1. Tickets:</h2>
-<code>
-	/api/tickets
-</code>
-<pre>
-curl --location 'https://hola.com' \
-	--header 'Authorization: table-clients-token'
-</pre>
-<h3>Response</h3>
-<pre>
-{
-    "status": 200,
-    "results": {
-        "total_counts": [],
-        "total": 0
-    }
-}
-</pre>
 </body>
 </html>
     """
