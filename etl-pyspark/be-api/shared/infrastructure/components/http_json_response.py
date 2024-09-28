54c6269d9777af587437436c4f45fa11f2986674
Eduardo A.F. <eacevedof@yahoo.es>
2024-09-28 20:55:06 +0200

    etl-pyspark

---
+++ etl-pyspark/be-api/shared/infrastructure/components/http_json_response.py
@@ -0,0 +1,46 @@
+from flask import jsonify, Response
+from dataclasses import dataclass, field
+from typing import Dict, Any
+from shared.domain.enums.http_response_code_enum import HttpResponseCodeEnum
+
+
+@dataclass(frozen=True)
+class HttpJsonResponse:
+    status: str = field(init=False)
+    message: str
+    code: int
+    data: Dict[str, Any]
+
+    def __post_init__(self):
+        object.__setattr__(self, "code", self._validate_code(self.code))
+        object.__setattr__(self, "status", self._get_status_by_code())
+
+    @staticmethod
+    def _validate_code(code: int) -> int:
+        if code < 100 or code > 599:
+            return HttpResponseCodeEnum.INTERNAL_SERVER_ERROR.value
+        return code
+
+    @staticmethod
+    def from_primitives(primitives: Dict[str, Any]) -> 'HttpJsonResponse':
+        code = int(primitives.get("code", HttpResponseCodeEnum.OK.value))
+        message = str(primitives.get("message", ""))
+        data = primitives.get("data", {})
+        return HttpJsonResponse(message=message, code=code, data=data)
+
+    def _get_status_by_code(self) -> str:
+        if str(self.code).startswith("2"):
+            return "success"
+        return "error"
+
+    def get_as_json_response(self) -> Response:
+        return jsonify(self.__to_dict())
+
+    def __to_dict(self) -> Dict[str, Any]:
+        return {
+            "code": self.code,
+            "status": self.status,
+            "message": self.message,
+            "data": self.data
+        }
+
