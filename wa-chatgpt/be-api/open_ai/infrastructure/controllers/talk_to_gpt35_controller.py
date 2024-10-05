25df8164aa37edd71d4819fbadc509f24005ef2b
Eduardo A.F. <eacevedof@yahoo.es>
2024-10-05 15:01:23 +0200

    wa reorg chatgpt

--- wa-chatgpt/be-api/open_ai/infrastructure/controllers/talk_to_gpt35_controller.py
+++ wa-chatgpt/be-api/open_ai/infrastructure/controllers/talk_to_gpt35_controller.py
@@ -1,7 +1,7 @@
 from flask import Response, request
 
 from shared.domain.enums.http_response_code_enum import HttpResponseCodeEnum
-from shared.infrastructure.components.http_json_response import HttpJsonResponse
+from shared.infrastructure.http.response.http_json_response import HttpJsonResponse
 from open_ai.application.talk_to_gpt35.talk_to_gpt35_dto import TalkToGpt35DTO
 from open_ai.application.talk_to_gpt35.talk_to_gpt35_service import talk_to_gpt35_service
 from open_ai.domain.exceptions.talk_to_gpt35_exception import TalkToGpt35Exception
