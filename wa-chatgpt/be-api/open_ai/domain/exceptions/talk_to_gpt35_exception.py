0ead15bd1df8737f58156bbdd7c7cc914f11b7f2
Eduardo A.F. <eacevedof@yahoo.es>
2024-10-06 12:29:25 +0200

    wa

--- wa-chatgpt/be-api/open_ai/domain/exceptions/talk_to_gpt35_exception.py
+++ wa-chatgpt/be-api/open_ai/domain/exceptions/talk_to_gpt35_exception.py
@@ -1,15 +1,19 @@
+from typing import final
 from shared.domain.enums.http_response_code_enum import HttpResponseCodeEnum
 from shared.domain.exceptions.abstract_domain_exception import AbstractDomainException
 
+
+@final
 class TalkToGpt35Exception(AbstractDomainException):
 
-    def __init__(self, message, code):
+    def __init__(self, message: str, code: int) -> None:
         self.message = message
         self.code = code
         super().__init__(self.message)
 
+
     @staticmethod
-    def empty_question():
+    def empty_question() -> None:
         raise TalkToGpt35Exception(
             "open-ai-tr.ticket-not-found",
             HttpResponseCodeEnum.NOT_FOUND.value
