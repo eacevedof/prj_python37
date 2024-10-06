8ef11fd449edeb2e82dfe712651380d00a3a7c41
Eduardo A.F. <eacevedof@yahoo.es>
2024-10-06 16:48:17 +0200

    wa repos

--- wa-chatgpt/be-api/whatsapp/application/send_message/send_message_dto.py
+++ wa-chatgpt/be-api/whatsapp/application/send_message/send_message_dto.py
@@ -2,10 +2,10 @@ from typing import final
 from dataclasses import dataclass
 from typing import List
 
+
 @final
 @dataclass(frozen=True)
 class SendMessageDto:
     to_phone_number: str
-    template_name: str
-    template_language: str
+    message: str
 
