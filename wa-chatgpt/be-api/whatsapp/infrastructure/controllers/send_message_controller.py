837ddf2a7048f248d51a1b63badab8e9b4d3a009
Eduardo A.F. <eacevedof@yahoo.es>
2024-10-06 12:19:05 +0200

    wa

---
+++ wa-chatgpt/be-api/whatsapp/infrastructure/controllers/send_message_controller.py
@@ -0,0 +1,35 @@
+from flask import Response, request
+
+from shared.infrastructure.log import Log
+from shared.domain.enums.http_response_code_enum import HttpResponseCodeEnum
+from shared.infrastructure.http.response.http_json_response import HttpJsonResponse
+
+
+def invoke(http_request: request) -> Response:
+    try:
+        talk_to_gpt35_dto = TalkToGpt35DTO(
+            question=http_request.args["question"]
+        )
+        talked_to_gpt35_dto = talk_to_gpt35_service(
+            talk_to_gpt35_dto
+        )
+
+        return HttpJsonResponse.from_primitives({
+            "code": HttpResponseCodeEnum.OK.value,
+            "message": "open-ai-tr.talk_to_gpt35",
+            "data": {"chat_response": talked_to_gpt35_dto.chat_response}
+        }).get_as_json_response()
+
+    except TalkToGpt35Exception as ex:
+        return HttpJsonResponse.from_primitives({
+            "code": ex.code,
+            "message": ex.message,
+        }).get_as_json_response()
+
+    except Exception as ex:
+        Log.log_exception(ex, "talk_to_gpt35_controller.invoke")
+        return HttpJsonResponse.from_primitives({
+            "code": HttpResponseCodeEnum.INTERNAL_SERVER_ERROR.value,
+            "message": "shared-tr.some-unexpected-error-occurred",
+        }).get_as_json_response()
+
