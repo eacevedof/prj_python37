3a466745ce7365b6fd2955ae1153126a1b983d46
Eduardo A.F. <eacevedof@yahoo.es>
2024-10-07 21:30:57 +0200

    wa langchain

--- wa-chatgpt/be-api/shared/infrastructure/components/text/language.py
+++ wa-chatgpt/be-api/shared/infrastructure/components/text/language.py
@@ -1,5 +1,5 @@
 from langchain.text_splitter import RecursiveTextSplitter
-
+from langchain.embeddings import HuggingFaceEmbeddings
 
 def get_chunks_from_text(text: str) -> list[str]:
     splitter = RecursiveTextSplitter(
@@ -8,3 +8,10 @@ def get_chunks_from_text(text: str) -> list[str]:
         length_function = len
     )
     return splitter.split_text(text)
+
+
+# https://youtu.be/iDrpdkIHMq8?t=549
+def get_embedding_by_minilm():
+    model_name = "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
+    embeddings = HuggingFaceEmbeddings(model_name = model_name)
+    return embeddings
