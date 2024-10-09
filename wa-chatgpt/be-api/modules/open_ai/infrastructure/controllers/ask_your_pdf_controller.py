d0ac632a7fd900d4f9e4f93f4ab4cbfa15273b71
Eduardo A.F. <eacevedof@yahoo.es>
2024-10-09 21:26:36 +0200

    wa ask-your-pdf

--- wa-chatgpt/be-api/modules/open_ai/infrastructure/controllers/ask_your_pdf_controller.py
+++ wa-chatgpt/be-api/modules/open_ai/infrastructure/controllers/ask_your_pdf_controller.py
@@ -3,27 +3,27 @@ from flask import Response, request
 from modules.shared.infrastructure.components.log import Log
 from modules.shared.domain.enums.http_response_code_enum import HttpResponseCodeEnum
 from modules.shared.infrastructure.components.http.response.http_json_response import HttpJsonResponse
-from modules.open_ai.application.ask_your_pdf.ask_your_pdf_dto import TalkToGpt35DTO
-from modules.open_ai.application.ask_your_pdf.ask_your_pdf_service import ask_your_pdf_service
-from modules.open_ai.domain.exceptions.ask_your_pdf_exception import TalkToGpt35Exception
+from modules.open_ai.application.ask_your_pdf.ask_your_pdf_dto import AskYourPdfDto
+from modules.open_ai.application.ask_your_pdf.ask_your_pdf_service import AskYourPdfService
+from modules.open_ai.domain.exceptions.ask_your_pdf_exception import AskYourPdfException
 
 
 def invoke(http_request: request) -> Response:
     try:
-        ask_your_pdf_dto = TalkToGpt35DTO(
+        ask_your_pdf_dto = AskYourPdfDto(
             question=http_request.args["question"]
         )
-        talked_to_gpt35_dto = ask_your_pdf_service(
+        AskedYourPdfDto = AskYourPdfService.invoke(
             ask_your_pdf_dto
         )
 
         return HttpJsonResponse.from_primitives({
             "code": HttpResponseCodeEnum.OK.value,
             "message": "open-ai-tr.ask_your_pdf",
-            "data": {"chat_response": talked_to_gpt35_dto.chat_response}
+            "data": {"chat_response": AskedYourPdfDto.chat_response}
         }).get_as_json_response()
 
-    except TalkToGpt35Exception as ex:
+    except AskYourPdfException as ex:
         return HttpJsonResponse.from_primitives({
             "code": ex.code,
             "message": ex.message,
