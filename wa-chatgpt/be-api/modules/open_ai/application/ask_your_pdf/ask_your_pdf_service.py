2f0f8153df2b8b3177eb571595ef51dfcba63b27
Eduardo A.F. <eacevedof@yahoo.es>
2024-10-09 23:01:55 +0200

    wa error get-response-using-chain

--- wa-chatgpt/be-api/modules/open_ai/application/ask_your_pdf/ask_your_pdf_service.py
+++ wa-chatgpt/be-api/modules/open_ai/application/ask_your_pdf/ask_your_pdf_service.py
@@ -59,6 +59,10 @@ class AskYourPdfService:
 
 
     def __get_response_from_chatgpt(self) -> str:
-        docs = self.__knowledge_base.similarity_search(self._ask_your_pdf_dto.question, 3)
+        number_of_paragraphs = 3
+        docs = self.__knowledge_base.similarity_search(
+            self._ask_your_pdf_dto.question,
+            number_of_paragraphs
+        )
         return get_response_using_chain(docs)
 
