9e74b361406517b3c94e3eb00308726e46f8dd50
Eduardo A.F. <eacevedof@yahoo.es>
2024-10-09 20:34:44 +0200

    wa module importer

---
+++ wa-chatgpt/be-api/modules/shared/infrastructure/components/flask_module_importer.py
@@ -0,0 +1,18 @@
+import importlib
+from flask import request, Response
+from modules.shared.infrastructure.components.log import Log
+from modules.shared.domain.enums.http_response_code_enum import HttpResponseCodeEnum
+from modules.shared.infrastructure.components.http.response.http_json_response import HttpJsonResponse
+
+def get_result_after_invoke(module_path: str, http_request: request) -> Response | str:
+    try:
+        module = importlib.import_module(module_path)
+        fn_invoke = getattr(module, "invoke")
+        return fn_invoke(http_request)
+    except Exception as ex:
+        Log.log_exception(ex, "main.get_result_after_invoke")
+        return HttpJsonResponse.from_primitives({
+            "code": HttpResponseCodeEnum.INTERNAL_SERVER_ERROR.value,
+            "message": "shared-tr.unexpected-error",
+            "data": {"infrastructure": str(ex), "module_path": module_path}
+        }).get_as_json_response()
---
+++ wa-chatgpt/be-api/modules/shared/infrastructure/enums/http_route_enum.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class HttpRouteEnum(Enum):
+    HOME = "/"
+    WHATSAPP_SEND_TEXT_MESSAGE = "/api/v1/whatsapp/send-text-message"
+    CHAT_GPT_PDF_QUESTION = "/api/v1/chat-gpt/pdf-question"
+    CHAT_GPT_ASK_QUESTION = "/api/v1/chat-gpt/ask"
+    HEALTH_CHECK = "/api/v1/health-check"
+    DOCUMENTATION = "/"
