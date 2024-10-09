231251f6b1433e96c75cc1d3cbf7b18d526ff96d
Eduardo A.F. <eacevedof@yahoo.es>
2024-10-09 20:39:18 +0200

    wa module importer

--- wa-chatgpt/be-api/modules/shared/infrastructure/enums/http_route_enum.py
+++ wa-chatgpt/be-api/modules/shared/infrastructure/enums/http_route_enum.py
@@ -1,7 +1,6 @@
 from enum import Enum
 
 class HttpRouteEnum(Enum):
-    HOME = "/"
     WHATSAPP_SEND_TEXT_MESSAGE = "/api/v1/whatsapp/send-text-message"
     CHAT_GPT_PDF_QUESTION = "/api/v1/chat-gpt/pdf-question"
     CHAT_GPT_ASK_QUESTION = "/api/v1/chat-gpt/ask"
--- wa-chatgpt/be-api/modules/shared/infrastructure/enums/module_path_enum.py
+++ wa-chatgpt/be-api/modules/shared/infrastructure/enums/module_path_enum.py
@@ -1,9 +1,8 @@
 from enum import Enum
 
 class ModulePathEnum(Enum):
-    HOME = "/"
-    WHATSAPP_SEND_TEXT_MESSAGE = "/api/v1/whatsapp/send-text-message"
-    CHAT_GPT_PDF_QUESTION = "/api/v1/chat-gpt/pdf-question"
-    CHAT_GPT_ASK_QUESTION = "/api/v1/chat-gpt/ask"
-    HEALTH_CHECK = "/api/v1/health-check"
-    DOCUMENTATION = "/"
+    WHATSAPP_SEND_TEXT_MESSAGE = "whatsapp.infrastructure.controllers.send_message_controller"
+    CHAT_GPT_PDF_QUESTION = "modules.open_ai.infrastructure.controllers.talk_to_gpt35_controller"
+    CHAT_GPT_ASK_QUESTION = "modules.open_ai.infrastructure.controllers.talk_to_gpt35_controller"
+    HEALTH_CHECK = "modules.health_check.infrastructure.controllers.get_health_check_controller"
+    DOCUMENTATION = "modules.api_doc.infrastructure.controllers.get_documentation_controller"
