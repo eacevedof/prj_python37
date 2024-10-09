b6b3b18bf231d68811d79ba2e3be41fb1803bb18
Eduardo A.F. <eacevedof@yahoo.es>
2024-10-09 23:17:52 +0200

    wa fix api-key

--- wa-chatgpt/be-api/modules/open_ai/application/ask_your_pdf/asked_to_pdf_dto.py
+++ wa-chatgpt/be-api/modules/open_ai/application/ask_your_pdf/asked_to_pdf_dto.py
@@ -1,8 +1,15 @@
+from typing import final
 from dataclasses import dataclass
 
+
+@final
 @dataclass(frozen=True)
 class AskedYourPdfDto:
     chat_response: str
+
+    @staticmethod
+    def from_primitives(chat_response: str) -> "AskedYourPdfDto":
+        return AskedYourPdfDto(chat_response)
 
     def __dict__(self):
         return {
