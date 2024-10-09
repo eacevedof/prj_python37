f6945261e4b252bac5074f8c3890472bd6201b98
Eduardo A.F. <eacevedof@yahoo.es>
2024-10-09 21:43:30 +0200

    wa ask-your-pdf

--- wa-chatgpt/be-api/modules/open_ai/application/ask_your_pdf/ask_your_pdf_dto.py
+++ wa-chatgpt/be-api/modules/open_ai/application/ask_your_pdf/ask_your_pdf_dto.py
@@ -8,6 +8,5 @@ class AskYourPdfDto:
     def from_primitives(question: str) -> 'AskYourPdfDto':
         question = str(question).strip()
         return AskYourPdfDto(
-            # question = str(primitives.get("question", "")).strip()
             question = question
         )
