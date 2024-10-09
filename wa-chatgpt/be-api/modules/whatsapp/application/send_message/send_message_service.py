acddbf7c4bf30f7a91102678224b4b224005a0b5
Eduardo A.F. <eacevedof@yahoo.es>
2024-10-09 20:11:32 +0200

    wa refactor module whatsapp

--- wa-chatgpt/be-api/modules/whatsapp/application/send_message/send_message_service.py
+++ wa-chatgpt/be-api/modules/whatsapp/application/send_message/send_message_service.py
@@ -4,10 +4,10 @@ from dataclasses import dataclass
 from modules.shared.infrastructure.log import Log
 from modules.shared.domain.enums.http_response_code_enum import HttpResponseCodeEnum
 
-from whatsapp.domain.exceptions.send_message_exception import SendMessageException
-from whatsapp.application.send_message.send_message_dto import SendMessageDto
-from whatsapp.application.send_message.sent_message_dto import SentMessageDto
-from whatsapp.infrastructure.repositories.whatsapp_business_writer_repository import WhatsappBusinessWriterRepository
+from modules.whatsapp.domain.exceptions.send_message_exception import SendMessageException
+from modules.whatsapp.application.send_message.send_message_dto import SendMessageDto
+from modules.whatsapp.application.send_message.sent_message_dto import SentMessageDto
+from modules.whatsapp.infrastructure.repositories.whatsapp_business_writer_repository import WhatsappBusinessWriterRepository
 
 
 @final
