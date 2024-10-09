c83a261a70ed3c0a9b87d4e800c82c6342abe4d4
Eduardo A.F. <eacevedof@yahoo.es>
2024-10-09 20:08:24 +0200

    wa refactor module open-ai

--- wa-chatgpt/be-api/modules/open_ai/infrastructure/controllers/talk_to_gpt35_controller.py
+++ wa-chatgpt/be-api/modules/open_ai/infrastructure/controllers/talk_to_gpt35_controller.py
@@ -3,9 +3,9 @@ from flask import Response, request
 from shared.infrastructure.log import Log
 from shared.domain.enums.http_response_code_enum import HttpResponseCodeEnum
 from shared.infrastructure.http.response.http_json_response import HttpJsonResponse
-from open_ai.application.talk_to_gpt35.talk_to_gpt35_dto import TalkToGpt35DTO
-from open_ai.application.talk_to_gpt35.talk_to_gpt35_service import talk_to_gpt35_service
-from open_ai.domain.exceptions.talk_to_gpt35_exception import TalkToGpt35Exception
+from modules.open_ai.application.talk_to_gpt35.talk_to_gpt35_dto import TalkToGpt35DTO
+from modules.open_ai.application.talk_to_gpt35.talk_to_gpt35_service import talk_to_gpt35_service
+from modules.open_ai.domain.exceptions.talk_to_gpt35_exception import TalkToGpt35Exception
 
 
 def invoke(http_request: request) -> Response:
