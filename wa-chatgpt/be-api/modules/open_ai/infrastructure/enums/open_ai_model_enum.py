a99bffaac5319b35569a178a00b348c8ae378a41
Eduardo A.F. <eacevedof@yahoo.es>
2024-10-09 22:10:26 +0200

    wa ask-your-pdf

---
+++ wa-chatgpt/be-api/modules/open_ai/infrastructure/enums/open_ai_model_enum.py
@@ -0,0 +1,4 @@
+from enum import Enum
+
+class OpenAiModelEnum(Enum):
+    GPT_3_5_TURBO = "gpt-3.5-turbo"
--- wa-chatgpt/be-api/modules/open_ai/infrastructure/repositories/openai_repository.py
+++ wa-chatgpt/be-api/modules/open_ai/infrastructure/repositories/openai_repository.py
@@ -1,19 +1,16 @@
 from openai import OpenAI
 from config.config import OPENAI_API_KEY
-
+from modules.open_ai.infrastructure.enums.open_ai_model_enum import OpenAiModelEnum
 
 def get_gpt35_turbo(question: str) -> str:
-    #return "get_gpt35_turbo :)"
-    model_engine = "gpt-3.5-turbo"
-
     client_open_ai = __get_client_openai()
 
     chat_completion = client_open_ai.chat.completions.create(
-        model=model_engine,
-        max_tokens=250,
-        n=1, # numero de respuestas
-        stop=None,
-        temperature=0.7, # nivel de creatividad moderado [0,1]
+        model = OpenAiModelEnum.GPT_3_5_TURBO.value,
+        max_tokens = 250,
+        n = 1, # numero de respuestas
+        stop = None,
+        temperature = 0.7, # nivel de creatividad moderado [0,1]
         messages=[
             {
                 "role": "user",
