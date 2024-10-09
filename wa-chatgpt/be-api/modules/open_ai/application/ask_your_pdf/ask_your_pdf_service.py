09658da9c755df9e8394762069bbc4d80b796990
Eduardo A.F. <eacevedof@yahoo.es>
2024-10-09 22:02:55 +0200

    wa ask-your-pdf

--- wa-chatgpt/be-api/modules/open_ai/application/ask_your_pdf/ask_your_pdf_service.py
+++ wa-chatgpt/be-api/modules/open_ai/application/ask_your_pdf/ask_your_pdf_service.py
@@ -12,17 +12,22 @@ from modules.open_ai.application.ask_your_pdf.ask_your_pdf_dto import AskYourPdf
 from modules.open_ai.application.ask_your_pdf.asked_to_pdf_dto import AskedYourPdfDto
 
 from modules.shared.infrastructure.components.files.pdf_reader import get_text_from_pdf_file
+from modules.shared.infrastructure.components.ia.text.language import get_knowledge_base_from_text
+
 
 @final
 #@dataclass(frozen=True)
 class AskYourPdfService:
 
     _ask_your_pdf_dto: AskYourPdfDto
+    __knowledge_base: object
 
     def invoke(self, ask_your_pdf_dto: AskYourPdfDto) -> AskedYourPdfDto:
 
         self._ask_your_pdf_dto = ask_your_pdf_dto
         self.__fail_if_wrong_input()
+
+        self.__load_knowledge_database()
 
         message = ask_your_pdf_dto.question
 
@@ -38,9 +43,9 @@ class AskYourPdfService:
             )
 
 
-    def __prepare_pdf(self):
+    def __load_knowledge_database(self):
         pdf_file_name = "boe-constitucion-espanola.pdf"
-        path_pdf_file = f"{PATH_UPLOAD_FOLDER}/{pdf_file_name}"
+        path_pdf_file = f"{PATH_UPLOAD_FOLDER}.{pdf_file_name}"
 
         pdf_text = get_text_from_pdf_file(path_pdf_file)
-        pass
+        self.__knowledge_base = get_knowledge_base_from_text(pdf_text)
