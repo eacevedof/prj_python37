1e5b71b6281742b7328c448be937edf4f22c49c2
ioedumac <eacevedof@yahoo.es>
2024-10-18 22:15:28 +0200

    openai-repo

--- wa-chatgpt/be-api/modules/whatsapp/infrastructure/repositories/whatsapp_business_writer_repository.py
+++ wa-chatgpt/be-api/modules/whatsapp/infrastructure/repositories/whatsapp_business_writer_repository.py
@@ -1,10 +1,11 @@
 from typing import final
 
-from modules.whatsapp.infrastructure.repositories.abstract_whatsapp_business_repository import AbstractWhatsappBusinessRepository
+from modules.whatsapp.infrastructure.repositories.abstract_whatsapp_business_repository import \
+    AbstractWhatsappBusinessRepository
+
 
 @final
 class WhatsappBusinessWriterRepository(AbstractWhatsappBusinessRepository):
-
 
     def send_text_message(self, phone_number: str, message: str) -> dict:
         payload = {
@@ -17,4 +18,3 @@ class WhatsappBusinessWriterRepository(AbstractWhatsappBusinessRepository):
         }
         response_data = self._post("messages", payload)
         return response_data
-
