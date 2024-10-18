acc5038af3e214c3f99735a7b249e24acf2277ea
ioedumac <eacevedof@yahoo.es>
2024-10-18 21:24:33 +0200

    wa openai-repository

--- wa-chatgpt/be-api/modules/open_ai/infrastructure/repositories/abstract_openai_repository.py
+++ wa-chatgpt/be-api/modules/open_ai/infrastructure/repositories/abstract_openai_repository.py
@@ -11,35 +11,8 @@ from typing import List
 
 class AbstractOpenAiRepository(ABC):
 
-    def __get_client_openai(self) -> OpenAI:
+    def _get_client_openai(self) -> OpenAI:
         return OpenAI(
             api_key=OPENAI_API_KEY
         )
 
-    def get_gpt35_turbo(self, question: str) -> str:
-        client_open_ai = self.__get_client_openai()
-
-        chat_completion = client_open_ai.chat.completions.create(
-            model=OpenAiModelEnum.GPT_3_5_TURBO.value,
-            max_tokens=250,
-            n=1,  # numero de respuestas
-            stop=None,
-            temperature=0.7,  # nivel de creatividad moderado [0,1]
-            messages=[
-                {
-                    "role": "user",
-                    "content": question
-                }
-            ]
-        )
-        # return chat_completion.choices[0].message["content"] error
-        return chat_completion.choices[0].message.content
-
-    def get_response_using_chain(self, langchain_documents: List[Document], question: str) -> str:
-        llm_obj = ChatOpenAI(
-            model_name=OpenAiModelEnum.GPT_3_5_TURBO.value,
-            openai_api_key=OPENAI_API_KEY
-        )
-        chain_obj = load_qa_chain(llm_obj, chain_type=LangchainTypeEnum.STUFF.value)
-        respuesta = chain_obj.run(input_documents=langchain_documents, question=question)
-        return respuesta
--- wa-chatgpt/be-api/modules/open_ai/infrastructure/repositories/openai_repository.py
+++ wa-chatgpt/be-api/modules/open_ai/infrastructure/repositories/openai_repository.py
@@ -5,40 +5,41 @@ from langchain.chat_models import ChatOpenAI
 from langchain.chains.question_answering import load_qa_chain
 from modules.shared.infrastructure.enums.langchain_type_enum import LangchainTypeEnum
 from langchain_core.documents import Document
-from typing import List
+from typing import List, final
 
+from open_ai.infrastructure.repositories.abstract_openai_repository import AbstractOpenAiRepository
 
-def __get_client_openai() -> OpenAI:
-    return OpenAI(
-        api_key = OPENAI_API_KEY
-    )
+@final
+class OpenAiRepository(AbstractOpenAiRepository):
 
+    @staticmethod
+    def get_instance() -> OpenAiRepository:
+        return OpenAiRepository()
 
-def get_gpt35_turbo(question: str) -> str:
-    client_open_ai = __get_client_openai()
+    def get_gpt35_turbo(self, question: str) -> str:
+        client_open_ai = self._get_client_openai()
 
-    chat_completion = client_open_ai.chat.completions.create(
-        model = OpenAiModelEnum.GPT_3_5_TURBO.value,
-        max_tokens = 250,
-        n = 1, # numero de respuestas
-        stop = None,
-        temperature = 0.7, # nivel de creatividad moderado [0,1]
-        messages=[
-            {
-                "role": "user",
-                "content": question
-            }
-        ]
-    )
-    # return chat_completion.choices[0].message["content"] error
-    return chat_completion.choices[0].message.content
+        chat_completion = client_open_ai.chat.completions.create(
+            model=OpenAiModelEnum.GPT_3_5_TURBO.value,
+            max_tokens=250,
+            n=1,  # numero de respuestas
+            stop=None,
+            temperature=0.7,  # nivel de creatividad moderado [0,1]
+            messages=[
+                {
+                    "role": "user",
+                    "content": question
+                }
+            ]
+        )
+        # return chat_completion.choices[0].message["content"] error
+        return chat_completion.choices[0].message.content
 
-
-def get_response_using_chain(langchain_documents: List[Document], question: str) -> str:
-    llm_obj = ChatOpenAI(
-        model_name = OpenAiModelEnum.GPT_3_5_TURBO.value,
-        openai_api_key = OPENAI_API_KEY
-    )
-    chain_obj = load_qa_chain(llm_obj, chain_type = LangchainTypeEnum.STUFF.value)
-    respuesta = chain_obj.run(input_documents = langchain_documents, question = question)
-    return respuesta
+    def get_response_using_chain(self, langchain_documents: List[Document], question: str) -> str:
+        llm_obj = ChatOpenAI(
+            model_name=OpenAiModelEnum.GPT_3_5_TURBO.value,
+            openai_api_key=OPENAI_API_KEY
+        )
+        chain_obj = load_qa_chain(llm_obj, chain_type=LangchainTypeEnum.STUFF.value)
+        respuesta = chain_obj.run(input_documents=langchain_documents, question=question)
+        return respuesta
