c4d613adf40021b315beaf36a7f5c2858184caa4
ioedumac <eacevedof@yahoo.es>
2024-10-18 21:48:04 +0200

    openai-repo

--- wa-chatgpt/be-api/modules/open_ai/infrastructure/repositories/abstract_openai_repository.py
+++ wa-chatgpt/be-api/modules/open_ai/infrastructure/repositories/abstract_openai_repository.py
@@ -1,12 +1,6 @@
 from abc import ABC
 from openai import OpenAI
 from config.config import OPENAI_API_KEY
-from modules.open_ai.infrastructure.enums.open_ai_model_enum import OpenAiModelEnum
-from langchain.chat_models import ChatOpenAI
-from langchain.chains.question_answering import load_qa_chain
-from modules.shared.infrastructure.enums.langchain_type_enum import LangchainTypeEnum
-from langchain_core.documents import Document
-from typing import List
 
 
 class AbstractOpenAiRepository(ABC):
