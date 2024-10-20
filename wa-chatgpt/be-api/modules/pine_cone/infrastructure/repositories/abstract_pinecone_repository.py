1c3be77a83ae70001b3d386d48c9abe77fefbfa2
Eduardo A.F. <eacevedof@yahoo.es>
2024-10-20 16:26:46 +0200

    wa - pinecone upsert

--- wa-chatgpt/be-api/modules/pine_cone/infrastructure/repositories/abstract_pinecone_repository.py
+++ wa-chatgpt/be-api/modules/pine_cone/infrastructure/repositories/abstract_pinecone_repository.py
@@ -1,6 +1,6 @@
 from abc import ABC
 
-from config.config import PINECONE_API_KEY, PINECONE_SERVER
+from config.config import PINECONE_API_KEY, PINECONE_SERVER, PINECONE_INDEX_NAME
 from pinecone import Pinecone
 
 class AbstractPineconeRepository(ABC):
@@ -10,3 +10,10 @@ class AbstractPineconeRepository(ABC):
             api_key=PINECONE_API_KEY,
             host=PINECONE_SERVER
         )
+
+    def _get_pinecone_by_index(self, db_index: str) -> Pinecone:
+        return Pinecone(
+            api_key=PINECONE_API_KEY,
+            host=PINECONE_SERVER,
+            index_api=db_index
+       )
