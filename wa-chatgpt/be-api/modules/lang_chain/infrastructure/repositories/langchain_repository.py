1a4ce6cd234689848a8d644443804abda183bdeb
Eduardo A.F. <eacevedof@yahoo.es>
2024-10-20 13:57:30 +0200

    wa - refactor Prompt must accept context as an input variable. Received prompt with input variables: []

--- wa-chatgpt/be-api/modules/lang_chain/infrastructure/repositories/langchain_repository.py
+++ wa-chatgpt/be-api/modules/lang_chain/infrastructure/repositories/langchain_repository.py
@@ -7,6 +7,9 @@ from langchain.chains.question_answering import load_qa_chain
 from modules.shared.infrastructure.enums.langchain_type_enum import LangchainTypeEnum
 from modules.lang_chain.infrastructure.repositories.abstract_langchain_repository import AbstractLangchainRepository
 
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+
 
 @final
 @dataclass(frozen=True)
@@ -16,10 +19,21 @@ class LangchainRepository(AbstractLangchainRepository):
     def get_instance() -> "LangchainRepository":
         return LangchainRepository()
 
-
     def get_response_using_chain(self, langchain_documents: List[Document], question: str) -> str:
+        prompt_tpl = ChatPromptTemplate.from_template(question+" {context}")
+        oai_llm = self._get_chat_openai()
+        chain = create_stuff_documents_chain(llm=oai_llm, prompt=prompt_tpl)
+        respuesta = chain.invoke({"context": langchain_documents})
+        return respuesta
+
+    def get_response_using_chain_old(self, langchain_documents: List[Document], question: str) -> str:
         llm_obj = self._get_chat_openai()
-        chain_obj = load_qa_chain(llm_obj, chain_type=LangchainTypeEnum.STUFF.value)
+
+        chain_obj = load_qa_chain(
+            llm = llm_obj,
+            chain_type=LangchainTypeEnum.STUFF.value
+        )
         respuesta = chain_obj.run(input_documents=langchain_documents, question=question)
+
         return respuesta
 
