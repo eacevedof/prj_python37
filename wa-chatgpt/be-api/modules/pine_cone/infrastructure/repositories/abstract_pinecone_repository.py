4592c746ae42bc03a3d88f760e7232b900e7cce2
Eduardo A.F. <eacevedof@yahoo.es>
2024-10-20 16:19:03 +0200

    wa - pinecone

--- wa-chatgpt/be-api/modules/pine_cone/infrastructure/repositories/abstract_pinecone_repository.py
+++ wa-chatgpt/be-api/modules/pine_cone/infrastructure/repositories/abstract_pinecone_repository.py
@@ -1,9 +1,12 @@
 from abc import ABC
 
-from config.config import PINE_CONE_API_KEY
+from config.config import PINECONE_API_KEY, PINECONE_SERVER
 from pinecone import Pinecone
 
 class AbstractPineconeRepository(ABC):
 
     def _get_pinecone(self) -> Pinecone:
-        return Pinecone(api_key=PINE_CONE_API_KEY)
+        return Pinecone(
+            api_key=PINECONE_API_KEY,
+            host=PINECONE_SERVER
+        )
