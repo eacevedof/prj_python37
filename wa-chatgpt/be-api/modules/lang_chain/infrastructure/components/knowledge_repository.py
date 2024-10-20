7dc2747ee805dae8c4235797819ea71a01776456
Eduardo A.F. <eacevedof@yahoo.es>
2024-10-20 13:15:00 +0200

    wa - refactor a module lang-chain

---
+++ wa-chatgpt/be-api/modules/lang_chain/infrastructure/components/knowledge_repository.py
@@ -0,0 +1,46 @@
+from typing import final
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+
+from modules.lang_chain.domain.enums.langchain_embedding_enum import LangchainEmbeddingEnum
+
+
+@final
+class KnowledgeRepository:
+    @staticmethod
+    def get_instance() -> "KnowledgeRepository":
+        return KnowledgeRepository()
+
+    def __get_chunks_from_text(self, text: str) -> list[str]:
+        splitter = RecursiveCharacterTextSplitter(
+            chunk_size=800,
+            chunk_overlap=100,
+            length_function=len
+        )
+        return splitter.split_text(text)
+
+    # https://youtu.be/iDrpdkIHMq8?t=549
+    def __get_embedding_by_minilm(self):
+        transformer_name = LangchainEmbeddingEnum.PARAPHRASE_MULTILINGUAL_MINILM_L12_V2
+        embeddings = HuggingFaceEmbeddings(model_name=transformer_name)
+        return embeddings
+
+    def __get_embedding_by_mpnet_base(self):
+        transformer_name = LangchainEmbeddingEnum.PARAPHRASE_MULTILINGUAL_MPNET_BASE_V2
+        embeddings = HuggingFaceEmbeddings(model_name=transformer_name)
+        return embeddings
+
+    def get_knowledge_base_from_text(self, text: str) -> FAISS:
+        chunks = self.__get_chunks_from_text(text)
+        # embeddings = __get_embedding_by_minilm()
+        embeddings = self.__get_embedding_by_mpnet_base()
+        knowledge_base = FAISS.from_texts(chunks, embeddings)
+        return knowledge_base
+
+
+
+
+
+
