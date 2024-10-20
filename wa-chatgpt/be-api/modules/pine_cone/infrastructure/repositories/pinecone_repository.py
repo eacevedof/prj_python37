4c8972863e8e786e61aaf08aff5c363cad44cf1b
Eduardo A.F. <eacevedof@yahoo.es>
2024-10-20 16:26:46 +0200

    wa - pinecone upsert

--- wa-chatgpt/be-api/modules/pine_cone/infrastructure/repositories/pinecone_repository.py
+++ wa-chatgpt/be-api/modules/pine_cone/infrastructure/repositories/pinecone_repository.py
@@ -1,15 +1,10 @@
 from dataclasses import dataclass
 from typing import List, final
 
+from config.config import PINECONE_INDEX_NAME
 from langchain_core.documents import Document
-from langchain.chains.question_answering import load_qa_chain
 
-from modules.shared.infrastructure.enums.langchain_type_enum import LangchainTypeEnum
 from modules.pine_cone.infrastructure.repositories.abstract_pinecone_repository import AbstractPineconeRepository
-
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain_core.prompts import ChatPromptTemplate
-
 
 @final
 @dataclass(frozen=True)
@@ -19,12 +14,11 @@ class PineconeRepository(AbstractPineconeRepository):
     def get_instance() -> "PineconeRepository":
         return PineconeRepository()
 
-    def get_response_using_chain(self, langchain_documents: List[Document], question: str) -> str:
-        str_tpl = question+" {context}"
-        prompt_tpl = ChatPromptTemplate.from_template(str_tpl)
-        oai_llm = self._get_pinecone()
-        chain = create_stuff_documents_chain(llm=oai_llm, prompt=prompt_tpl)
-        str_result = chain.invoke({"context": langchain_documents})
-        return str_result
+
+    def upsert_pdf_index(self, documents: List[Document]) -> None:
+        pinecone = self._get_pinecone_by_index(PINECONE_INDEX_NAME)
+        pinecone.upsert(documents)
+
+
 
 
