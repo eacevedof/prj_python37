1a63a4d5dba3b5cd9a9d0a21157f163a2434f859
Eduardo A.F. <eacevedof@yahoo.es>
2024-10-23 22:22:33 +0200

    talk-bot form singup ok

--- wa-chatgpt/fe-flet/talk_bot/modules/users/infrastructure/views/form_user_singnup_view.py
+++ wa-chatgpt/fe-flet/talk_bot/modules/users/infrastructure/views/form_user_singnup_view.py
@@ -1,21 +1,25 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import final
 import flet as ft
 from flet import TextField, Checkbox, ElevatedButton, Row, Text
 from flet_core import Column
 
-
 @final
-# @dataclass(frozen=True)
+@dataclass
 class FormUserSignupView:
-    def __init__(self, page: ft.Page) -> None:
-        self.__page = page
-        self.__page.title = "Signup"
-        self.__page.vertical_alignment = ft.VerticalAlignment.CENTER
-        self.__page.theme_mode = ft.ThemeMode.LIGHT
-        self.__page.window_width = 400
-        self.__page.window_height = 400
-        self.__page.window_resizable = False
+    page: ft.Page
+    __text_username: TextField = field(init=False)
+    __text_password: TextField = field(init=False)
+    __chk_agree: Checkbox = field(init=False)
+    __btn_signup: ElevatedButton = field(init=False)
+
+    def __post_init__(self):
+        self.page.title = "Signup"
+        self.page.vertical_alignment = ft.VerticalAlignment.CENTER
+        self.page.theme_mode = ft.ThemeMode.LIGHT
+        self.page.window_width = 400
+        self.page.window_height = 400
+        self.page.window_resizable = False
 
         self.__text_username = TextField(label="Username", text_align=ft.TextAlign.LEFT, width=300)
         self.__text_password = TextField(password=True, label="Password", text_align=ft.TextAlign.LEFT, width=300)
@@ -24,19 +28,20 @@ class FormUserSignupView:
 
         self.__configure_input_events()
 
-        page.add(Row(
-            controls=[
-                Column([
-                        self.__text_username,
-                        self.__text_password,
-                        self.__chk_agree,
-                        self.__btn_signup
-                    ],
-                    alignment=ft.MainAxisAlignment.CENTER
-                )
-            ],
-            alignment=ft.MainAxisAlignment.CENTER
-        ))
+        self.page.add(
+            Row(
+                controls=[
+                    Column([
+                            self.__text_username,
+                            self.__text_password,
+                            self.__chk_agree,
+                            self.__btn_signup
+                        ],
+                    )
+                ],
+                alignment=ft.MainAxisAlignment.CENTER
+            )
+        )
 
     def __configure_input_events(self) -> None:
         self.__text_username.on_change = self.__validate_input
@@ -49,13 +54,13 @@ class FormUserSignupView:
         if all([self.__text_username.value, self.__text_password.value, self.__chk_agree.value]):
             self.__btn_signup.disabled = False
 
-        self.__page.update()
+        self.page.update()
 
     def __on_submit(self, event: ft.ControlEvent) -> None:
         print("username:", self.__text_username.value)
         print("password:", self.__text_password.value)
-        self.__page.clean()
-        self.__page.add(Row(
+        self.page.clean()
+        self.page.add(Row(
             controls=[
                 Text(value=f"User {self.__text_username.value} created successfully", text_align=ft.TextAlign.CENTER, size=20)
             ],
