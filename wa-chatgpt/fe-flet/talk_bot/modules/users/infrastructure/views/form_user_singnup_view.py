dc147e3831ccac97c6eaa23a43a06774f35b2511
Eduardo A.F. <eacevedof@yahoo.es>
2024-10-23 21:36:35 +0200

    talk-bot form singup

--- wa-chatgpt/fe-flet/talk_bot/modules/users/infrastructure/views/form_user_singnup_view.py
+++ wa-chatgpt/fe-flet/talk_bot/modules/users/infrastructure/views/form_user_singnup_view.py
@@ -1,15 +1,27 @@
+
 import flet as ft
-from flet import TextField
+from flet import TextField, Checkbox, ElevatedButton
+from flet_core import ElevatedButton
 
 
 class FormUserSignupView:
     def __init__(self, page: ft.Page) -> None:
-        page.title = "Login"
+        page.title = "Singup"
         page.vertical_alignment = ft.VerticalAlignment.CENTER
         page.theme_mode = ft.ThemeMode.LIGHT
         page.window_width = 400
         page.window_height = 400
         page.window_resizable = False
 
-        text_username = TextField(label="Username", text_align=ft.TextAlign.LEFT, width=300)
-        page.add(text_username)
+        inputs = []
+        tmp = TextField(label="Username", text_align=ft.TextAlign.LEFT, width=300)
+        inputs.append(tmp)
+        tmp = TextField(label="Password", text_align=ft.TextAlign.LEFT, width=300, password=True)
+        inputs.append(tmp)
+        tmp = Checkbox(label="I agree to the terms and conditions", value=False)
+        inputs.append(tmp)
+        tmp = ElevatedButton(text="Singup", width=300)
+        inputs.append(tmp)
+
+        for input in inputs:
+            page.add(input)
