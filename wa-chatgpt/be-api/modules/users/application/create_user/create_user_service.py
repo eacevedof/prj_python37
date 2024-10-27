5b73058cbfaa3d076b43b092c38c9435ce0fd3a6
ioedumac <eacevedof@yahoo.es>
2024-10-27 19:51:48 +0100

    uuider

--- wa-chatgpt/be-api/modules/users/application/create_user/create_user_service.py
+++ wa-chatgpt/be-api/modules/users/application/create_user/create_user_service.py
@@ -1,30 +1,36 @@
-from typing import final
 from dataclasses import dataclass
+from typing import final
+from modules.users.infrastructure.repositories.users_postgres_repository import UsersPostgresRepository
+from shared.infrastructure.components.encrypter import Encrypter
+from shared.infrastructure.components.uuider import Uuider
+from users.application.create_user.create_user_dto import CreateUserDto
+from users.domain.entities.user_entity import UserEntity
 
 
 @final
 @dataclass(frozen=True)
-class CreateUserDto:
-    user_name: str
-    user_password: str
-    user_email: str
-    user_code: str = None
+class CreateUserService:
+
+    __uuider: Uuider
+    __encrypter: Encrypter
+    __users_repository: UsersPostgresRepository
 
     @staticmethod
-    def from_primitives(
-        user_name: str,
-        user_password: str,
-        user_email: str,
-        user_code: str = None
-    ) -> 'CreateUserDto':
-
-        user_name = str(user_name).strip()
-        user_password = str(user_password).strip()
-        user_email = str(user_email).strip()
-        user_code = str(user_code).strip()
-        return CreateUserDto(
-            user_name=user_name,
-            user_password=user_password,
-            user_email=user_email,
-            user_code=user_code
+    def get_instance() -> 'CreateUserService':
+        return CreateUserService(
+            Uuider.get_instance(),
+            Encrypter.get_instance(),
+            UsersPostgresRepository.get_instance()
         )
+
+    def invoke(self, create_user_dto: CreateUserDto) -> None:
+        user_uuid = self.__uuider.get_id_with_prefix("usr")
+        user_password = self.__encrypter.get_encrypted(create_user_dto.user_password)
+        user_entity = UserEntity.from_primitives(
+            user_uuid=user_uuid,
+            user_name=create_user_dto.user_name,
+            user_password=user_password,
+            user_email=create_user_dto.user_email,
+            user_code=create_user_dto.user_code
+        )
+        self.__users_repository.create_user(user_entity)
