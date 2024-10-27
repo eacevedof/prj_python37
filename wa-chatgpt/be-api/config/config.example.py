f89a1c4015d196d88c29d671524f25cb3788f2e5
Eduardo A.F. <eacevedof@yahoo.es>
2024-10-27 09:57:54 +0100

    wa postgres cacher

--- wa-chatgpt/be-api/config/config.example.py
+++ wa-chatgpt/be-api/config/config.example.py
@@ -1,12 +1,12 @@
 OPENAI_API_KEY = ":)"
+
 META_BUSINESS_ID = ":)"
 META_BUSINESS_BEARER_TOKEN = ":)"
-PINE_CONE_API_KEY = ":)" # https://app.pinecone.io/
 
-
-PINECONE_SERVER = "https://some-names-space.pinecone.io"
+PINECONE_SERVER = "https://yyy-xxxx.pinecone.io"
 PINECONE_API_KEY = ":)"
 PINECONE_INDEX_NAME = "pdf"
+
 
 import os
 os.environ['PINECONE_API_KEY'] = PINECONE_API_KEY
