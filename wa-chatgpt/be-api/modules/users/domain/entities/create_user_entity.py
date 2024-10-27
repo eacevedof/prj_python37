20d105bc1e287aa60af876be549e40353050149e
ioedumac <eacevedof@yahoo.es>
2024-10-27 19:37:05 +0100

    uuider

--- wa-chatgpt/be-api/modules/users/domain/entities/create_user_entity.py
+++ wa-chatgpt/be-api/modules/users/domain/entities/create_user_entity.py
@@ -13,17 +13,19 @@ class CreateUserEntity:
 
     @staticmethod
     def from_primitives(
+        user_uuid: str,
         user_name: str,
         user_password: str,
         user_email: str,
         user_code: str = None
     ) -> 'CreateUserEntity':
-
+        user_uuid = str(user_uuid).strip()
         user_name = str(user_name).strip()
         user_password = str(user_password).strip()
         user_email = str(user_email).strip()
         user_code = str(user_code).strip()
         return CreateUserEntity(
+            user_uuid=user_uuid,
             user_name=user_name,
             user_password=user_password,
             user_email=user_email,
