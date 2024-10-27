709b8cec12f6b0de845969d2ac890f7ef3d35573
ioedumac <eacevedof@yahoo.es>
2024-10-27 16:43:26 +0100

    uuider

--- wa-chatgpt/be-api/database/migrations/20240126101112_create_table_app_users.sql
+++ wa-chatgpt/be-api/database/migrations/20240126101112_create_table_app_users.sql
@@ -14,7 +14,9 @@ CREATE TABLE app_users
     user_login VARCHAR(100) NOT NULL UNIQUE,
     user_password VARCHAR(100) NOT NULL,
     user_email VARCHAR(100) NOT NULL UNIQUE,
-    is_enabled INT DEFAULT 1
+    user_tmp VARCHAR(100) NOT NULL UNIQUE,
+    is_enabled INT DEFAULT 1,
+    user_misc JSONB DEFAULT '{}'
 )
 ;
 
