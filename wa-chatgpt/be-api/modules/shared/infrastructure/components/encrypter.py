a4aa70086fa857765c9fea0c8b69320c6b90e977
Eduardo A.F. <eacevedof@yahoo.es>
2024-10-29 19:54:35 +0100

    wa crud user bcrypt

--- wa-chatgpt/be-api/modules/shared/infrastructure/components/encrypter.py
+++ wa-chatgpt/be-api/modules/shared/infrastructure/components/encrypter.py
@@ -14,7 +14,7 @@ class Encrypter:
         return Encrypter()
 
     def get_encrypted(self, text: str) -> str:
-        text_encode = text.encode("utf-8")
-        salt = self.__SALT.encode("utf-8")
-        hashed_password = bcrypt.hashpw(text_encode, salt)
+        text_encode = str.encode(text)
+        salt_encode = str.encode(self.__SALT)
+        hashed_password = bcrypt.hashpw(text_encode, salt_encode)
         return hashed_password.decode("utf-8")
