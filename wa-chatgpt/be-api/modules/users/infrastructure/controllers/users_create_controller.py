e3177d8a455ece749878bd104d7be9e9de6f8e97
Eduardo A.F. <eacevedof@yahoo.es>
2024-10-29 19:14:50 +0100

    wa crud user create

--- wa-chatgpt/be-api/modules/users/infrastructure/controllers/users_create_controller.py
+++ wa-chatgpt/be-api/modules/users/infrastructure/controllers/users_create_controller.py
@@ -22,7 +22,7 @@ def invoke(http_request: Request) -> Response:
 
         return HttpJsonResponse.from_primitives({
             "code": HttpResponseCodeEnum.OK.value,
-            "message": "open-ai-tr.create_user",
+            "message": "users-tr.create_user",
             "data": {"user": created_user_dto}
         }).get_as_json_response()
 
