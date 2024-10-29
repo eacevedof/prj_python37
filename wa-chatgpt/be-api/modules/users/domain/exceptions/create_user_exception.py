ad21d7673625a5ba4184cecd3507ed339de5b555
Eduardo A.F. <eacevedof@yahoo.es>
2024-10-29 18:59:07 +0100

    wa crud user create

--- wa-chatgpt/be-api/modules/users/domain/exceptions/create_user_exception.py
+++ wa-chatgpt/be-api/modules/users/domain/exceptions/create_user_exception.py
@@ -1,10 +1,13 @@
 from typing import final
+from dataclasses import dataclass
+
 from modules.shared.domain.enums.http_response_code_enum import HttpResponseCodeEnum
 from modules.shared.domain.exceptions.abstract_domain_exception import AbstractDomainException
 
 
 @final
-class SendMessageException(AbstractDomainException):
+# @dataclass(frozen=True)
+class CreateUserException(AbstractDomainException):
 
     def __init__(self, message: str, code: int) -> None:
         self._message = message
@@ -13,8 +16,8 @@ class SendMessageException(AbstractDomainException):
 
 
     @staticmethod
-    def empty_question() -> None:
-        raise SendMessageException(
-            "open-ai-tr.resource-not-found",
-            HttpResponseCodeEnum.NOT_FOUND.value
+    def empty_email() -> None:
+        raise CreateUserException(
+            "users-tr.empty-email",
+            HttpResponseCodeEnum.BAD_REQUEST.value
         )
