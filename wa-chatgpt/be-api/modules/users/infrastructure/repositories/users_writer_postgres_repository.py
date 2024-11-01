9d2125db7fb603f010074c4aa91f5b1dbda6c578
ioedumac <eacevedof@yahoo.es>
2024-11-01 19:43:29 +0100

    wa user-update

--- wa-chatgpt/be-api/modules/users/infrastructure/repositories/users_writer_postgres_repository.py
+++ wa-chatgpt/be-api/modules/users/infrastructure/repositories/users_writer_postgres_repository.py
@@ -38,13 +38,13 @@ class UsersWriterPostgresRepository(AbstractPostgresRepository):
         Log.log_sql(sql, "create_user")
         self._command(sql)
 
-    def update_user(self, create_user_entity: UserEntity) -> None:
-        user_uuid = create_user_entity.user_uuid
-        user_name = create_user_entity.user_name
-        user_login = create_user_entity.user_login
-        user_password = create_user_entity.user_password
-        user_email = create_user_entity.user_email
-        user_code = create_user_entity.user_code
+    def update_user(self, update_user_entity: UserEntity) -> None:
+        user_uuid = update_user_entity.user_uuid
+        user_name = update_user_entity.user_name
+        user_login = update_user_entity.user_login
+        user_password = update_user_entity.user_password
+        user_email = update_user_entity.user_email
+        user_code = update_user_entity.user_code
         updated_at = self.__date_timer.get_now_ymd_his()
 
         sql = f"""
