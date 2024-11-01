29218e017339a57ec8262d8dcd254ffd2ad4a4ef
ioedumac <eacevedof@yahoo.es>
2024-11-01 18:44:28 +0100

    wa update

---
+++ wa-chatgpt/be-api/modules/users/application/update_user/update_user_service.py
@@ -0,0 +1,67 @@
+from dataclasses import dataclass
+from typing import final
+from modules.users.infrastructure.repositories.users_writer_postgres_repository import UsersWriterPostgresRepository
+from modules.users.infrastructure.repositories.users_reader_postgres_repository import UsersReaderPostgresRepository
+from shared.infrastructure.components.encrypter import Encrypter
+from shared.infrastructure.components.uuider import Uuider
+from users.application.create_user.create_user_dto import CreateUserDto
+from users.application.create_user.created_user_dto import CreatedUserDto
+from users.domain.entities.user_entity import UserEntity
+from users.domain.exceptions.create_user_exception import CreateUserException
+
+
+@final
+@dataclass(frozen=False)
+class UpdateUserService:
+    __create_user_dto: CreateUserDto
+    __uuider: Uuider
+    __encrypter: Encrypter
+    __users_writer_repository: UsersWriterPostgresRepository
+    __users_reader_repository: UsersReaderPostgresRepository
+
+    @staticmethod
+    def get_instance() -> 'UpdateUserService':
+        return UpdateUserService(
+            Uuider.get_instance(),
+            Encrypter.get_instance(),
+            UsersWriterPostgresRepository.get_instance(),
+            UsersReaderPostgresRepository.get_instance()
+        )
+
+    def invoke(self, create_user_dto: CreateUserDto) -> CreatedUserDto:
+        self.__create_user_dto = create_user_dto
+
+        self.__fail_if_wrong_input()
+
+        user_uuid = self.__uuider.get_id_with_prefix("usr")
+        user_password = self.__encrypter.get_encrypted(create_user_dto.user_password)
+
+        user_entity = UserEntity.from_primitives(
+            id=None,
+            user_uuid=user_uuid,
+            user_name=create_user_dto.user_name,
+            user_password=user_password,
+            user_email=create_user_dto.user_email,
+            user_code=create_user_dto.user_code,
+            user_login="",
+            created_at=""
+        )
+        user_entity.login_with_email()
+        self.__users_writer_repository.create_user(user_entity)
+
+        new_user = self.__users_reader_repository.get_user_by_uuid(user_entity)
+        return CreatedUserDto.from_primitives(
+            new_user.id,
+            new_user.user_uuid,
+            new_user.user_name,
+            new_user.user_login,
+            new_user.user_email,
+            new_user.user_code,
+            new_user.created_at
+        )
+
+    def __fail_if_wrong_input(self) -> None:
+        if not self.__create_user_dto.user_name:
+            raise CreateUserException.empty_user_name()
+
+
