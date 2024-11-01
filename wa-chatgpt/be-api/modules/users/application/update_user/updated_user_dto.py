b53aa9ab04fd4b79975f52b946f704a60c986b9a
ioedumac <eacevedof@yahoo.es>
2024-11-01 18:44:28 +0100

    wa update

---
+++ wa-chatgpt/be-api/modules/users/application/update_user/updated_user_dto.py
@@ -0,0 +1,42 @@
+from typing import final
+from dataclasses import dataclass
+
+
+@final
+@dataclass(frozen=True)
+class UpdatedUserDto:
+
+    id: int
+    user_uuid: str
+    user_name: str
+    user_login: str
+    user_password: str
+    user_email: str
+    user_code: str
+    created_at: str
+
+    @staticmethod
+    def from_primitives(
+        id: int,
+        user_uuid: str,
+        user_name: str,
+        user_login: str,
+        user_email: str,
+        user_code: str,
+        created_at: str
+    ) -> 'UpdatedUserDto':
+
+        user_name = str(user_name).strip()
+        user_email = str(user_email).strip()
+        user_code = str(user_code).strip()
+
+        return UpdatedUserDto(
+            id=id,
+            user_uuid=user_uuid,
+            user_name=user_name,
+            user_login=user_login,
+            user_password="****",
+            user_email=user_email,
+            user_code=user_code,
+            created_at=created_at
+        )
