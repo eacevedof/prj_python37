a6890fe29c12ceca09f291d8d43eab8a117af55d
Eduardo A.F. <eacevedof@yahoo.es>
2025-01-02 22:17:46 +0100

    langchain - result optimization ejemplo sin optimizacion

--- wa-chatgpt/be-api/modules/shared/infrastructure/repositories/abstract_sklearn_repository.py
+++ wa-chatgpt/be-api/modules/shared/infrastructure/repositories/abstract_sklearn_repository.py
@@ -4,9 +4,12 @@ from typing import Dict, List
 import pyarrow.parquet as pq
 
 from langchain_openai import (
-    OpenAIEmbeddings
+    OpenAIEmbeddings, ChatOpenAI
 )
 from langchain_community.vectorstores import SKLearnVectorStore
+
+from langchain.retrievers import ContextualCompressionRetriever
+from langchain.retrievers.document_compressors import LLMChainExtractor
 
 from config.config import OPENAI_API_KEY
 from modules.shared.infrastructure.components.log import Log
@@ -54,6 +57,20 @@ class AbstractSklearnRepository(ABC):
             Log.log_exception(e, "AbstractSklearnRepository.__is_valid_parquet_file")
             return False
 
+    def get_optimization_db(self) -> SKLearnVectorStore:
+        if not self.db_exists():
+            raise Exception(f"db does not exist: {self.__persist_path_optimization}")
+
+        if not self.__is_valid_parquet_file():
+            raise Exception(f"db {self.__persist_path_optimization} is not a parquet file")
+
+        return SKLearnVectorStore(
+            serializer = "parquet",
+            persist_path = self.__persist_path_optimization,
+            embedding = self.__get_embeddings_openai(),
+        )
+
+
     def get_openai_db(self) -> SKLearnVectorStore:
         if not self.db_exists():
             raise Exception(f"db does not exist: {self.__persist_path}")
@@ -72,6 +89,19 @@ class AbstractSklearnRepository(ABC):
         return OpenAIEmbeddings(
             openai_api_key=OPENAI_API_KEY
         )
+
+    def get_compresion_retriever(self) -> ContextualCompressionRetriever:
+        return ContextualCompressionRetriever(
+            base_retriever= self.get_optimization_db().as_retriever(),
+            base_compressor = self.__get_llm_chain_extractor(),
+        )
+
+    def __get_llm_chain_extractor(self) -> LLMChainExtractor:
+        chat_open_ai = ChatOpenAI(
+            temperature=0, ## nivel de aleatoriedad 0: no hay aleatoriedad
+            openai_api_key=OPENAI_API_KEY
+        )
+        return LLMChainExtractor.from_llm(chat_open_ai)
 
     def db_optimization_exists(self) -> bool:
         return os.path.exists(self.__persist_path_optimization)
