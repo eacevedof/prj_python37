a0440b37633a65d328da41d3955db0f1266ccd04
Eduardo A.F. <eacevedof@yahoo.es>
2025-01-04 22:35:51 +0100

    langchain - cadena secuencial simple

--- wa-chatgpt/be-api/modules/lang_chain/application/lc_ask_question/lc_ask_question_service.py
+++ wa-chatgpt/be-api/modules/lang_chain/application/lc_ask_question/lc_ask_question_service.py
@@ -37,7 +37,7 @@ class LcAskQuestionService:
         # str_response = LcCursoRepository.get_instance().ejemplo_embeddings()
         # str_response = LcCursoRepository.get_instance().ejemplo_save_embeddings()
         # str_response = LcCursoRepository.get_instance().ejemplo_compresion_y_optimizacion_de_resultados()
-        str_response = LcCursoRepository.get_instance().ejemplo_cadena_secuencial_simple()
+        str_response = LcCursoRepository.get_instance().ejemplo_creacion_objeto_llm_chain()
 
         return LcAskedQuestionDTO(chat_response=f"{str_response}")
 
