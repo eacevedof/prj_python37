3bc0a74f8998967ecfda4efa4899b93fdc4b26f1
Eduardo A.F. <eacevedof@yahoo.es>
2025-01-08 17:27:12 +0100

    langchain - agent con google search api pip install google-search-results

--- wa-chatgpt/be-api/config/config.example.py
+++ wa-chatgpt/be-api/config/config.example.py
@@ -13,4 +13,5 @@ GOOGLE_SEARCH_API = "https://serpapi.com/search"
 GOOGLE_SEARCH_API_KEY = ":)"
 
 import os
-os.environ['PINECONE_API_KEY'] = PINECONE_API_KEY
+os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
+os.environ["SERPAPI_API_KEY"] = GOOGLE_SEARCH_API_KEY
