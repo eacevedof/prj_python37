476b235d4734562fc0763ad8441a7b51479ed72d
Eduardo A.F. <eacevedof@yahoo.es>
2025-02-02 21:36:06 +0100

    postgres reader abstract

---
+++ wa-chatgpt/be-api/modules/talk_db/infrastructure/repositories/talk_db_postgres_reader_repository.py
@@ -0,0 +1,42 @@
+from dataclasses import dataclass
+from typing import final
+
+from modules.shared.infrastructure.components.log import Log
+from modules.users.domain.entities.user_entity import UserEntity
+from modules.shared.infrastructure.components.date_timer import DateTimer
+from modules.shared.infrastructure.repositories.abstract_postgres_repository import AbstractPostgresRepository
+
+@final
+@dataclass(frozen=False)
+class TalkDbPostgresReaderRepository(AbstractPostgresRepository):
+
+    @staticmethod
+    def get_instance() -> "TalkDbPostgresReaderRepository":
+        return TalkDbPostgresReaderRepository()
+
+    def get_user_by_uuid(self, create_user_entity: UserEntity) -> UserEntity|None:
+        sql = f"""
+        SELECT *
+        FROM app_users 
+        WHERE 1=1 
+        AND user_uuid = '{create_user_entity.user_uuid}'
+        """
+        Log.log_sql(sql, "get_user_by_uuid")
+        result = self._query(sql)
+        if not result:
+            return None
+
+        created_at = DateTimer.get_instance().get_datetime_to_ymd_his(
+            result[0].get("created_at")
+        )
+
+        return UserEntity.from_primitives(
+            id=result[0].get("id"),
+            user_uuid=result[0].get("user_uuid"),
+            user_name=result[0].get("user_name"),
+            user_login=result[0].get("user_login"),
+            user_password=result[0].get("user_password"),
+            user_email=result[0].get("user_email"),
+            user_code=result[0].get("user_code"),
+            created_at=created_at
+        )
