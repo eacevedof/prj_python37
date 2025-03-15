a1e9e0426b478ea2e4c94b16bb47fbe9194f0148
Eduardo A.F. <eacevedof@yahoo.es>
2025-03-15 21:05:41 +0100

    sftp-sync

--- sftp-sync/file_sync.py
+++ sftp-sync/file_sync.py
@@ -8,6 +8,9 @@ from watchdog.events import FileSystemEventHandler
 
 load_dotenv()
 
+def pr(text):
+    pr(f"{time.strftime('%Y-%m-%d %H:%M:%S')} - {text}")
+
 def get_sftp_client():
     hostname = os.getenv("SFTP_HOST")
     port = int(os.getenv("SFTP_PORT"))
@@ -40,29 +43,29 @@ class SFTPHandler(FileSystemEventHandler):
 
         time.sleep(1)
 
-        print(f"upload_path: {upload_path}")
-        print(f"remote_path: {self.remote_path}")
+        pr(f"upload_path: {upload_path}")
+        pr(f"remote_path: {self.remote_path}")
         base_name = os.path.basename(upload_path)
-        print(f"base_name: {base_name}")
+        pr(f"base_name: {base_name}")
 
         remote_file_path = f"./{self.remote_path}/{base_name}"
-        print(f"remote_file_path: {remote_file_path}")
+        pr(f"remote_file_path: {remote_file_path}")
 
         self.sftp_client.put(upload_path, remote_file_path)
-        print(f"Uploaded {upload_path} to {remote_file_path}")
+        pr(f"Uploaded {upload_path} to {remote_file_path}")
 
 
 def main():
     sftp_client = get_sftp_client()
 
     remote_path = os.getenv("PATH_REMOTE_FOLDER")
-    print(f"remote_path: {remote_path}")
+    pr(f"remote_path: {remote_path}")
     event_handler = SFTPHandler(sftp_client, remote_path)
 
     observer = Observer()
     local_path = os.getenv("PATH_LOCAL_FOLDER")
     local_path = os.path.realpath(local_path)
-    print(f"local_path: {local_path}")
+    pr(f"local_path: {local_path}")
     observer.schedule(event_handler, path=local_path, recursive=True)
 
     observer.start()
