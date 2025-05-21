479ec0d814e2c7a039cdce095b903e9c308e9d62
Eduardo A.F. <eacevedof@yahoo.es>
2025-05-21 11:09:02 +0200

    querier

--- querier/querier.py
+++ querier/querier.py
@@ -1,7 +1,8 @@
+import os
+import sys
+from dotenv import load_dotenv
 import requests
 from tabulate import tabulate
-from dotenv import load_dotenv
-import os
 
 load_dotenv()
 
@@ -15,6 +16,7 @@ def __query_history():
     except FileNotFoundError:
         pass
     atexit.register(readline.write_history_file, histfile)
+
 
 def __query(sql):
     url = os.getenv("API_ANUBIS_URL")
@@ -41,29 +43,34 @@ def main():
     url = os.getenv("API_ANUBIS_URL")
     auth_token = os.getenv("API_ANUBIS_TOKEN")
     print(f"\nurl: {url}\ntoken: {auth_token}\n")
+
     while True:
         try:
-            sql = input("Introduce tu consulta SQL (o 'salir' para terminar):\n\t> ")
+            sql = input("SQL (quit + enter):\n\t> ")
+            if sql.lower() == "quit" or (len(sql) == 1 and ord(sql) == 24):  # Ctrl+X es 24 en ASCII
+                break
+
+            result = __query(sql)
+            if result.get("error"):
+                print(f"error: {result['status_code']} {result['error']}")
+                continue
+
+            rows = result.get("result", [])
+            if not rows:
+                print("Sin resultados.")
+                continue
+
+            headers = rows[0].keys()
+            table = [row.values() for row in rows]
+            print(tabulate(table, headers, tablefmt="grid"))
+
         except EOFError:
             print("EOF")
             break
+        except KeyboardInterrupt:
+            print("\nCtrl+C detected quiting...")
+            sys.exit(0)
 
-        if sql.lower() == "salir":
-            break
-
-        result = __query(sql)
-        if result.get("error"):
-            print(f"error: {result['status_code']} {result['error']}")
-            continue
-
-        rows = result.get("result", [])
-        if not rows:
-            print("Sin resultados.")
-            continue
-
-        headers = rows[0].keys()
-        table = [row.values() for row in rows]
-        print(tabulate(table, headers, tablefmt="grid"))
 
 if __name__ == "__main__":
     main()
