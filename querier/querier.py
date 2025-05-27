026a91ddbcd7ff4a48eea09cb3e8994220c1ed4b
Eduardo A.F. <eacevedof@yahoo.es>
2025-05-27 11:10:53 +0200

    querier prompt

--- querier/querier.py
+++ querier/querier.py
@@ -1,10 +1,15 @@
 import os
+from printer import *
 import sys
 from dotenv import load_dotenv
 import requests
 from tabulate import tabulate
-from printer import *
 from tokenizer import *
+from prompt_toolkit import PromptSession
+from prompt_toolkit.key_binding import KeyBindings
+from prompt_toolkit.formatted_text import HTML
+from prompt_toolkit.history import InMemoryHistory
+
 
 def __query(sql):
     url = os.getenv("API_ANUBIS_DOMAIN") + os.getenv("API_ANUBIS_ENDPOINT")
@@ -31,7 +36,6 @@ def __query(sql):
 
 def main():
     env_choice = input("select environment (dev or prod): ").strip()
-
     env_choice = f".{env_choice}" if env_choice in ["dev", "prod"] else ".dev"
 
     os.system("cls")
@@ -50,29 +54,40 @@ def main():
 
     pr_lemon(f"\nurl: {url}")
     pr_blue(f"token: {auth_token}\n")
-    pr_yellow("SQL o (quit + enter), (ctrl+c)")
+    pr_yellow("paste your sql. (quit para salir, clear para limpiar)")
+
+    history = InMemoryHistory()
+    prompt_session = PromptSession(multiline=True, history=history)
+    bindings = KeyBindings()
+
+    @bindings.add("c-m")
+    def _(event):
+        event.app.exit(result=event.app.current_buffer.text)
 
     while True:
         try:
             prompt = get_red("anubis> ") if is_prod else get_green("anubis> ")
-            sql = input(prompt)
-            if sql == "":
+            # sql = input(prompt)
+
+            html_prompt = HTML('<ansired>anubis&gt; </ansired>') if is_prod else HTML('<ansigreen>anubis&gt; </ansigreen>')
+            sql = prompt_session.prompt(html_prompt, key_bindings=bindings)
+            if sql.strip() == "":
                 continue
 
-            if sql.lower() == "clear":
+            if sql.lower().strip() == "clear":
                 os.system("cls")
                 continue
 
-            if sql.lower() == "help":
+            if sql.lower().strip() == "help":
                 pr_blue(f"\nurl: {url}\ntoken: {auth_token}\n")
                 continue
 
-            if sql.lower() == "quit" or (len(sql) == 1 and ord(sql) == 24):
+            if sql.lower().strip() == "quit":
                 break
 
             result = __query(sql)
             if result.get("error"):
-                pr_red(f"error: {result['status_code']} {result['error']}")
+                pr_red(f"error: {result["status_code"]} {result["error"]}")
                 continue
 
             rows = result.get("result", [])
