9c66a2ca74d475b998b5db1e7dfa090c2d40c6ab
Eduardo A.F. <eacevedof@yahoo.es>
2026-04-18 18:52:05 +0200

    ia-mcps: env

--- ia-mcps/ddd/shared/infrastructure/repositories/__init__.py
+++ ia-mcps/ddd/shared/infrastructure/repositories/__init__.py
@@ -1,3 +1,1 @@
-from ddd.shared.infrastructure.repositories.environment_reader_raw_repository import EnvironmentReaderRawRepository
-
-__all__ = ["EnvironmentReaderRawRepository"]
+# Shared repositories
